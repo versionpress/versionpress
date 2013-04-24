325b0aeec4adfec78eae1d2e43ea8f7e83f1fe1e
JanVoracek <jan@voracek.net>
2013-04-24 10:59:39 +0200

    [#2] Global variables in sync class refactored to DI

--- versionpress/sync.php
+++ versionpress/sync.php
@@ -4,38 +4,50 @@ require_once(dirname(__FILE__) . '/../../wp-load.php');
 
 class PostSynchronizer {
 
+    /**
+     * @var EntityStorage
+     */
+    private $postStorage;
+
+    /**
+     * @var string
+     */
+    private $tableName;
+
+    /**
+     * @var wpdb
+     */
+    private $database;
+
+    function __construct(EntityStorage $postStorage, wpdb $database, $tableName) {
+        $this->postStorage = $postStorage;
+        $this->database = $database;
+        $this->tableName = $tableName;
+    }
 
     function syncPosts() {
-        $posts = $this->loadAllPostsFromFiles();
-        $this->updatePostsInDatabase($posts);
+        $this->updatePostsInDatabase();
         $this->fixParentIds();
         $this->mirrorDatabaseToFiles();
     }
 
-    private function updatePostsInDatabase($posts) {
-        global $table_prefix, $wpdb;
+    private function updatePostsInDatabase() {
+        $posts = $this->loadAllPostsFromFiles();
         $postWithoutIDs = array_map(function($post){ unset($post['ID']); return $post; }, $posts);
 
         foreach ($postWithoutIDs as $post) {
-            $sql = $this->buildInsertWithUpdateFallbackQuery($table_prefix . 'posts', $post);
-            $wpdb->query($sql);
+            $sql = $this->buildInsertWithUpdateFallbackQuery($this->tableName, $post);
+            $this->database->query($sql);
         }
 
         $postVpIds = array_map(function($post){ return $post['vp_id'];  }, $posts);
-        $sql = "DELETE FROM wp_posts WHERE vp_id NOT IN (" . implode(', ', $postVpIds) . ")";
-        $wpdb->query($sql);
+        $sql = "DELETE FROM {$this->tableName} WHERE vp_id NOT IN (" . implode(', ', $postVpIds) . ")";
+        $this->database->query($sql);
     }
 
     private function loadAllPostsFromFiles() {
-        $postStorage = $this->getPostStorage();
-        $posts = $postStorage->loadAll();
+        $posts = $this->postStorage->loadAll();
         return $posts;
-    }
-
-    private function getPostStorage() {
-        $storageFactory = new EntityStorageFactory(VERSIONPRESS_MIRRORING_DIR);
-        $postStorage = $storageFactory->getStorage('posts');
-        return $postStorage;
     }
 
     private function buildInsertWithUpdateFallbackQuery($table, $data) {
@@ -46,16 +58,15 @@ class PostSynchronizer {
         $updatePairs = array_map(function($column) use ($safeValues){ return "$column = $safeValues[$column]"; }, $columns);
         $updateString = implode(', ', $updatePairs);
 
-        $sql = "INSERT INTO $table ($stringColumns) VALUES ($stringValues)
+        $sql = "INSERT INTO {$this->tableName} ($stringColumns) VALUES ($stringValues)
                 ON DUPLICATE KEY UPDATE $updateString";
 
         return $sql;
     }
 
     private function fixParentIds() {
-        global $wpdb;
-        $sql = "SELECT ID, post_parent, vp_id, vp_parent_id FROM wp_posts";
-        $posts = $wpdb->get_results($sql);
+        $sql = "SELECT ID, post_parent, vp_id, vp_parent_id FROM {$this->tableName}";
+        $posts = $this->database->get_results($sql);
         $vpId_ID_map = array();
         foreach($posts as $post) {
             $vpId_ID_map[$post->vp_id] = $post->ID;
@@ -67,8 +78,8 @@ class PostSynchronizer {
                 $newParent = $vpId_ID_map[$post->vp_parent_id];
             }
             if($post->post_parent != $newParent) {
-                $updateSql = "UPDATE wp_posts SET post_parent = $newParent WHERE ID = $post->ID";
-                $wpdb->query($updateSql);
+                $updateSql = "UPDATE {$this->tableName} SET post_parent = $newParent WHERE ID = $post->ID";
+                $this->database->query($updateSql);
             }
         }
     }
@@ -83,21 +94,23 @@ class PostSynchronizer {
         $filePostIds = array_map($getPostId, $postsInFiles);
 
         $deletedPostIds =  array_diff($filePostIds, $dbPostIds);
-        $postStorage = $this->getPostStorage();
 
         foreach($deletedPostIds as $deletedPostId) {
-            $postStorage->delete(array('ID' => $deletedPostId));
+            $this->postStorage->delete(array('ID' => $deletedPostId));
         }
 
-        $postStorage->saveAll($postsInDatabase);
+        $this->postStorage->saveAll($postsInDatabase);
     }
 
     private function loadAllPostsFromDatabase() {
-        global $wpdb;
-        $sql = "SELECT * FROM wp_posts";
-        return $wpdb->get_results($sql, ARRAY_A);
+        $sql = "SELECT * FROM {$this->tableName}";
+        return $this->database->get_results($sql, ARRAY_A);
     }
 }
 
-$postSynchronizer = new PostSynchronizer();
+$storageFactory = new EntityStorageFactory(VERSIONPRESS_MIRRORING_DIR);
+$postStorage = $storageFactory->getStorage('posts');
+
+global $wpdb, $table_prefix;
+$postSynchronizer = new PostSynchronizer($postStorage, $wpdb, $table_prefix . 'posts');
 $postSynchronizer->syncPosts();
