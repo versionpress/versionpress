ed4a67703c61748cff2b9a9cc343501d928530dc
JanVoracek <jan@voracek.net>
2013-04-29 14:34:03 +0200

    [#3] Implemented committing from PHP (for now it's by calling "shell_exec")

---
+++ versionpress/Git.php
@@ -0,0 +1,32 @@
+<?php
+
+abstract class Git {
+
+    private static $gitRoot = null;
+
+    // Constants
+    private static $ADD_AND_COMMIT_COMMAND = "git add -u %s && git commit -m %s";
+    private static $RELPATH_TO_GIT_ROOT_COMMAND = "git rev-parse --show-cdup";
+
+    static function commit($message, $directory = "") {
+        if ($directory === "" && self::$gitRoot === null) {
+            self::detectGitRoot();
+        }
+        $directory = $directory === "" ? self::$gitRoot : $directory;
+        $gitAddPath = $directory . "/" . "*";
+
+        self::runShellCommand(self::$ADD_AND_COMMIT_COMMAND, $gitAddPath, $message);
+    }
+
+    private static function detectGitRoot() {
+        self::$gitRoot = trim(self::runShellCommand(self::$RELPATH_TO_GIT_ROOT_COMMAND), "/\n");
+    }
+
+    private static function runShellCommand($command, $args = '') {
+        $functionArgs = func_get_args();
+        array_shift($functionArgs); // Remove $command
+        $escapedArgs = array_map("escapeshellarg", $functionArgs);
+        $commandWithArguments = vsprintf($command, $escapedArgs);
+        return shell_exec($commandWithArguments);
+    }
+}
--- versionpress/sync.php
+++ versionpress/sync.php
@@ -2,6 +2,7 @@
 
 require_once(dirname(__FILE__) . '/../../wp-load.php');
 require_once(dirname(__FILE__) . '/PostSynchronizer.php');
+require_once(dirname(__FILE__) . '/Git.php');
 
 $storageFactory = new EntityStorageFactory(VERSIONPRESS_MIRRORING_DIR);
 $postStorage = $storageFactory->getStorage('posts');
@@ -9,3 +10,5 @@ $postStorage = $storageFactory->getStorage('posts');
 global $wpdb, $table_prefix;
 $postSynchronizer = new PostSynchronizer($postStorage, $wpdb, $table_prefix . 'posts');
 $postSynchronizer->syncPosts();
+
+Git::commit('Test commit', dirname(__FILE__) . '/db');
