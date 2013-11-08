67db0f530a5637cb7d681e30477ac6df3eb5bebe
Jan Voráček <jan@voracek.net>
2013-11-08 12:03:14 +0100

    [#17] Added Nette Debugger

--- db.php
+++ db.php
@@ -2,5 +2,8 @@
 define('VERSIONPRESS_PLUGIN_DIR', dirname(__FILE__) . '/plugins/versionpress');
 define('VERSIONPRESS_MIRRORING_DIR', VERSIONPRESS_PLUGIN_DIR . '/db');
 
+require_once(VERSIONPRESS_PLUGIN_DIR . '/libs/NDebugger.php');
+NDebugger::enable(NDebugger::DEVELOPMENT, VERSIONPRESS_PLUGIN_DIR . '/log');
+
 if(file_exists(VERSIONPRESS_PLUGIN_DIR . '/.active'))
     require_once(VERSIONPRESS_PLUGIN_DIR . '/init.php');
