f573c26953dc2081d85da7158b4d050ce493a9e5
Jan Voráček <jan@voracek.net>
2013-12-06 10:51:20 +0100

    [#16] Implemented preservation of environment-specific variables in wp-config.

--- plugins/versionpress/administration/sync.php
+++ plugins/versionpress/administration/sync.php
@@ -1,6 +1,38 @@
 <?php
+
+function createConstantReplacementDefinition($constantName) {
+    return array(
+        'regexp' => "/define[^\\w]+$constantName/i",
+        'replaceWith' => "define('$constantName', '%s');",
+        'value' => constant($constantName)
+    );
+}
+
+function replaceDatabaseSettings() {
+    global $table_prefix;
+    $replacedConstants = array('DB_NAME', 'DB_USER', 'DB_PASSWORD', 'DB_HOST', 'DB_CHARSET', 'DB_COLLATE');
+    $replacementDefinitions = array_map("createConstantReplacementDefinition", $replacedConstants);
+    $replacementDefinitions[] = array(
+        'regexp' => '/\\$table_prefix[^\\w]*=/i',
+        'replaceWith' => '$table_prefix = "%s"',
+        'value' => $table_prefix
+    );
+
+    $configFileLines = file(ABSPATH . '/wp-config.php');
+    foreach($configFileLines as $lineNumber => $line) {
+        foreach($replacementDefinitions as $i => $replacementDefinition) {
+            if(preg_match($replacementDefinition['regexp'],$line)) {
+                $configFileLines[$lineNumber] = sprintf($replacementDefinition['replaceWith'], $replacementDefinition['value']);
+                unset($replacementDefinitions[$i]);
+            }
+        }
+    }
+    file_put_contents(ABSPATH . '/wp-config.php', implode(PHP_EOL, $configFileLines));
+}
+
 if (isset($_GET['pull'])) {
     Git::pull();
+    replaceDatabaseSettings();
     require_once __DIR__ . '/../../versionpress/sync.php';
 }
 if (isset($_GET['push'])) {
@@ -10,9 +42,9 @@ if (isset($_GET['push'])) {
 }
 ?>
 <h1>VersionPress</h1>
-<form method="POST" action="<?php echo admin_url('admin.php?page=versionpress/test.php&pull'); ?>">
+<form method="POST" action="<?php echo admin_url('admin.php?page=versionpress/administration/sync.php&pull'); ?>">
     <input type="submit" value="Synchronize - pull">
 </form>
-<form method="POST" action="<?php echo admin_url('admin.php?page=versionpress/test.php&push'); ?>">
+<form method="POST" action="<?php echo admin_url('admin.php?page=versionpress/administration/sync.php&push'); ?>">
     <input type="submit" value="Synchronize - push">
 </form>
