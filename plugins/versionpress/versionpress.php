fb39b3c278cbd4e636c2c9a2a937d0e4974cf84e
Borek Bernard <borekb@gmail.com>
2014-07-21 13:50:29 +0200

    [#30] Uninstallation split from deactivation

--- plugins/versionpress/versionpress.php
+++ plugins/versionpress/versionpress.php
@@ -6,14 +6,26 @@ Description: Git-versioning plugin for WordPress
 Author: Agilio
 Version: 1.0
 */
+
+
+register_activation_hook(__FILE__, 'versionpress_activate');
+register_deactivation_hook(__FILE__, 'versionpress_deactivate');
+register_uninstall_hook(__FILE__, 'versionpress_uninstall');
+
+
 function versionpress_activate() {
     copy(dirname(__FILE__) . '/_db.php', WP_CONTENT_DIR . '/db.php');
 }
 
 function versionpress_deactivate() {
-    global $wpdb, $table_prefix;
+
     unlink(VERSIONPRESS_PLUGIN_DIR . '/.active');
     unlink(WP_CONTENT_DIR . '/db.php');
+}
+
+function versionpress_uninstall() {
+
+    global $wpdb, $table_prefix;
 
     $queries[] = 'DROP VIEW `' . $table_prefix . 'vp_reference_details`;';
     $queries[] = 'DROP TABLE `' . $table_prefix . 'vp_references`, `' . $table_prefix . 'vp_id`;';
@@ -21,10 +33,9 @@ function versionpress_deactivate() {
     foreach ($queries as $query) {
         $wpdb->query($query);
     }
+
 }
 
-register_activation_hook(__FILE__, 'versionpress_activate');
-register_deactivation_hook(__FILE__, 'versionpress_deactivate');
 
 function isActive() {
     return defined('VERSIONPRESS_PLUGIN_DIR') && file_exists(VERSIONPRESS_PLUGIN_DIR . '/.active');
