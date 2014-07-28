8cdb2a5b209d16974a0cb51f254253c9bb021f18
Borek Bernard <borekb@gmail.com>
2014-07-28 10:37:20 +0200

    [#38] _db.php and init.php  documented
    Conflicts:
    	plugins/versionpress/_db.php
    	plugins/versionpress/init.php

--- plugins/versionpress/_db.php
+++ plugins/versionpress/_db.php
@@ -1,7 +1,24 @@
 <?php
+/**
+ * Plugin activation saves this file as `wp-content/db.php`. This means that it is executed
+ * with every request, both public and admin.
+ *
+ * VersionPress uses this primarily to overwrite the `$wpdb` instance with its mirroring implementation
+ * but we also use this to auto-load all the classes etc. This might be a little wasteful
+ * and should be looked at, see http://jira.agilio.cz/browse/WP-41.
+ */
+
+/**  */
 define('VERSIONPRESS_PLUGIN_DIR', dirname(__FILE__) . '/plugins/versionpress');
 define('VERSIONPRESS_MIRRORING_DIR', VERSIONPRESS_PLUGIN_DIR . '/db');
 
+/**
+ * Nette is currently referenced as a minified library. We only need pieces from it so we should
+ * ideally create a custom distribution at some point in the future.
+ *
+ * (Note: Nette 2.2 already uses the modular structure, however, it supports PHP 5.3 only. This might be
+ * a problem for us, or not, see http://jira.agilio.cz/browse/WP-10 and http://jira.agilio.cz/browse/WP-40.
+ */
 require_once(VERSIONPRESS_PLUGIN_DIR . '/libs/nette.min.php');
 NDebugger::enable(NDebugger::DETECT, VERSIONPRESS_PLUGIN_DIR . '/log');
 
@@ -13,5 +30,6 @@ $robotLoader->register();
 global $wpdb, $versionPressContainer;
 $versionPressContainer = DIContainer::getConfiguredInstance();
 
-if(file_exists(VERSIONPRESS_PLUGIN_DIR . '/.active'))
+if (file_exists(VERSIONPRESS_PLUGIN_DIR . '/.active')) {
     $wpdb = $versionPressContainer->resolve(VersionPressServices::DATABASE);
+}
