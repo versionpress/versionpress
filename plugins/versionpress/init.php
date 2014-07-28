54f8e5b85e7f600dd80cd3df096a1c28165453b6
Jan Voráček <jan@voracek.net>
2014-07-28 13:39:04 +0200

    Committer extracted to separated file

--- plugins/versionpress/init.php
+++ plugins/versionpress/init.php
@@ -7,6 +7,7 @@ $mirror = new Mirror($storageFactory);
 $schemaFile = VERSIONPRESS_PLUGIN_DIR . '/src/database/schema.neon';
 $schemaInfo = new DbSchemaInfo($schemaFile, $table_prefix);
 $wpdb = new MirroringDatabase(DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, $mirror, $schemaInfo);
+$committer = new Committer($mirror);
 
 // Hook for saving taxonomies into files
 // WordPress creates plain INSERT query and executes it using wpdb::query method instead of wpdb::insert.
@@ -39,63 +40,6 @@ function createUpdatePostTermsHook(EntityStorage $storage, wpdb $wpdb) {
     };
 }
 
-class Committer {
-
-    /** @var Mirror */
-    private $mirror;
-    /** @var  ChangeInfo */
-    private $forcedChangeInfo;
-
-    public function __construct(Mirror $mirror) {
-        $this->mirror = $mirror;
-    }
-
-    /**
-     * Checks if there is any change. If so, it tries to commit.
-     */
-    public function commit() {
-        if ($this->forcedChangeInfo) {
-            @unlink(get_home_path() . 'versionpress.maintenance'); // todo: this shouldn't be here...
-            Git::commit($this->forcedChangeInfo->getCommitMessage());
-            $this->forcedChangeInfo = null;
-        } elseif ($this->mirror->wasAffected() && $this->shouldCommit()) {
-            $changeList = $this->mirror->getChangeList();
-            $commitMessage = $changeList[0]->getCommitMessage();
-
-            Git::commit($commitMessage);
-        }
-    }
-
-    public function forceChangeInfo(ChangeInfo $changeInfo) {
-        $this->forcedChangeInfo = $changeInfo;
-    }
-
-    private function shouldCommit() {
-        // proof of concept
-        if ($this->dbWasUpdated() && $this->existsMaintenanceFile())
-            return false;
-        return true;
-    }
-
-    private function dbWasUpdated() {
-        $changes = $this->mirror->getChangeList();
-        foreach ($changes as $change) {
-            if ($change instanceof EntityChangeInfo &&
-                $change->getObjectType() == 'option' &&
-                $change->getEntityId() == 'db_version'
-            )
-                return true;
-        }
-        return false;
-    }
-
-    private function existsMaintenanceFile() {
-        $maintenanceFilePattern = get_home_path() . '*.maintenance';
-        return count(glob($maintenanceFilePattern)) > 0;
-    }
-}
-
-$committer = new Committer($mirror);
 add_filter('update_feedback', function () {
     touch(get_home_path() . 'versionpress.maintenance');
 });
