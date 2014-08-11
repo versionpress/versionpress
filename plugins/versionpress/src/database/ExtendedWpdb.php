b2eab0fe616873adcdbbd63c73230f1b3adea028
Jan Voráček <jan@voracek.net>
2014-08-11 13:39:39 +0200

    Fixed WP "bug": $wpdb->prefix is now available even before first SQL query

--- plugins/versionpress/src/database/ExtendedWpdb.php
+++ plugins/versionpress/src/database/ExtendedWpdb.php
@@ -7,13 +7,18 @@ define('ARRAY_MAP', 'ARRAY_MAP');
  */
 class ExtendedWpdb extends wpdb {
 
+    function __construct($dbuser, $dbpassword, $dbname, $dbhost) {
+        global $table_prefix;
+        parent::__construct($dbuser, $dbpassword, $dbname, $dbhost);
+        $this->prefix = $table_prefix;
+    }
+
     function get_row($query = null, $output = OBJECT, $y = 0) {
         if ($output === ARRAY_MAP) {
             $result = parent::get_row($query, ARRAY_N, $y);
             return array($result[0] => $result[1]);
         }
         return parent::get_row($query, $output, $y);
-
     }
 
 
