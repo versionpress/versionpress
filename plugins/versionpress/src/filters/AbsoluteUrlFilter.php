8a309d56efccb283a732bb2c293d623efa056f2a
Jan Voráček <jan@voracek.net>
2014-10-23 10:12:02 +0200

    [#128] Created AbsoluteUrlFilter for replacing the absolute URLs with placeholder

---
+++ plugins/versionpress/src/filters/AbsoluteUrlFilter.php
@@ -0,0 +1,49 @@
+<?php
+
+class AbsoluteUrlFilter implements EntityFilter {
+
+    const PLACEHOLDER = "<<[site-url]>>";
+    private $siteUrl;
+
+    function __construct() {
+        $this->siteUrl = get_site_url();
+    }
+
+    /**
+     * Replaces absolute URLs with placeholder
+     *
+     * @param array $entity
+     * @return array
+     */
+    function apply($entity) {
+        foreach ($entity as $field => $value) {
+            if(isset($entity[$field])) {
+                $entity[$field] = $this->replaceLocalUrls($value);
+            }
+        }
+        return $entity;
+    }
+
+    /**
+     * Replaces the placeholder with absolute URL
+     *
+     * @param array $entity
+     * @return array
+     */
+    function restore($entity) {
+        foreach ($entity as $field => $value) {
+            if(isset($entity[$field])) {
+                $entity[$field] = $this->replacePlaceholders($value);
+            }
+        }
+        return $entity;
+    }
+
+    private function replaceLocalUrls($value) {
+        return str_replace($this->siteUrl, self::PLACEHOLDER, $value);
+    }
+
+    private function replacePlaceholders($value) {
+        return str_replace(self::PLACEHOLDER, $this->siteUrl, $value);
+    }
+}
--- plugins/versionpress/src/synchronizers/PostsSynchronizer.php
+++ plugins/versionpress/src/synchronizers/PostsSynchronizer.php
@@ -1,8 +1,12 @@
 <?php
 
 class PostsSynchronizer extends SynchronizerBase {
+    /** @var EntityFilter */
+    private $filter;
+
     function __construct(EntityStorage $storage, wpdb $database, DbSchemaInfo $dbSchema) {
         parent::__construct($storage, $database, $dbSchema, 'posts');
+        $this->filter = new AbsoluteUrlFilter();
     }
 
     protected function filterEntities($entities) {
@@ -11,6 +15,7 @@ class PostsSynchronizer extends SynchronizerBase {
         foreach ($entities as $entity) {
             $entityClone = $entity;
             unset($entityClone['category'], $entityClone['post_tag']); // categories and tags are synchronized by TermRelationshipsSynchronizer
+            $entityClone = $this->filter->restore($entityClone);
             $filteredEntities[] = $entityClone;
         }
 
