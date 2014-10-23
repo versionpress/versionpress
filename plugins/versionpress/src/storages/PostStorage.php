301685b9cbc9e120e8df530c5efa312b63e66e07
Jan Voráček <jan@voracek.net>
2014-10-23 14:44:35 +0200

    [#134] Removed PostGuidFilter - guids cannot be changed

--- plugins/versionpress/src/storages/PostStorage.php
+++ plugins/versionpress/src/storages/PostStorage.php
@@ -4,7 +4,6 @@ class PostStorage extends DirectoryStorage implements EntityStorage {
 
     function __construct($directory) {
         parent::__construct($directory, 'post');
-        $this->addFilter(new PostGuidFilter());
         $this->addFilter(new AbsoluteUrlFilter());
     }
 
--- plugins/versionpress/src/synchronizers/PostsSynchronizer.php
+++ plugins/versionpress/src/synchronizers/PostsSynchronizer.php
@@ -28,18 +28,11 @@ class PostsSynchronizer extends SynchronizerBase {
 
     protected function doEntitySpecificActions() {
         $this->fixCommentCounts();
-        $this->fixGuids();
     }
 
     private function fixCommentCounts() {
         $sql = "update {$this->database->prefix}posts set comment_count =
      (select count(*) from {$this->database->prefix}comments where comment_post_ID = {$this->database->prefix}posts.ID and comment_approved = 1);";
-        $this->database->query($sql);
-    }
-
-    private function fixGuids() {
-        $siteUrl = get_site_url();
-        $sql = "update {$this->database->prefix}posts set guid = concat('{$siteUrl}/?p=', ID)";
         $this->database->query($sql);
     }
 }
