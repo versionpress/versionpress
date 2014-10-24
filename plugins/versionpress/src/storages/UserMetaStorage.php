5a5ad41711c3d3db88748531216920da9956c615
Jan Voráček <jan@voracek.net>
2014-10-24 11:37:02 +0200

    [minor] Removed duplicity

--- plugins/versionpress/src/storages/UserMetaStorage.php
+++ plugins/versionpress/src/storages/UserMetaStorage.php
@@ -9,13 +9,8 @@ class UserMetaStorage extends SingleFileStorage implements EntityStorage {
         parent::__construct($file, 'user', 'ID');
     }
 
-    function save($data, $restriction = array(), $id = 0) {
-        $values = array_merge($data, $restriction);
-        $key = sprintf('%s#%s', $values['meta_key'], $values['vp_id']);
-        $data = array(
-            'vp_id' => $values['vp_user_id'],
-            $key => $values['meta_value']
-        );
+    function save($values) {
+        $data = $this->transformToUserField($values);
 
         $this->userMetaKey = $values['meta_key'];
         $this->userMetaVpId = $values['vp_id'];
@@ -25,12 +20,7 @@ class UserMetaStorage extends SingleFileStorage implements EntityStorage {
 
     function saveAll($entities) {
         foreach($entities as $entity) {
-            $key = sprintf('%s#%s', $entity['meta_key'], $entity['vp_id']);
-            $data = array(
-                'vp_id' => $entity['vp_user_id'],
-                $key => $entity['meta_value']
-            );
-
+            $data = $this->transformToUserField($entity);
             $this->saveEntity($data);
         }
     }
@@ -42,5 +32,14 @@ class UserMetaStorage extends SingleFileStorage implements EntityStorage {
      */
     protected function createChangeInfo($entity, $changeType) {
         return new UserMetaChangeInfo($changeType, $this->userMetaVpId, $entity['user_login'], $this->userMetaKey);
+    }
+
+    private function transformToUserField($values) {
+        $key = sprintf('%s#%s', $values['meta_key'], $values['vp_id']);
+        $data = array(
+            'vp_id' => $values['vp_user_id'],
+            $key => $values['meta_value']
+        );
+        return $data;
     }
 }
