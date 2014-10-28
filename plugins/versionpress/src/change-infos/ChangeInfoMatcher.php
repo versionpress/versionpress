2c82b100adf6e1f307a6901f463339268c524805
Borek Bernard <borekb@gmail.com>
2014-10-28 14:45:44 +0100

    [#142] UninstallationUtil::uninstallation_should_remove_git_repo() now uses a new method in ChangeInfoMatcher to find out whether the first commit was VP installation commit

--- plugins/versionpress/src/change-infos/ChangeInfoMatcher.php
+++ plugins/versionpress/src/change-infos/ChangeInfoMatcher.php
@@ -3,55 +3,81 @@
 class ChangeInfoMatcher {
 
     /**
-     * For a given commit message, finds the matching ChangeInfo class and creates
-     * an instance of it.
+     * Map between partial regex describing the format of the VP-Action tag
+     * and a ChangeInfo class. The full PCRE regex will be constructed from the string
+     * by adding "^" and "$" so the values below have to match the whole VP-Action value.
      *
-     * Matching is done based on the value of the VP-Action tag.
+     * @var array
+     */
+    private static $changeInfoMap = array(
+
+        // VersionPress actions:
+        "versionpress/(?!(undo|rollback)).*" => 'VersionPressChangeInfo',
+        "versionpress/(undo|rollback)/.*" => 'RevertChangeInfo',
+
+        // WordPress core actions:
+        "plugin/.*" => 'PluginChangeInfo',
+        "theme/.*" => 'ThemeChangeInfo',
+        "wordpress/update/.*" => 'WordPressUpdateChangeInfo',
+
+        // Actions on entities:
+        "post/.*" => 'PostChangeInfo',
+        "comment/.*" => 'CommentChangeInfo',
+        "option/.*" => 'OptionChangeInfo',
+        "term/.*" => 'TermChangeInfo',
+        "usermeta/.*" => 'UserMetaChangeInfo',
+        "user/.*" => 'UserChangeInfo',
+
+        // Unknown action:
+        "" => 'UntrackedChangeInfo',
+
+    );
+
+    /**
+     * For a given commit message, finds the matching ChangeInfo class and creates
+     * an instance of it. Matching is done based on the value of the VP-Action tag.
      *
      * @param CommitMessage $commitMessage
      * @return ChangeInfo
      */
     public static function createMatchingChangeInfo(CommitMessage $commitMessage) {
+        $matchingChangeInfoType = self::findMatchingChangeInfo($commitMessage);
+        return $matchingChangeInfoType::buildFromCommitMessage($commitMessage);
+    }
 
-        // Map between partial regex describing the format of the VP-Action tag
-        // and a ChangeInfo class. The full PCRE regex will be constructed from the string
-        // by adding "^" and "$" so the values below have to match the whole VP-Action value.
+    /**
+     * Returns matching ChangeInfo type for a given commit message. Matching is done based on the value of the VP-Action tag.
+     *
+     * @param CommitMessage $commitMessage
+     * @throws Exception When no matching ChangeInfo type is found (should never happen)
+     * @return string "Class" of the matching ChangeInfo object
+     */
+    public static function findMatchingChangeInfo(CommitMessage $commitMessage) {
 
-        $map = array(
+        $actionTagValue = $commitMessage->getVersionPressTag(TrackedChangeInfo::ACTION_TAG); // can be empty string which is not a problem
 
-            // VersionPress actions:
-            "versionpress/(?!(undo|rollback)).*" => 'VersionPressChangeInfo',
-            "versionpress/(undo|rollback)/.*" => 'RevertChangeInfo',
-
-            // WordPress core actions:
-            "plugin/.*" => 'PluginChangeInfo',
-            "theme/.*" => 'ThemeChangeInfo',
-            "wordpress/update/.*" => 'WordPressUpdateChangeInfo',
-
-            // Actions on entities:
-            "post/.*" => 'PostChangeInfo',
-            "comment/.*" => 'CommentChangeInfo',
-            "option/.*" => 'OptionChangeInfo',
-            "term/.*" => 'TermChangeInfo',
-            "usermeta/.*" => 'UserMetaChangeInfo',
-            "user/.*" => 'UserChangeInfo',
-
-        );
-
-        $changeInfoTypeToCreate = 'UntrackedChangeInfo'; // fallback
-
-        $actionTagValue = $commitMessage->getVersionPressTag(TrackedChangeInfo::ACTION_TAG);
-        if ($actionTagValue != '') {
-            foreach ($map as $expr => $changeInfoType) {
-                $regex = "~^" . $expr . "$~";
-                if (preg_match($regex, $actionTagValue)) {
-                    $changeInfoTypeToCreate = $changeInfoType;
-                }
+        foreach (self::$changeInfoMap as $actionTagExpression => $changeInfoType) {
+            $regex = "~^" . $actionTagExpression . "$~";
+            if (preg_match($regex, $actionTagValue)) {
+                return $changeInfoType;
             }
         }
 
-        return $changeInfoTypeToCreate::buildFromCommitMessage($commitMessage);
+        // Code execution should never reach this point, at least the 'UntrackedChangeInfo' should match
+        throw new Exception("Matching ChangeInfo type not found");
 
+
+    }
+
+    /**
+     * Return true if the given $commitMesssage matches the $changeInfoClass
+     *
+     * @param CommitMessage $commitMessage
+     * @param string $changeInfoClass
+     * @return bool
+     */
+    public static function matchesChangeInfo(CommitMessage $commitMessage, $changeInfoClass) {
+        return self::findMatchingChangeInfo($commitMessage) == $changeInfoClass;
     }
 
 }
--- plugins/versionpress/src/utils/UninstallationUtil.php
+++ plugins/versionpress/src/utils/UninstallationUtil.php
@@ -14,6 +14,6 @@ class UninstallationUtil {
      */
     public static function uninstallation_should_remove_git_repo() {
         $initialCommit = Git::getInitialCommit();
-        return VersionPressChangeInfo::matchesCommitMessage($initialCommit->getMessage());
+        return ChangeInfoMatcher::matchesChangeInfo($initialCommit->getMessage(), 'VersionPressChangeInfo');
     }
 }
