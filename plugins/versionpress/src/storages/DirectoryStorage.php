versionpress/versionpress

commit d9c710bc9c2af53eb133185c3bfe182504e47844

[#130] Implemented synchronizing and created PostMetaChangeInfo

plugins/versionpress/src/storages/DirectoryStorage.php
@@ -79,12 +79,17 @@ abstract class DirectoryStorage extends ObservableStorage implements EntityStora
 
     private function loadAllFromFiles($entityFiles) {
         $entities = array();
+        $indexedEntities = array();
 
         foreach ($entityFiles as $file) {
             $entities[] = $this->deserializeEntity(file_get_contents($file));
         }
 
-        return $entities;
+        foreach($entities as $entity) {
+            $indexedEntities[$entity['vp_id']] = $entity;
+        }
+
+        return $indexedEntities;
     }
 
     protected function removeUnwantedColumns($entity) {

plugins/versionpress/admin/index.php
@@ -342,6 +342,7 @@ function _vp_show_progress_message($progressMessage) {
                     'WordPressUpdateChangeInfo',
                     'VersionPressChangeInfo',
                     'RevertChangeInfo',
+                    'PostMetaChangeInfo',
                     'PostChangeInfo',
                     'CommentChangeInfo',
                     'OptionChangeInfo',

plugins/versionpress/src/change-infos/PostMetaChangeInfo.php
@@ -1,0 +1,71 @@
+<?php
+
+class PostMetaChangeInfo extends EntityChangeInfo {
+
+    const POST_TITLE_TAG = "VP-Post-Title";
+    const POST_TYPE_TAG = "VP-Post-Type";
+    const POST_META_KEY = "VP-PostMeta-Key";
+
+    /**
+     * @var string
+     */
+    private $postType;
+    /**
+     * @var string
+     */
+    private $postTitle;
+    /**
+     * @var string
+     */
+    private $metaKey;
+
+    public function __construct($action, $entityId, $postType, $postTitle, $metaKey) {
+        parent::__construct("postmeta", $action, $entityId);
+        $this->postType = $postType;
+        $this->postTitle = $postTitle;
+        $this->metaKey = $metaKey;
+    }
+
+    /**
+     * @param CommitMessage $commitMessage
+     * @return bool
+     */
+    public static function matchesCommitMessage(CommitMessage $commitMessage) {
+        return parent::matchesCommitMessage($commitMessage) && ChangeInfoHelpers::actionTagStartsWith($commitMessage, "postmeta");
+    }
+
+    /**
+     * @param CommitMessage $commitMessage
+     * @return PostChangeInfo
+     */
+    public static function buildFromCommitMessage(CommitMessage $commitMessage)  {
+        $tags = $commitMessage->getVersionPressTags();
+        $actionTag = $tags[BaseChangeInfo::ACTION_TAG];
+        list($_, $action, $entityId) = explode("/", $actionTag, 3);
+        $titleTag = isset($tags[self::POST_TITLE_TAG]) ? $tags[self::POST_TITLE_TAG] : $entityId;
+        $type = $tags[self::POST_TYPE_TAG];
+        $metaKey = $tags[self::POST_META_KEY];
+        return new self($action, $entityId, $type, $titleTag, $metaKey);
+    }
+
+    /**
+     * @return string
+     */
+    public function getChangeDescription() {
+        switch($this->getAction()) {
+            case "create":
+                return "Created option '{$this->metaKey}' for {$this->postType} '{$this->postTitle}'";
+            case "delete":
+                return "Deleted option '{$this->metaKey}' for {$this->postType} '{$this->postTitle}'";
+        }
+        return "Edited option '{$this->metaKey}' for {$this->postType} '{$this->postTitle}'";
+    }
+
+    protected function getCustomTags() {
+        return array(
+            self::POST_TITLE_TAG => $this->postTitle,
+            self::POST_TYPE_TAG => $this->postType,
+            self::POST_META_KEY => $this->metaKey
+        );
+    }
+}

plugins/versionpress/src/git/Reverter.php
@@ -68,7 +68,7 @@ class Reverter {
     }
 
     private function synchronize() {
-        $synchronizationQueue = array('options', 'users', 'usermeta', 'posts', 'comments', 'terms', 'term_taxonomy', 'term_relationships');
+        $synchronizationQueue = array('options', 'users', 'usermeta', 'posts', 'postmeta', 'comments', 'terms', 'term_taxonomy', 'term_relationships');
         $this->synchronizationProcess->synchronize($synchronizationQueue);
     }
 

plugins/versionpress/src/storages/PostMetaStorage.php
@@ -5,21 +5,21 @@ class PostMetaStorage extends DirectoryStorage {
     private $postMetaKey;
     private $postMetaVpId;
 
+    private $ignoredMeta = array('_edit_lock', '_edit_last');
+
     function __construct($directory) {
         parent::__construct($directory, 'post');
     }
 
     function save($values) {
+        if(in_array($values['meta_key'], $this->ignoredMeta)) return;
+
         $data = $this->transformToPostField($values);
 
         $this->postMetaKey = $values['meta_key'];
         $this->postMetaVpId = $values['vp_id'];
 
         $this->saveEntity($data, array($this, 'notifyChangeListeners'));
-    }
-
-    function delete($restriction) {
-        parent::delete($restriction); // TODO: Change the autogenerated stub
     }
 
     function saveAll($entities) {
@@ -29,9 +29,11 @@ class PostMetaStorage extends DirectoryStorage {
         }
     }
 
-
     protected function createChangeInfo($entity, $changeType) {
-        return new PostChangeInfo('edit', $entity['vp_id'], 'postmeta', $entity['post_title']);
+        $postTitle = $entity['post_title'];
+        $postType = $entity['post_type'];
+
+        return new PostMetaChangeInfo($changeType, $this->postMetaVpId, $postType, $postTitle, $this->postMetaKey);
     }
 
     private function transformToPostField($values) {

plugins/versionpress/src/synchronizers/PostMetaSynchronizer.php
@@ -1,6 +1,6 @@
 <?php
 
-class UserMetaSynchronizer extends SynchronizerBase {
+class PostMetaSynchronizer extends SynchronizerBase {
     /** @var  wpdb */
     private $database;
 
@@ -8,14 +8,14 @@ class UserMetaSynchronizer extends SynchronizerBase {
     private $dbSchema;
 
     function __construct(EntityStorage $storage, wpdb $database, DbSchemaInfo $dbSchema) {
-        parent::__construct($storage, $database, $dbSchema, 'usermeta');
+        parent::__construct($storage, $database, $dbSchema, 'postmeta');
         $this->database = $database;
         $this->dbSchema = $dbSchema;
     }
 
     protected function transformEntities($entities) {
         $transformedEntities = array();
-        foreach ($entities as $userId => $entity) {
+        foreach ($entities as $postId => $entity) {
             foreach($entity as $meta_key => $meta_value) {
                 $dividerPosition = strrpos($meta_key, '#');
 
@@ -28,7 +28,7 @@ class UserMetaSynchronizer extends SynchronizerBase {
 
                 $transformedEntity = array();
                 $transformedEntity['vp_id'] = $id;
-                $transformedEntity['vp_user_id'] = $userId;
+                $transformedEntity['vp_post_id'] = $postId;
                 $transformedEntity['meta_key'] = $key;
                 $transformedEntity['meta_value'] = $meta_value;
                 $transformedEntities[] = $transformedEntity;

plugins/versionpress/src/synchronizers/SynchronizerFactory.php
@@ -27,6 +27,7 @@ class SynchronizerFactory {
     public function createSynchronizer($synchronizerName) {
         static $synchronizerClasses = array(
             'posts' => 'PostsSynchronizer',
+            'postmeta' => 'PostMetaSynchronizer',
             'comments' => 'CommentsSynchronizer',
             'options' => 'OptionsSynchronizer',
             'users' => 'UsersSynchronizer',

commit 3d969246f8ddb1628ca5f9ce2b37269e9dd01ebf

[#153] Fixed regression from commit d9c710b

plugins/versionpress/src/storages/DirectoryStorage.php
@@ -155,9 +155,9 @@ abstract class DirectoryStorage extends ObservableStorage implements EntityStora
         return 'edit';
     }
 
-    private function loadEntity($id) {
-        $entities = $this->loadAllFromFiles(array($this->getFilename($id)));
-        return $entities[0];
+    private function loadEntity($vpid) {
+        $entities = $this->loadAllFromFiles(array($this->getFilename($vpid)));
+        return $entities[$vpid];
     }
 
     protected function applyFilters($data) {