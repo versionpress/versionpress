0e6a09a70d9050f6ef1f92292df6f11e03824efb
Borek Bernard <borekb@gmail.com>
2014-11-05 11:33:02 +0100

    [#159] Renamed `wp vp clone --branch` parameter to `--name` and added some documentation

--- plugins/versionpress/src/cli/VPCommand.php
+++ plugins/versionpress/src/cli/VPCommand.php
@@ -1,16 +1,24 @@
 <?php
 
+/**
+ * VersionPress CLI commands
+ */
 class VPCommand extends WP_CLI_Command
 {
     const BRANCH_FLAG = "branch";
 
     /**
-     * Clone the WordPress site
+     * Clone the site to a separate folder, db and Git branch.
+     *
      *
      * ## OPTIONS
      *
-     * --branch=<name>
-     * : Name for created branch.
+     * --name=<name>
+     * : Name of the clone. Will be used as a suffix of the cloned folder, new
+     * database and the name of a new Git branch. For example, if the original
+     * site is "wpsite" and the `name` parameter is "test", the clone will be
+     * in the folder called "wpsite_test", the database will be "<orig>_test"
+     * and the Git branch name will be "test".
      * 
      *
      * @subcommand clone
