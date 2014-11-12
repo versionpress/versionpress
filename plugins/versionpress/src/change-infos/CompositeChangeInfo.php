440065e6d4a1e1ecd18f5fe51b546acc0d167826
Jan Voráček <jan@voracek.net>
2014-11-12 10:10:55 +0100

    [#167] Implemented priorities for ChangeInfo objects in CompositeChangeInfo

--- plugins/versionpress/src/change-infos/CompositeChangeInfo.php
+++ plugins/versionpress/src/change-infos/CompositeChangeInfo.php
@@ -9,9 +9,30 @@ class CompositeChangeInfo implements ChangeInfo {
     private $changeInfoList;
 
     /**
+     * List of change info classes ordered by their priorities.
+     * They are listed in commits / commit table in this order.
+     *
+     * @var string[]
+     */
+    private $priorityOrder = array(
+        "WordPressUpdateChangeInfo",
+        "VersionPressChangeInfo",
+        "PostChangeInfo",
+        "CommentChangeInfo",
+        "UserChangeInfo",
+        "RevertChangeInfo",
+        "PluginChangeInfo",
+        "ThemeChangeInfo",
+        "TermChangeInfo",
+        "OptionChangeInfo",
+        "PostMetaChangeInfo",
+        "UserMetaChangeInfo",
+    );
+
+    /**
      * @param ChangeInfo[] $changeInfoList
      */
-    function __construct($changeInfoList) {
+    public function __construct($changeInfoList) {
         $this->changeInfoList = $changeInfoList;
     }
 
@@ -21,11 +42,11 @@ class CompositeChangeInfo implements ChangeInfo {
      * @see Committer::commit()
      * @return CommitMessage
      */
-    function getCommitMessage() {
+    public function getCommitMessage() {
         $subject = $this->getChangeDescription();
 
         $bodies = array();
-        foreach ($this->changeInfoList as $changeInfo) {
+        foreach ($this->getSortedChangeInfoList() as $changeInfo) {
             $bodies[] = $changeInfo->getCommitMessage()->getBody();
         }
 
@@ -41,9 +62,10 @@ class CompositeChangeInfo implements ChangeInfo {
      *
      * @return string
      */
-    function getChangeDescription() {
-        $firstChangeDescription = $this->changeInfoList[0]->getChangeDescription();
-        $numberOfAnotherChanges = count($this->changeInfoList) - 1; // minus the one which change description is displayed
+    public function getChangeDescription() {
+        $changeList = $this->getSortedChangeInfoList();
+        $firstChangeDescription = $changeList[0]->getChangeDescription();
+        $numberOfAnotherChanges = count($changeList) - 1; // minus the one which change description is displayed
         return $firstChangeDescription . " and $numberOfAnotherChanges more change" . ($numberOfAnotherChanges > 1 ? "s" : "");
     }
 
@@ -54,7 +76,7 @@ class CompositeChangeInfo implements ChangeInfo {
      * @param CommitMessage $commitMessage
      * @return ChangeInfo
      */
-    static function buildFromCommitMessage(CommitMessage $commitMessage) {
+    public static function buildFromCommitMessage(CommitMessage $commitMessage) {
         $fullBody = $commitMessage->getBody();
         $splittedBodies = explode("\n\n", $fullBody);
         $changeInfoList = array();
@@ -65,5 +87,33 @@ class CompositeChangeInfo implements ChangeInfo {
         }
 
         return new self($changeInfoList);
+    }
+
+    /**
+     * @return ChangeInfo[]
+     */
+    private function getSortedChangeInfoList() {
+        $changeList = $this->changeInfoList;
+        usort($changeList, array($this, 'compareChangeInfoByPriority'));
+        return $changeList;
+    }
+
+    /**
+     * @param ChangeInfo $changeInfo1
+     * @param ChangeInfo $changeInfo2
+     * @return int
+     */
+    private function compareChangeInfoByPriority($changeInfo1, $changeInfo2) {
+        $class1 = get_class($changeInfo1);
+        $class2 = get_class($changeInfo2);
+
+        $priority1 = array_search($class1, $this->priorityOrder);
+        $priority2 = array_search($class2, $this->priorityOrder);
+
+        if ($priority1 < $priority2)
+            return -1;
+        if ($priority1 > $priority2)
+            return 1;
+        return 0;
     }
 }
