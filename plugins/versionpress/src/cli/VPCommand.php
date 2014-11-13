2b75a0fed4bef62b92fd89c265ce2a3ea72fcc2c
Borek Bernard <borekb@gmail.com>
2014-11-13 16:06:29 +0100

    [#159] Clone CLI command split into public part (`wp vp clone`) and an internal part (`wp vp-internal init-clone`). The whole process roughly works, thought the final synchronization step still has to be reviewed and possibly fixed as it hasn't been touched in ages.

--- plugins/versionpress/src/cli/VPCommand.php
+++ plugins/versionpress/src/cli/VPCommand.php
@@ -5,92 +5,76 @@
  */
 class VPCommand extends WP_CLI_Command
 {
-    const NAME_FLAG = "name";
 
     /**
-     * Clone the site to a separate folder, db and Git branch.
+     * Clones site to a new folder, database and Git branch.
      *
      * ## OPTIONS
      *
      * --name=<name>
-     * : Name of the clone. Will be used as a suffix of the cloned folder, new
-     * database and the name of a new Git branch. For example, if the original
-     * site is "wpsite" and the `name` parameter is "test", the clone will be
-     * in the folder called "wpsite_test", the database will be "<orig>_test"
-     * and the Git branch name will be "test".
+     * : Name of the clone. Used as a suffix for new folder, a suffix for new
+     * database and a name of the new Git branch. See example below.
      *
+     * --force
+     * : Forces cloning even if the target folder / database already exist.
+     *
+     * ## EXAMPLES
+     *
+     * Let's say we have a site in folder `wp01` that uses database called `wp01db`. The command
+     *
+     *     wp vp clone --name=test
+     *
+     * creates a copy of the site in `wp01_test`, a new Git branch called `test`
+     * and a new database `wp01db_test`.
+     *
+     * @synopsis --name=<name> [--force]
      *
      * @subcommand clone
      */
-    public function clone_($args = array(), $flags = array()) {
-        $name = $flags[self::NAME_FLAG];
-        $this->doClone($name);
-    }
+    public function clone_($args = array(), $assoc_args = array()) {
+        $name = $assoc_args['name'];
 
-    /**
-     * Creates a new db
-     *
-     * ## OPTIONS
-     *
-     * --name=<name>
-     * : New db name
-     *
-     * @subcommand new-db
-     */
-    public function newDb_($args = array(), $flags = array()) {
-        $dbName = $flags[self::NAME_FLAG];
-        $this->doNewDb($dbName);
-    }
-
-    private function doClone($name) {
-
-        // 1) Clone the repo
         $currentWpPath = get_home_path();
         $clonePath = sprintf("%s/%s_%s", dirname($currentWpPath), basename($currentWpPath), $name);
-        Git::cloneRepository($currentWpPath, $clonePath);
 
-        // 2) Create a new Git branch there
-        $createBranchCommand = 'git checkout -b ' . escapeshellarg($name);
-        $process = new \Symfony\Component\Process\Process($createBranchCommand, $clonePath);
-        $process->run();
-
-        if (!$process->isSuccessful()) {
-            WP_CLI::error("Failed creating branch on clone, message: " . $process->getErrorOutput());
+        if (is_dir($clonePath) && !array_key_exists('force', $assoc_args)) {
+            WP_CLI::error("Directory '" . basename($clonePath) . "' already exists. Use --force to overwrite it or use another clone name.");
         }
 
-        // 3) Create the new db in the clone
-        $configureCloneCmd = 'wp --require=' . escapeshellarg($clonePath . '/wp-content/plugins/versionpress/src/cli/VPCommand.php') . ' vp new-db --name=' . escapeshellarg(DB_NAME . '_' . $name);
+        if (is_dir($clonePath)) {
+            $rmDirResult = FileSystem::getWpFilesystem()->rmdir($clonePath, true);
+            if (!$rmDirResult) {
+                // rmdir most often fails, maybe because of Git repo in it? try some other removal strategy
+                WP_CLI::error("Could not delete directory '" . basename($clonePath) . "'. Please do it manually.");
+            }
+        }
+
+        Git::cloneRepository($currentWpPath, $clonePath);
+
+        WP_CLI::success("Site files cloned");
+
+
+        $configureCloneCmd = 'wp --require=' . escapeshellarg($clonePath . '/wp-content/plugins/versionpress/src/cli/VPInternalCommand.php');
+        $configureCloneCmd .= ' vp-internal init-clone --name=' . escapeshellarg($name);
+        if (array_key_exists('force', $assoc_args)) {
+            $configureCloneCmd .= ' --force-db';
+        }
+        $configureCloneCmd .= " --debug";
+
         $process = new \Symfony\Component\Process\Process($configureCloneCmd, $clonePath);
         $process->run();
 
         if (!$process->isSuccessful()) {
-            WP_CLI::error("Failed executing `wp vp new-db` on clone, message: " . $process->getErrorOutput());
+            WP_CLI::log($process->getOutput()); // WP-CLI sends it to STDOUT, not STDERR
+            WP_CLI::error("Initializing clone failed");
+        } else {
+            WP_CLI::log($process->getOutput());
         }
 
+        WP_CLI::success("Cloning done. Find your clone in '" . basename($clonePath) . "'.");
 
     }
 
-    private function doNewDb($dbName) {
-
-        WP_CLI::log("Doing new-db");
-
-        // 1) Update wp-config
-        $wpConfigFile = ABSPATH . 'wp-config.php';
-        $config = file_get_contents($wpConfigFile);
-        $config = preg_replace("/^(define.*\\(.*['\"]DB_NAME['\"]\\,.*['\"])(.*)(['\"].*)$/m", "$1$dbName$3", $config, 1);
-        file_put_contents($wpConfigFile, $config);
-
-        // 2) Create the db
-        $createDbCmd = 'wp db create';
-        $process = new \Symfony\Component\Process\Process($createDbCmd, ABSPATH);
-        $process->run();
-
-        // 3) Run synchronization
-        global $versionPressContainer;
-        /** @var SynchronizationProcess $syncProcess */
-        $syncProcess = $versionPressContainer->resolve(VersionPressServices::SYNCHRONIZATION_PROCESS);
-        $syncProcess->synchronize();
-    }
 }
 
 if (defined('WP_CLI') && WP_CLI) {
---
+++ plugins/versionpress/src/cli/VPInternalCommand.php
@@ -0,0 +1,109 @@
+<?php
+
+/**
+ * Internal VersionPress commands.
+ *
+ * ## USAGE
+ *
+ * These internal commands are not registered with WP-CLI automatically like the "public"
+ * `wp vp` commands in versionpress.php. You have to manually require the file, e.g.:
+ *
+ *     wp --require=path/to/this/VPInternalCommand.php vp-internal ...
+ *
+ * These internal commands are mostly used by public `wp vp` commands.
+ *
+ */
+class VPInternalCommand extends WP_CLI_Command
+{
+
+    /**
+     * Initializes a clone
+     *
+     * ## OPTIONS
+     *
+     * --name=<name>
+     * : Name of the clone.
+     *
+     * --force-db
+     * : Normally, init-clone fails if the targed DB already exists. Specify
+     * this flag to drop the db first.
+     *
+     * @synopsis --name=<name> [--force-db]
+     *
+     * @subcommand init-clone
+     */
+    public function initClone($args = array(), $assoc_args = array()) {
+        $name = $assoc_args['name'];
+        $dbName = DB_NAME . '_' . $name;
+
+        // 1) Create a new Git branch here
+        $createBranchCommand = 'git checkout -b ' . escapeshellarg($name);
+        $process = new \Symfony\Component\Process\Process($createBranchCommand);
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            WP_CLI::error("Failed creating branch on clone, message: " . $process->getErrorOutput());
+        } else {
+            WP_CLI::success("New Git branch created");
+        }
+
+
+        // 2) Update wp-config
+        $wpConfigFile = ABSPATH . 'wp-config.php';
+        $config = file_get_contents($wpConfigFile);
+        $config = preg_replace("/^(define.*\\(.*['\"]DB_NAME['\"]\\,.*['\"])(.*)(['\"].*)$/m", "$1$dbName$3", $config, 1);
+        file_put_contents($wpConfigFile, $config);
+        WP_CLI::success("wp-config.php updated");
+
+
+        // 3) Create the db
+
+        if (array_key_exists("force-db", $assoc_args)) {
+            $dropDbCmd = 'wp db drop --yes';
+            $process = new \Symfony\Component\Process\Process($dropDbCmd);
+            $process->run();
+            if ($process->isSuccessful()) {
+                WP_CLI::success("Database dropped");
+            } else {
+                // Here, most probably the database could not be dropped because it didn't
+                // exist. That's fine, we wanted to delete it anyway.
+            }
+        }
+
+
+        $createDbCmd = 'wp db create';
+        $process = new \Symfony\Component\Process\Process($createDbCmd);
+        $process->run();
+        if (!$process->isSuccessful()) {
+            WP_CLI::log("Failed creating database. If the problem is existing db, try running this with --force-db flag.");
+            WP_CLI::error($process->getOutput()); // WPCLI uses STDOUT instead of STDERR
+        } else {
+            WP_CLI::success("Database created");
+        }
+
+        // 4) Create WP tables
+        $createWpTablesCmd = 'wp core install --url=x --title=x --admin_user=x --admin_password=x --admin_email=x@example.com';
+        $process = new \Symfony\Component\Process\Process($createWpTablesCmd);
+        $process->run();
+        if (!$process->isSuccessful()) {
+            WP_CLI::log("Failed creating WP tables.");
+            WP_CLI::error($process->getErrorOutput());
+        } else {
+            WP_CLI::success("WP tables created");
+        }
+
+        // 5) Run synchronization
+        global $versionPressContainer;
+        /** @var SynchronizationProcess $syncProcess */
+        $syncProcess = $versionPressContainer->resolve(VersionPressServices::SYNCHRONIZATION_PROCESS);
+        $syncProcess->synchronize();
+        WP_CLI::success("Git -> db synchronization run");
+
+
+    }
+
+}
+
+if (defined('WP_CLI') && WP_CLI) {
+    WP_CLI::add_command('vp-internal', 'VPInternalCommand');
+}
