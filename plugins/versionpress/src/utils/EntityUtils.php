versionpress/versionpress

commit 4ca317e663aa23191cb307d0c668a8d36382c176

[#175] Change listeners in storages replaced with return values of `save()` and `delete()` methods
Details:

 * `Storage::addChangeListener()` and `callOnChangeListeners()` removed
 * `Storage::save()` and `delete()` methods now return ChangeInfo|null
 * Storages no longer use the helper `saveEntity()` method - the work was just moved to the save() method (there was no value added in saveEntity() after the refactoring)
 * `createChangeInfo()` method which used to be internal method in various subclasses extracted to the Storage base class
 * DirectoryStorage and its subclasses no longer have a dedicated `getEditAction()` - the detection (if at all necessary) is simply done in `createChangeInfo()`
 * Inline diff computation replaced with the new `EntityUtils::getDiff()` method

plugins/versionpress/src/utils/EntityUtils.php
@@ -1,0 +1,46 @@
+<?php
+
+/**
+ * VersionPress "entities" are often just associative arrays, often not even complete
+ * (for instance if the SQL update method is captured and only part of the entity data
+ * is available). Here are some helper methods for such arrays.
+ */
+class EntityUtils {
+
+    /**
+     * Used by storages to determine a diff between two entity data. Note that the data might
+     * not be complet - for example, the $oldEntityData might be the previous, full state
+     * of the entity while the $newEntityData is just the change that was captured by VersionPress.
+     * Or the $newEntityData might be another full entity, for example in its new state.
+     * It doesn't really matter, the algo is always the same: it scans for new keys
+     * in $newEntityData and changed values on existing keys and adds those two things
+     * to the result. 'vp_id' key is ignored by default.
+     *
+     * Note: keys may never be "removed" (or marked as removed) in the diff because that
+     * will just not ever happen - the SQL UPDATE command is only capable of sending a change,
+     * clearing a key value at most but never "removing" it entirely.
+     *
+     * @param array $oldEntityData Usually a full entity data in its original state
+     * @param array $newEntityData Full or partial data of the new entity
+     * @param bool $ignoreVpid If true (default), `vp_id` key is ignored (not added to the diff)
+     *
+     * @return array Key=>value pairs of things that are new or changed in $newEntity (they can
+     *               never be "removed", that will never happen when capturing SQL UPDATE actions)
+     */
+    public static function getDiff($oldEntityData, $newEntityData, $ignoreVpid = true) {
+        $diff = array();
+        foreach ($newEntityData as $key => $value) {
+            if (!isset($oldEntityData[$key]) || $oldEntityData[$key] != $value) {
+                $diff[$key] = $value;
+            }
+        }
+
+        if ($ignoreVpid && isset($diff['vp_id'])) {
+            unset($diff['vp_id']);
+        }
+
+        return $diff;
+
+    }
+
+}

plugins/versionpress/src/Mirror.php
@@ -9,9 +9,6 @@ class Mirror {
     /** @var StorageFactory */
     private $storageFactory;
 
-    /** @var array */
-    private $registeredStorages = array();
-
     /** @var bool */
     private $wasAffected;
 
@@ -31,10 +28,15 @@ class Mirror {
      * @param array $data Data passed to the `Storage::save()` method
      */
     public function save($entityType, $data) {
-        $storage = $this->getStorage($entityType);
-        if ($storage == null)
+        $storage = $this->storageFactory->getStorage($entityType);
+        if ($storage == null) {
             return;
-        $storage->save($data);
+        }
+
+        $changeInfo = $storage->save($data);
+        if ($changeInfo) {
+            $this->changeList[] = $changeInfo;
+        }
     }
 
     /**
@@ -46,10 +48,15 @@ class Mirror {
      * @param array $restriction Restriction passed to the `Storage::delete()` method
      */
     public function delete($entityType, $restriction) {
-        $storage = $this->getStorage($entityType);
-        if ($storage == null)
+        $storage = $this->storageFactory->getStorage($entityType);
+        if ($storage == null) {
             return;
-        $storage->delete($restriction);
+        }
+
+        $changeInfo = $storage->delete($restriction);
+        if ($changeInfo) {
+            $this->changeList[] = $changeInfo;
+        }
     }
 
     /**
@@ -75,37 +82,15 @@ class Mirror {
      *
      * @see Storage::shouldBeSaved()
      *
-     * @param string $entityName Determines the storage
+     * @param string $entityType Determines the storage
      * @param array $data Data passed to Storage::shouldBeSaved()
      * @return bool
      */
-    public function shouldBeSaved($entityName, $data) {
-        $storage = $this->getStorage($entityName);
+    public function shouldBeSaved($entityType, $data) {
+        $storage = $this->storageFactory->getStorage($entityType);
         if($storage === null)
             return false;
         return $storage->shouldBeSaved($data);
-    }
-
-
-    private function getStorage($entityType) {
-
-        $storage = $this->storageFactory->getStorage($entityType);
-        if($storage == null)
-            return null;
-
-        $object_hash = spl_object_hash($storage);
-
-        if ($storage != null && !isset($this->registeredStorages[$object_hash])) {
-            $this->registeredStorages[$object_hash] = true;
-
-            $that = $this;
-            $storage->addChangeListener(function (EntityChangeInfo $changeInfo) use ($that) {
-                $that->wasAffected = true;
-                $that->changeList[] = $changeInfo;
-            });
-        }
-
-        return $storage;
     }
 
 }

plugins/versionpress/src/storages/CommentStorage.php
@@ -6,19 +6,28 @@ class CommentStorage extends DirectoryStorage {
         parent::__construct($directory, 'comment', 'comment_ID');
     }
 
-    protected function createChangeInfo($oldEntity, $newEntity, $changeType) {
+    protected function createChangeInfo($oldEntity, $newEntity, $action = null) {
+
+        if (!$action) {
+
+            $diff = EntityUtils::getDiff($oldEntity, $newEntity);
+
+            if (isset($diff['comment_approved']) && $diff['comment_approved'] === 'trash') {
+                $action = 'trash';
+            } elseif (isset($diff['comment_approved']) && $oldEntity['comment_approved'] === 'trash') {
+                $action = 'untrash';
+            } else {
+                $action = 'edit';
+            }
+        }
+
+        $author = $newEntity["comment_author"];
+
         global $wpdb;
         $result = $wpdb->get_row("SELECT post_title FROM {$wpdb->prefix}posts JOIN {$wpdb->prefix}vp_id ON {$wpdb->prefix}posts.ID = {$wpdb->prefix}vp_id.id WHERE vp_id = UNHEX('$newEntity[vp_comment_post_ID]')");
-        $author = $newEntity["comment_author"];
         $postTitle = $result->post_title;
-        return new CommentChangeInfo($changeType, $newEntity["vp_id"], $author, $postTitle);
+
+        return new CommentChangeInfo($action, $newEntity["vp_id"], $author, $postTitle);
     }
 
-    protected function getEditAction($diff, $oldEntity, $newEntity) {
-        if(isset($diff['comment_approved']) && $diff['comment_approved'] === 'trash')
-            return 'trash';
-        if(isset($diff['comment_approved']) && $oldEntity['comment_approved'] === 'trash')
-            return 'untrash';
-        return parent::getEditAction($diff, $oldEntity, $newEntity);
-    }
 }

plugins/versionpress/src/storages/DirectoryStorage.php
@@ -29,7 +29,40 @@ abstract class DirectoryStorage extends Storage {
     }
 
     function save($data) {
-        $this->saveEntity($data, array($this, 'notifyChangeListeners'));
+        $id = $data['vp_id'];
+
+        if (!$id)
+            return null;
+
+        unset($data[$this->idColumnName]);
+        $data = $this->removeUnwantedColumns($data);
+        $data = $this->applyFilters($data);
+
+        $filename = $this->getEntityFilename($id);
+        $oldSerializedEntity = "";
+        $isExistingEntity = $this->isExistingEntity($id);
+
+        if (!$this->shouldBeSaved($data))
+            return null;
+
+        if ($isExistingEntity) {
+            $oldSerializedEntity = file_get_contents($filename);
+        }
+
+        $oldEntity = $this->deserializeEntity($oldSerializedEntity);
+        $diff = EntityUtils::getDiff($oldEntity, $data);
+
+        if (count($diff) > 0) {
+
+            $newEntity = array_merge($oldEntity, $diff);
+            file_put_contents($filename, $this->serializeEntity($newEntity));
+
+            return $this->createChangeInfo($oldEntity, $newEntity, !$isExistingEntity ? 'create' : null);
+
+        } else {
+            return null;
+        }
+
     }
 
     function delete($restriction) {
@@ -37,7 +70,9 @@ abstract class DirectoryStorage extends Storage {
         if (is_file($fileName)) {
             $entity = $this->loadEntity($restriction['vp_id']);
             unlink($fileName);
-            $this->notifyChangeListeners($entity, $entity, 'delete');
+            return $this->createChangeInfo($entity, $entity, 'delete');
+        } else {
+            return null;
         }
     }
 
@@ -49,7 +84,7 @@ abstract class DirectoryStorage extends Storage {
 
     function saveAll($entities) {
         foreach ($entities as $entity) {
-            $this->saveEntity($entity);
+            $this->save($entity);
         }
     }
 
@@ -102,63 +137,8 @@ abstract class DirectoryStorage extends Storage {
         return $entity;
     }
 
-    protected function notifyChangeListeners($oldEntity, $newEntity, $changeType) {
-        $changeInfo = $this->createChangeInfo($oldEntity, $newEntity, $changeType);
-        $this->callOnChangeListeners($changeInfo);
-    }
-
-    protected abstract function createChangeInfo($oldEntity, $newEntity, $changeType);
-
-    protected function saveEntity($data, $callback = null) {
-        $id = $data['vp_id'];
-
-        if (!$id)
-            return;
-
-        unset($data[$this->idColumnName]);
-        $data = $this->removeUnwantedColumns($data);
-        $data = $this->applyFilters($data);
-
-        $filename = $this->getEntityFilename($id);
-        $oldSerializedEntity = "";
-        $isExistingEntity = $this->isExistingEntity($id);
-
-        if (!$this->shouldBeSaved($data))
-            return;
-
-        if ($isExistingEntity) {
-            $oldSerializedEntity = file_get_contents($filename);
-        }
-
-        $entity = $this->deserializeEntity($oldSerializedEntity);
-        if (isset($entity['vp_id']))
-            unset($data['vp_id']);
-
-        $diff = array();
-        foreach ($data as $key => $value) {
-            if (!isset($entity[$key]) || (isset($entity[$key]) && $entity[$key] != $value)) // not present or different value
-                $diff[$key] = $value;
-        }
-
-
-        if (count($diff) > 0) {
-            $oldEntity = $entity;
-            $newEntity = array_merge($entity, $diff);
-            file_put_contents($filename, $this->serializeEntity($newEntity));
-            if (is_callable($callback))
-                call_user_func($callback, $oldEntity, $newEntity, $isExistingEntity ? $this->getEditAction($diff, $oldEntity, $newEntity) : 'create');
-        }
-    }
-
     protected function isExistingEntity($id) {
         return file_exists($this->getEntityFilename($id));
-    }
-
-    /**
-     * @return string
-     */
-    protected function getEditAction($diff, $oldEntity, $newEntity) {
-        return 'edit';
     }
 
     private function loadEntity($vpid) {

plugins/versionpress/src/storages/OptionsStorage.php
@@ -20,12 +20,7 @@ class OptionsStorage extends SingleFileStorage {
                 || $id === 'recently_edited');
     }
 
-    /**
-     * @param $entity
-     * @param $changeType
-     * @return EntityChangeInfo
-     */
-    protected function createChangeInfo($entity, $changeType) {
-        return new OptionChangeInfo($changeType, $entity[$this->idColumnName]);
+    protected function createChangeInfo($oldEntity, $newEntity, $action = null) {
+        return new OptionChangeInfo($action, $newEntity[$this->idColumnName]);
     }
 }

plugins/versionpress/src/storages/PostMetaStorage.php
@@ -19,22 +19,22 @@ class PostMetaStorage extends DirectoryStorage {
         $this->postMetaKey = $values['meta_key'];
         $this->postMetaVpId = $values['vp_id'];
 
-        $this->saveEntity($data, array($this, 'notifyChangeListeners'));
+        parent::save($data);
     }
 
     function saveAll($entities) {
         foreach ($entities as $entity) {
             $data = $this->transformToPostField($entity);
-            $this->saveEntity($data);
+            parent::save($data);
         }
     }
 
-    protected function createChangeInfo($oldEntity, $newEntity, $changeType) {
+    protected function createChangeInfo($oldEntity, $newEntity, $action = null) {
         $postTitle = $newEntity['post_title'];
         $postType = $newEntity['post_type'];
         $postVpId = $newEntity['vp_id'];
 
-        return new PostMetaChangeInfo($changeType, $this->postMetaVpId, $postType, $postTitle, $postVpId, $this->postMetaKey);
+        return new PostMetaChangeInfo($action, $this->postMetaVpId, $postType, $postTitle, $postVpId, $this->postMetaKey);
     }
 
     private function transformToPostField($values) {

plugins/versionpress/src/storages/PostStorage.php
@@ -7,10 +7,10 @@ class PostStorage extends DirectoryStorage {
         $this->addFilter(new AbsoluteUrlFilter());
     }
 
-    /**
-     * Don't save revisions and drafts
-     */
     public function shouldBeSaved($data) {
+
+        // Don't save revisions and drafts
+
         $id = @$data['vp_id'];
         $isExistingEntity = !empty($id) && $this->isExistingEntity($id);
 
@@ -41,24 +41,29 @@ class PostStorage extends DirectoryStorage {
         return $entity;
     }
 
-    protected function getEditAction($diff, $oldEntity, $newEntity) {
-        if(isset($diff['post_status']) && $diff['post_status'] === 'trash')
-            return 'trash';
-        if(isset($diff['post_status']) && $oldEntity['post_status'] === 'trash')
-            return 'untrash';
-        if(isset($diff['post_status']) && $oldEntity['post_status'] === 'draft' && $newEntity['post_status'] === 'publish')
-            return 'publish';
-        return parent::getEditAction($diff, $oldEntity, $newEntity);
-    }
+    protected function createChangeInfo($oldEntity, $newEntity, $action = null) {
 
-    protected function createChangeInfo($oldEntity, $newEntity, $changeType) {
+        if (!$action) {
+
+            $diff = EntityUtils::getDiff($oldEntity, $newEntity);
+
+            if (isset($diff['post_status']) && $diff['post_status'] === 'trash') {
+                $action = 'trash';
+            } elseif (isset($diff['post_status']) && $oldEntity['post_status'] === 'trash') {
+                $action = 'untrash';
+            } elseif (isset($diff['post_status']) && $oldEntity['post_status'] === 'draft' && $newEntity['post_status'] === 'publish') {
+                $action = 'publish';
+            } elseif (!isset($oldEntity['post_status']) && isset($newEntity['post_status']) && $newEntity['post_status'] === 'draft') {
+                $action = 'draft';
+            } else {
+                $action = 'edit';
+            }
+
+        }
+
         $title = $newEntity['post_title'];
         $type = $newEntity['post_type'];
 
-        if(!isset($oldEntity['post_status']) && isset($newEntity['post_status']) && $newEntity['post_status'] === 'draft') {
-            $changeType = 'draft';
-        }
-
-        return new PostChangeInfo($changeType, $newEntity['vp_id'], $type, $title);
+        return new PostChangeInfo($action, $newEntity['vp_id'], $type, $title);
     }
 }

plugins/versionpress/src/storages/SingleFileStorage.php
@@ -32,47 +32,13 @@ abstract class SingleFileStorage extends Storage {
     }
 
     function save($data) {
-        $this->saveEntity($data, array($this, 'notifyOnChangeListeners'));
-    }
-
-    function delete($restriction) {
-        if (!$this->shouldBeSaved($restriction))
-            return;
-
-        $id = $restriction[$this->idColumnName];
-
-        $this->loadEntities();
-        $originalEntities = $this->entities;
-        $entity = $this->entities[$id];
-        unset($this->entities[$id]);
-        if ($this->entities != $originalEntities) {
-            $this->saveEntities();
-            $this->notifyOnChangeListeners($entity, 'delete');
-        }
-    }
-
-    function loadAll() {
-        $this->loadEntities();
-        return $this->entities;
-    }
-
-    function saveAll($entities) {
-        foreach ($entities as $entity) {
-            $this->saveEntity($entity);
-        }
-    }
-
-    function prepareStorage() {
-    }
-
-    protected function saveEntity($data, $callback = null) {
         if (!$this->shouldBeSaved($data))
-            return;
+            return null;
 
         $id = $data[$this->idColumnName];
 
         if (!$id)
-            return;
+            return null;
 
         $this->loadEntities();
         $isNew = !isset($this->entities[$id]);
@@ -86,10 +52,43 @@ abstract class SingleFileStorage extends Storage {
 
         if ($this->entities != $originalEntities) {
             $this->saveEntities();
-
-            if (is_callable($callback))
-                call_user_func($callback, $this->entities[$id], $isNew ? 'create' : 'edit');
+            return $this->createChangeInfo(null, $this->entities[$id], $isNew ? 'create' : 'edit');
+        } else {
+            return null;
         }
+
+    }
+
+    function delete($restriction) {
+        if (!$this->shouldBeSaved($restriction))
+            return null;
+
+        $id = $restriction[$this->idColumnName];
+
+        $this->loadEntities();
+        $originalEntities = $this->entities;
+        $entity = $this->entities[$id];
+        unset($this->entities[$id]);
+        if ($this->entities != $originalEntities) {
+            $this->saveEntities();
+            return $this->createChangeInfo(null, $entity, 'delete');
+        } else {
+            return null;
+        }
+    }
+
+    function loadAll() {
+        $this->loadEntities();
+        return $this->entities;
+    }
+
+    function saveAll($entities) {
+        foreach ($entities as $entity) {
+            $this->save($entity);
+        }
+    }
+
+    function prepareStorage() {
     }
 
     protected function updateEntity($id, $data) {
@@ -118,11 +117,6 @@ abstract class SingleFileStorage extends Storage {
         file_put_contents($this->file, $entities);
     }
 
-    protected function notifyOnChangeListeners($entity, $changeType) {
-        $changeInfo = $this->createChangeInfo($entity, $changeType);
-        $this->callOnChangeListeners($changeInfo);
-    }
-
     public function shouldBeSaved($data) {
         return true;
     }
@@ -131,10 +125,4 @@ abstract class SingleFileStorage extends Storage {
         return $this->file;
     }
 
-    /**
-     * @param $entity
-     * @param $changeType
-     * @return EntityChangeInfo
-     */
-    protected abstract function createChangeInfo($entity, $changeType);
 }

commit 5a0cdac960f22324a83fb4ff28f795ff03498b46

[#175] Fixing a bug that led to storing entities without their VPIDs. The bug was actually in the EntityUtils::getDiff() helper method which incorrectly removed the `vp_id` column from the returned diff.
Bug was introduced in 4ca317e663aa23191cb307d0c668a8d36382c176.

plugins/versionpress/src/utils/EntityUtils.php
@@ -22,21 +22,16 @@ class EntityUtils {
      *
      * @param array $oldEntityData Usually a full entity data in its original state
      * @param array $newEntityData Full or partial data of the new entity
-     * @param bool $ignoreVpid If true (default), `vp_id` key is ignored (not added to the diff)
      *
      * @return array Key=>value pairs of things that are new or changed in $newEntity (they can
      *               never be "removed", that will never happen when capturing SQL UPDATE actions)
      */
-    public static function getDiff($oldEntityData, $newEntityData, $ignoreVpid = true) {
+    public static function getDiff($oldEntityData, $newEntityData) {
         $diff = array();
         foreach ($newEntityData as $key => $value) {
             if (!isset($oldEntityData[$key]) || $oldEntityData[$key] != $value) {
                 $diff[$key] = $value;
             }
-        }
-
-        if ($ignoreVpid && isset($diff['vp_id'])) {
-            unset($diff['vp_id']);
         }
 
         return $diff;