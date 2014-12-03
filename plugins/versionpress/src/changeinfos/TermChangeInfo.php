7a70d0b28d267c8c97e613502c56eb9484f292fa
Jan Voráček <jan@voracek.net>
2014-12-03 11:43:31 +0100

    [#141] VersionPress now recognizes term renaming

--- plugins/versionpress/src/changeinfos/TermChangeInfo.php
+++ plugins/versionpress/src/changeinfos/TermChangeInfo.php
@@ -5,25 +5,32 @@
  *
  * VP tags:
  *
- *     VP-Action: term/(create|edit|delete)/VPID
+ *     VP-Action: term/(create|edit|rename|delete)/VPID
  *     VP-Term-Name: Uncategorized
  *
  */
 class TermChangeInfo extends EntityChangeInfo {
 
     const TERM_NAME_TAG = "VP-Term-Name";
+    const TERM_OLD_NAME_TAG = "VP-Term-OldName";
 
     /** @var string */
     private $termName;
 
-    public function __construct($action, $entityId, $termName) {
+    /** @var string */
+    private $oldTermName;
+
+    public function __construct($action, $entityId, $termName, $oldTermName = null) {
         parent::__construct("term", $action, $entityId);
         $this->termName = $termName;
+        $this->oldTermName = $oldTermName;
     }
 
     public function getChangeDescription() {
         if ($this->getAction() === "create")
             return "New term '{$this->termName}'";
+        if ($this->getAction() === "rename")
+            return "Term '{$this->oldTermName}' renamed to '{$this->termName}'";
         return "Edited term '{$this->termName}'";
     }
 
@@ -31,14 +38,21 @@ class TermChangeInfo extends EntityChangeInfo {
         $tags = $commitMessage->getVersionPressTags();
         $actionTag = $tags[TrackedChangeInfo::ACTION_TAG];
         list( , $action, $entityId) = explode("/", $actionTag, 3);
-        $nameTag = $tags[self::TERM_NAME_TAG];
-        return new self($action, $entityId, $nameTag);
+        $name = $tags[self::TERM_NAME_TAG];
+        $oldName = isset($tags[self::TERM_OLD_NAME_TAG]) ? $tags[self::TERM_OLD_NAME_TAG] : null;
+        return new self($action, $entityId, $name, $oldName);
     }
 
     protected function getCustomTags() {
-        return array(
+        $tags = array(
             self::TERM_NAME_TAG => $this->termName
         );
+
+        if ($this->getAction() === "rename") {
+            $tags[self::TERM_OLD_NAME_TAG] = $this->oldTermName;
+        }
+
+        return $tags;
     }
 
 }
--- plugins/versionpress/src/storages/TermsStorage.php
+++ plugins/versionpress/src/storages/TermsStorage.php
@@ -3,6 +3,12 @@
 class TermsStorage extends SingleFileStorage {
 
     protected function createChangeInfo($oldEntity, $newEntity, $action = null) {
+        $diff = EntityUtils::getDiff($oldEntity, $newEntity);
+
+        if (isset($diff['name'])) {
+            return new TermChangeInfo('rename', $newEntity['vp_id'], $newEntity['name'], $oldEntity['name']);
+        }
+
         return new TermChangeInfo($action, $newEntity['vp_id'], $newEntity['name']);
     }
 }
