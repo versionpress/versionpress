versionpress/versionpress

commit bd124ce6a1bcdccbb4eb024678ee39bf7548f5eb

[#258] Every commit has now an X-VP-Version tag

plugins/versionpress/admin/index.php
@@ -10,6 +10,7 @@ use VersionPress\Initialization\VersionPressOptions;
 use VersionPress\Utils\JsRedirect;
 use VersionPress\Utils\Markdown;
 use VersionPress\Utils\RequirementsChecker;
+use VersionPress\VersionPress;
 
 require_once(__DIR__ . '/../bootstrap.php');
 
@@ -110,7 +111,7 @@ function _vp_show_progress_message($progressMessage) {
 
     <?php
 
-    if (isset($_GET['init']) && !vp_is_active()) {
+    if (isset($_GET['init']) && !VersionPress::isActive()) {
     ?>
 
         <div class="welcome-panel">
@@ -151,7 +152,7 @@ function _vp_show_progress_message($progressMessage) {
         <?php
 
 
-    } elseif (!vp_is_active()) {
+    } elseif (!VersionPress::isActive()) {
     ?>
 
         <div class="welcome-panel vp-activation-panel">
@@ -358,8 +359,7 @@ function _vp_show_progress_message($progressMessage) {
                 $canUndoCommit = $canUndoCommit && ($commit->getHash() !== $initialCommitHash);
                 $canRollbackToThisCommit = !$isFirstCommit && ($canUndoCommit || $commit->getHash() === $initialCommitHash);
 
-                $changeInfo = ChangeInfoMatcher::createMatchingChangeInfo($commit->getMessage());
-
+                $changeInfo = ChangeInfoMatcher::buildChangeInfo($commit->getMessage());
                 $undoSnippet = "<a href='" . admin_url('admin.php?action=vp_undo&commit=' . $commit->getHash()) . "' style='text-decoration:none; white-space:nowrap;' title='Reverts changes done by this commit'>Undo this</a>";
 
                 $rollbackSnippet = "<a href='" . admin_url('admin.php?action=vp_rollback&commit=' . $commit->getHash()) . "' style='text-decoration:none; white-space:nowrap;' title='Reverts site back to this state; effectively undos all the change up to this commit'>Roll back to this</a>";

plugins/versionpress/versionpress.php
@@ -23,10 +23,11 @@ use VersionPress\Storages\Mirror;
 use VersionPress\Utils\BugReporter;
 use VersionPress\Utils\FileSystem;
 use VersionPress\Utils\IdUtil;
+use VersionPress\VersionPress;
 
 defined('ABSPATH') or die("Direct access not allowed");
 
-if (defined('WP_CLI') && WP_CLI && vp_is_active()) {
+if (defined('WP_CLI') && WP_CLI && VersionPress::isActive()) {
     WP_CLI::add_command('vp', 'VersionPress\Cli\VPCommand');
 }
 
@@ -41,7 +42,7 @@ if (defined('VP_MAINTENANCE')) {
 //----------------------------------------
 
 
-if (vp_is_active()) {
+if (VersionPress::isActive()) {
     vp_register_hooks();
 }
 
@@ -296,7 +297,7 @@ function vp_activate() {
  * @see vp_admin_post_cancel_deactivation()
  */
 function vp_deactivate() {
-    if (defined('WP_CLI') || !vp_is_active()) {
+    if (defined('WP_CLI') || !VersionPress::isActive()) {
         vp_admin_post_confirm_deactivation();
     } else {
         wp_redirect(admin_url('admin.php?page=versionpress/admin/deactivate.php'));
@@ -390,7 +391,7 @@ add_action('admin_notices', 'vp_activation_nag', 4 /* WP update nag is 3, we are
  */
 function vp_activation_nag() {
 
-    if (vp_is_active() ||
+    if (VersionPress::isActive() ||
         get_current_screen()->id == "versionpress/admin/index" ||
         get_current_screen()->id == "versionpress/admin/deactivate"
     ) {
@@ -415,7 +416,7 @@ add_filter('wp_insert_post_data', 'vp_generate_post_guid', '99', 2);
  * @return array
  */
 function vp_generate_post_guid($data, $postarr) {
-    if (!vp_is_active()) return $data;
+    if (!VersionPress::isActive()) return $data;
 
     if (empty($postarr['ID'])) { // it's insert not update
         $data['guid'] = IdUtil::newUuid();
@@ -484,7 +485,7 @@ function _vp_revert($reverterMethod) {
     }
 }
 
-if (vp_is_active()) {
+if (VersionPress::isActive()) {
     add_action('admin_bar_menu', 'vp_admin_bar_warning');
 }
 
@@ -497,7 +498,7 @@ function vp_admin_bar_warning(WP_Admin_Bar $adminBar) {
 
     $adminBarText = "<span style=\"color:#FF8800;font-weight:bold\">VersionPress EAP running</span>";
     $popoverTitle = "Note";
-    $popoverText = "<p style='margin-top: 5px;'>You are running <strong>VersionPress " . vp_get_version() . "</strong> which is an <strong style='font-size: 1.15em;'>EAP release</strong>. Please understand that EAP releases are early versions of the software and as such might not fully support certain workflows, 3<sup>rd</sup> party plugins, hosts etc.<br /><br /><strong>We recommend that you keep a safe backup of the site at all times</strong></p>";
+    $popoverText = "<p style='margin-top: 5px;'>You are running <strong>VersionPress " . VersionPress::getVersion() . "</strong> which is an <strong style='font-size: 1.15em;'>EAP release</strong>. Please understand that EAP releases are early versions of the software and as such might not fully support certain workflows, 3<sup>rd</sup> party plugins, hosts etc.<br /><br /><strong>We recommend that you keep a safe backup of the site at all times</strong></p>";
     $popoverText .= "<p><a href='http://docs.versionpress.net/en/release-notes' target='_blank'>Learn more about VersionPress releases</a></p>";
 
     $adminBar->add_node(array(
@@ -545,31 +546,3 @@ function vp_enable_maintenance() {
 function vp_disable_maintenance() {
     FileSystem::remove(ABSPATH . '/.maintenance');
 }
-
-//----------------------------------
-// Public functions
-//----------------------------------
-
-/**
- * Returns true if VersionPress is active. Note that active != activated and being
- * active means that VersionPress is tracking changes.
- *
- * @return bool
- */
-function vp_is_active() {
-    return defined('VERSIONPRESS_ACTIVATION_FILE') && file_exists(VERSIONPRESS_ACTIVATION_FILE);
-}
-
-/**
- * Returns VersionPress version as specified in plugin metadata
- *
- * @return string
- */
-function vp_get_version() {
-    if (!function_exists('get_plugin_data')) {
-        require_once(ABSPATH . 'wp-admin/includes/plugin.php');
-    }
-    $pluginData = get_plugin_data(VERSIONPRESS_PLUGIN_DIR . "/versionpress.php", false, false);
-    return $pluginData['Version'];
-}
-

plugins/versionpress/src/ChangeInfos/ChangeInfoEnvelope.php
@@ -3,11 +3,18 @@ namespace VersionPress\ChangeInfos;
 
 use VersionPress\Git\CommitMessage;
 use VersionPress\Utils\ArrayUtils;
+use VersionPress\VersionPress;
 
 /**
  * Class representing more changes in one commit
  */
-class CompositeChangeInfo implements ChangeInfo {
+class ChangeInfoEnvelope implements ChangeInfo {
+
+    /**
+     * VP meta tag that says the version of VersionPress in which was the commit made.
+     * It's parsed into {@link version} field by the {@link buildFromCommitMessage} method.
+     */
+    const VP_VERSION_TAG = "X-VP-Version";
 
     /** @var TrackedChangeInfo[] */
     private $changeInfoList;
@@ -33,11 +40,15 @@ class CompositeChangeInfo implements ChangeInfo {
         "VersionPress\ChangeInfos\UserMetaChangeInfo",
     );
 
+    private $version;
+
     /**
      * @param TrackedChangeInfo[] $changeInfoList
+     * @param string|null $version
      */
-    public function __construct($changeInfoList) {
+    public function __construct($changeInfoList, $version = null) {
         $this->changeInfoList = $changeInfoList;
+        $this->version = $version === null ? VersionPress::getVersion() : $version;
     }
 
     /**
@@ -55,6 +66,7 @@ class CompositeChangeInfo implements ChangeInfo {
         }
 
         $body = join("\n\n", $bodies);
+        $body .= sprintf("\n\n%s: %s", self::VP_VERSION_TAG, $this->version);
 
         return new CommitMessage($subject, $body);
     }
@@ -82,18 +94,27 @@ class CompositeChangeInfo implements ChangeInfo {
     public static function buildFromCommitMessage(CommitMessage $commitMessage) {
         $fullBody = $commitMessage->getBody();
         $splittedBodies = explode("\n\n", $fullBody);
+        $lastBody = $splittedBodies[count($splittedBodies) - 1];
         $changeInfoList = array();
+        $version = null;
+
+        if (self::containsVersion($lastBody)) {
+            $version = self::extractVersion($lastBody);
+            array_pop($splittedBodies);
+        }
 
         foreach ($splittedBodies as $body) {
             $partialCommitMessage = new CommitMessage("", $body);
-            $changeInfoList[] = ChangeInfoMatcher::createMatchingChangeInfo($partialCommitMessage);
+            /** @var ChangeInfo $matchingChangeInfoType */
+            $matchingChangeInfoType = ChangeInfoMatcher::findMatchingChangeInfo($partialCommitMessage);
+            $changeInfoList[] = $matchingChangeInfoType::buildFromCommitMessage($partialCommitMessage);
         }
 
-        return new self($changeInfoList);
+        return new self($changeInfoList, $version);
     }
 
     /**
-     * Returns all ChangeInfo objects encapsulated in CompositeChangeInfo.
+     * Returns all ChangeInfo objects encapsulated in ChangeInfoEnvelope.
      *
      * @return TrackedChangeInfo[]
      */
@@ -174,5 +195,15 @@ class CompositeChangeInfo implements ChangeInfo {
         }
 
         return 0;
+    }
+
+    private static function containsVersion($lastBody) {
+        return \NStrings::startsWith($lastBody, self::VP_VERSION_TAG);
+    }
+
+    private static function extractVersion($lastBody) {
+        $tmpMessage = new CommitMessage("", $lastBody);
+        $version = $tmpMessage->getVersionPressTag(self::VP_VERSION_TAG);
+        return $version;
     }
 }

plugins/versionpress/src/ChangeInfos/ChangeInfoMatcher.php
@@ -40,18 +40,17 @@ class ChangeInfoMatcher {
     );
 
     /**
-     * For a given commit message, finds the matching ChangeInfo class and creates
-     * an instance of it. Matching is done based on the value of the VP-Action tag.
+     * For a given commit message, creates ChangeInfoEnvelope containing all ChangeInfo.
      *
      * @param CommitMessage $commitMessage
-     * @return ChangeInfo
+     * @return ChangeInfoEnvelope|UntrackedChangeInfo
      */
-    public static function createMatchingChangeInfo(CommitMessage $commitMessage) {
+    public static function buildChangeInfo(CommitMessage $commitMessage) {
+        if (self::findMatchingChangeInfo($commitMessage) === 'VersionPress\ChangeInfos\UntrackedChangeInfo') {
+            return UntrackedChangeInfo::buildFromCommitMessage($commitMessage);
+        }
 
-        /** @var ChangeInfo $matchingChangeInfoType */
-        $matchingChangeInfoType = self::findMatchingChangeInfo($commitMessage);
-        return $matchingChangeInfoType::buildFromCommitMessage($commitMessage);
-
+        return ChangeInfoEnvelope::buildFromCommitMessage($commitMessage);
     }
 
     /**
@@ -64,7 +63,7 @@ class ChangeInfoMatcher {
     public static function findMatchingChangeInfo(CommitMessage $commitMessage) {
 
         if (substr_count($commitMessage->getBody(), TrackedChangeInfo::ACTION_TAG) > 1) {
-            return "VersionPress\ChangeInfos\CompositeChangeInfo";
+            return "VersionPress\ChangeInfos\ChangeInfoEnvelope";
         }
 
         $actionTagValue = $commitMessage->getVersionPressTag(TrackedChangeInfo::ACTION_TAG); // can be empty string which is not a problem
@@ -78,8 +77,6 @@ class ChangeInfoMatcher {
 
         // Code execution should never reach this point, at least the 'UntrackedChangeInfo' should match
         throw new Exception("Matching ChangeInfo type not found");
-
-
     }
 
     /**

plugins/versionpress/src/ChangeInfos/RevertChangeInfo.php
@@ -55,7 +55,7 @@ class RevertChangeInfo extends TrackedChangeInfo {
         $revertedCommit = $repository->getCommit($this->commitHash);
 
         if ($this->action === self::ACTION_UNDO) {
-            $revertedChangeInfo = ChangeInfoMatcher::createMatchingChangeInfo($revertedCommit->getMessage());
+            $revertedChangeInfo = ChangeInfoMatcher::buildChangeInfo($revertedCommit->getMessage());
             return sprintf("Reverted change \"%s\"", $revertedChangeInfo->getChangeDescription());
         }
 

plugins/versionpress/src/Git/CommitMessage.php
@@ -73,7 +73,7 @@ class CommitMessage {
             $tagLines = array_filter(
                 array_map("trim", explode("\n", $this->getBody())),
                 function ($line) {
-                    return NStrings::startsWith($line, "VP-");
+                    return NStrings::startsWith($line, "VP-") || NStrings::startsWith($line, "X-VP-");
                 }
             );
             $tags = array();

plugins/versionpress/src/Git/Reverter.php
@@ -5,9 +5,10 @@ namespace VersionPress\Git;
 use Committer;
 use NStrings;
 use VersionPress\ChangeInfos\ChangeInfoMatcher;
-use VersionPress\ChangeInfos\CompositeChangeInfo;
+use VersionPress\ChangeInfos\ChangeInfoEnvelope;
 use VersionPress\ChangeInfos\EntityChangeInfo;
 use VersionPress\ChangeInfos\RevertChangeInfo;
+use VersionPress\ChangeInfos\UntrackedChangeInfo;
 use VersionPress\Database\DbSchemaInfo;
 use VersionPress\Storages\StorageFactory;
 use VersionPress\Synchronizers\SynchronizationProcess;
@@ -117,16 +118,15 @@ class Reverter {
     }
 
     private function checkReferencesForRevertedCommit(Commit $revertedCommit) {
-        $changeInfo = ChangeInfoMatcher::createMatchingChangeInfo($revertedCommit->getMessage());
-        if ($changeInfo instanceof EntityChangeInfo) {
-            return $this->checkEntityReferences($changeInfo);
+        $changeInfo = ChangeInfoMatcher::buildChangeInfo($revertedCommit->getMessage());
+
+        if ($changeInfo instanceof UntrackedChangeInfo) {
+            return true;
         }
 
-        if ($changeInfo instanceof CompositeChangeInfo) {
-            foreach ($changeInfo->getChangeInfoList() as $subChangeInfo) {
-                if ($subChangeInfo instanceof EntityChangeInfo && !$this->checkEntityReferences($subChangeInfo)) {
-                    return false;
-                }
+        foreach ($changeInfo->getChangeInfoList() as $subChangeInfo) {
+            if ($subChangeInfo instanceof EntityChangeInfo && !$this->checkEntityReferences($subChangeInfo)) {
+                return false;
             }
         }
 

plugins/versionpress/src/Initialization/Initializer.php
@@ -8,6 +8,7 @@ use VersionPress\Git\GitRepository;
 use VersionPress\Storages\StorageFactory;
 use VersionPress\Utils\FileSystem;
 use VersionPress\Utils\IdUtil;
+use VersionPress\VersionPress;
 use wpdb;
 
 /**
@@ -418,7 +419,7 @@ class Initializer {
 
 
         $this->reportProgressChange(InitializerStates::CREATING_INITIAL_COMMIT);
-        $installationChangeInfo = new VersionPressChangeInfo("activate", vp_get_version());
+        $installationChangeInfo = new VersionPressChangeInfo("activate", VersionPress::getVersion());
 
         $currentUser = wp_get_current_user();
         /** @noinspection PhpUndefinedFieldInspection */

commit 0edb6fb42f667ce4873cf6098ca7e909a563641a

[#258] Fixed bug introduced in bd124ce6a1bcdccbb4eb024678ee39bf7548f5eb. The bootstrap.php has to be required from versionpress.php if VersionPress is not yet fully activated.

plugins/versionpress/admin/index.php
@@ -12,8 +12,6 @@ use VersionPress\Utils\Markdown;
 use VersionPress\Utils\RequirementsChecker;
 use VersionPress\VersionPress;
 
-require_once(__DIR__ . '/../bootstrap.php');
-
 wp_enqueue_style('versionpress_admin_style', plugins_url( 'css/style.css' , __FILE__ ));
 wp_enqueue_style('versionpress_admin_icons', plugins_url( 'icons/style.css' , __FILE__ ));
 

plugins/versionpress/versionpress.php
@@ -27,12 +27,13 @@ use VersionPress\VersionPress;
 
 defined('ABSPATH') or die("Direct access not allowed");
 
+require_once(__DIR__ . '/bootstrap.php');
+
 if (defined('WP_CLI') && WP_CLI && VersionPress::isActive()) {
     WP_CLI::add_command('vp', 'VersionPress\Cli\VPCommand');
 }
 
 if (defined('VP_MAINTENANCE')) {
-    require_once(__DIR__ . '/bootstrap.php');
     NDebugger::log("Ooops. Something went wrong :( The .maintenance file was not deleted.");
     vp_disable_maintenance();
 }