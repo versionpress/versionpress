cc8b6b2afda1c86e80db971e0afc1956331ea483
Borek Bernard <borekb@gmail.com>
2015-02-04 16:52:58 +0100

    [#176] Many more tests for IniSerializer. The doubleQuoteEscapingAtTheEOL test uncovered an issue in PHP's ini library, this needs further inspecting / fixing.

--- plugins/versionpress/tests/unit/IniSerializerTest.php
+++ plugins/versionpress/tests/unit/IniSerializerTest.php
@@ -4,8 +4,16 @@
 use VersionPress\Utils\IniSerializer;
 use VersionPress\Utils\StringUtils;
 
+/**
+ * Tests covering IniSerializer. There is also {@link IniSerializerIssuesTest} which contains
+ * tests cases for reported issues.
+ */
 class IniSerializerTest extends PHPUnit_Framework_TestCase {
 
+
+    //--------------------------------
+    // Invalid inputs
+    //--------------------------------
 
     /**
      * @test
@@ -22,6 +30,13 @@ class IniSerializerTest extends PHPUnit_Framework_TestCase {
         $this->setExpectedException('Exception');
         IniSerializer::serializeSectionedData(array("Section" => array()));
     }
+
+
+
+    //--------------------------------
+    // The most basic test
+    //--------------------------------
+
 
     /**
      * Simplest possible sectioned INI - anything less that this throws. A couple of required elements can be seen here:
@@ -47,6 +62,12 @@ INI
         $this->assertEquals($data, IniSerializer::deserialize($ini));
 
     }
+
+
+    //--------------------------------
+    // Supported types
+    //--------------------------------
+
 
     /**
      * @test
@@ -110,7 +131,199 @@ INI
     /**
      * @test
      */
-    public function subsection() {
+    public function arraysAsSquareBrackets() {
+
+        $data = array("Section" => array("key" => array("val1", "val2")));
+        $ini = StringUtils::crlfize(<<<'INI'
+[Section]
+key[0] = "val1"
+key[1] = "val2"
+
+INI
+        );
+
+        $this->assertEquals($ini, IniSerializer::serializeSectionedData($data));
+        $this->assertEquals($data, IniSerializer::deserialize($ini));
+
+    }
+
+
+    //--------------------------------
+    // Escaping / special characters
+    //--------------------------------
+
+    /**
+     * @test
+     */
+    public function doubleQuoteEscaping() {
+
+        $data = array("Section" => array("key1" => "\"Hello\""));
+        $ini = StringUtils::crlfize(<<<'INI'
+[Section]
+key1 = "\"Hello\""
+
+INI
+        );
+
+        $this->assertEquals($ini, IniSerializer::serializeSectionedData($data));
+        $this->assertEquals($data, IniSerializer::deserialize($ini));
+
+    }
+
+    /**
+     * @test
+     */
+    public function doubleQuoteEscapingAtTheEOL() {
+
+        $data = array("Section" => array("key1" => "\"\r\nwhatever\""));
+        $ini = StringUtils::crlfize(<<<'INI'
+[Section]
+key1 = "\"
+whatever\""
+
+INI
+        );
+
+        $this->assertEquals($ini, IniSerializer::serializeSectionedData($data));
+        $this->assertEquals($data, IniSerializer::deserialize($ini));
+
+    }
+
+    /**
+     * @test
+     */
+    public function doubleQuoteEscaping_HereDoc() {
+
+        // Note: double quotes in heredoc MUST NOT be escaped with "\", although
+        // PHP manual states that it optionally might be
+        $data = array("Section" => array("key1" => <<<VAL
+"Hello"
+VAL
+        ));
+
+        $ini = StringUtils::crlfize(<<<'INI'
+[Section]
+key1 = "\"Hello\""
+
+INI
+        );
+
+        $this->assertEquals($ini, IniSerializer::serializeSectionedData($data));
+        $this->assertEquals($data, IniSerializer::deserialize($ini));
+
+    }
+
+    /**
+     * @test
+     */
+    public function hashSignCommentInsideQuotes() {
+
+        $data = array("Section" => array("key1" => StringUtils::crlfize(<<<VAL
+First line of the value
+# Continued value - should not be treated as comment
+VAL
+        )));
+
+        $ini = StringUtils::crlfize(<<<'INI'
+[Section]
+key1 = "First line of the value
+# Continued value - should not be treated as comment"
+
+INI
+        );
+
+        $this->assertEquals($ini, IniSerializer::serializeSectionedData($data));
+        $this->assertEquals($data, IniSerializer::deserialize($ini));
+
+    }
+
+    /**
+     * @test
+     */
+    public function semicolonCommentInsideQuotes() {
+
+        $data = array("Section" => array("key1" => StringUtils::crlfize(<<<VAL
+First line of the value
+; Continued value - should not be treated as comment
+VAL
+        )));
+
+        $ini = StringUtils::crlfize(<<<'INI'
+[Section]
+key1 = "First line of the value
+; Continued value - should not be treated as comment"
+
+INI
+        );
+
+        $this->assertEquals($ini, IniSerializer::serializeSectionedData($data));
+        $this->assertEquals($data, IniSerializer::deserialize($ini));
+
+    }
+
+
+    /**
+     * @test
+     */
+    public function newLineHandlingInsideValues_LF() {
+
+        $data = array("Section" => array("key1" => "Hello\nWorld"));
+        $ini = "[Section]\r\nkey1 = \"Hello\nWorld\"\r\n";
+
+        $this->assertEquals($ini, IniSerializer::serializeSectionedData($data));
+        $this->assertEquals($data, IniSerializer::deserialize($ini));
+
+    }
+
+    /**
+     * @test
+     */
+    public function newLineHandlingInsideValues_CR() {
+
+        $data = array("Section" => array("key1" => "Hello\rWorld"));
+        $ini = "[Section]\r\nkey1 = \"Hello\rWorld\"\r\n";
+
+        $this->assertEquals($ini, IniSerializer::serializeSectionedData($data));
+        $this->assertEquals($data, IniSerializer::deserialize($ini));
+
+    }
+
+    /**
+     * @test
+     */
+    public function newLineHandlingInsideValues_CRLF() {
+
+        $data = array("Section" => array("key1" => "Hello\r\nWorld"));
+        $ini = "[Section]\r\nkey1 = \"Hello\r\nWorld\"\r\n";
+
+        $this->assertEquals($ini, IniSerializer::serializeSectionedData($data));
+        $this->assertEquals($data, IniSerializer::deserialize($ini));
+
+    }
+
+    /**
+     * @test
+     */
+    public function newLineHandling_BlankLines() {
+
+        $data = array("Section" => array("key1" => "\r\n"));
+        $ini = "[Section]\r\nkey1 = \"\r\n\"\r\n";
+
+        $this->assertEquals($ini, IniSerializer::serializeSectionedData($data));
+        $this->assertEquals($data, IniSerializer::deserialize($ini));
+
+    }
+
+
+
+    //--------------------------------
+    // Subsections
+    //--------------------------------
+
+    /**
+     * @test
+     */
+    public function singleSubsection() {
 
         $data = array("Section" => array("Subsection" => array("key" => "value")));
         $ini = StringUtils::crlfize(<<<'INI'
@@ -124,6 +337,141 @@ INI
         $this->assertEquals($data, IniSerializer::deserialize($ini));
 
     }
+
+    /**
+     * @test
+     */
+    public function deepSubsections() {
+
+        $data = array(
+            "Section" => array(
+                "Subsection" => array(
+                    "SubSubsection" => array(
+                        "SubSubSubsection" => array("key" => "value")
+                    )
+                )
+            )
+        );
+        $ini = StringUtils::crlfize(<<<'INI'
+[Section.Subsection.SubSubsection.SubSubSubsection]
+key = "value"
+
+INI
+        );
+
+        $this->assertEquals($ini, IniSerializer::serializeSectionedData($data));
+        $this->assertEquals($data, IniSerializer::deserialize($ini));
+
+    }
+
+    /**
+     * @test
+     */
+    public function deepSubsectionsMixedWithData() {
+
+        $data = array(
+            "Section" => array(
+                "key" => "value",
+                "Subsection" => array(
+                    "SubSubsection" => array(
+                        "key" => "value",
+                        "SubSubSubsection" => array("key" => "value")
+                    )
+                )
+            )
+        );
+        $ini = StringUtils::crlfize(<<<'INI'
+[Section]
+key = "value"
+[Section.Subsection.SubSubsection]
+key = "value"
+[Section.Subsection.SubSubsection.SubSubSubsection]
+key = "value"
+
+INI
+        );
+
+        $this->assertEquals($ini, IniSerializer::serializeSectionedData($data));
+        $this->assertEquals($data, IniSerializer::deserialize($ini));
+
+    }
+
+    /**
+     * @test
+     */
+    public function twoSubsections() {
+
+        $data = array("Section" => array(
+            "Subsection1" => array("key" => "value"),
+            "Subsection2" => array("key" => "value")
+        ));
+
+        $ini = StringUtils::crlfize(<<<'INI'
+[Section.Subsection1]
+key = "value"
+
+[Section.Subsection2]
+key = "value"
+
+INI
+        );
+
+        $this->assertEquals($ini, IniSerializer::serializeSectionedData($data));
+        $this->assertEquals($data, IniSerializer::deserialize($ini));
+
+    }
+
+    /**
+     * @test
+     */
+    public function subsectionMixedWithData() {
+
+        $data = array("Section" => array(
+            "key" => "value",
+            "Subsection" => array("key" => "value")
+        ));
+
+        $ini = StringUtils::crlfize(<<<'INI'
+[Section]
+key = "value"
+[Section.Subsection]
+key = "value"
+
+INI
+        );
+
+        $this->assertEquals($ini, IniSerializer::serializeSectionedData($data));
+        $this->assertEquals($data, IniSerializer::deserialize($ini));
+
+    }
+
+    /**
+     * @test
+     */
+    public function subsectionMixedWithDataInWrongOrder() {
+
+        // "Wrong" order - key-value must appear before Subsection so that it doesn't belong
+        // to the subsection in the INI format
+        $data = array("Section" => array(
+            "Subsection" => array("key" => "value"),
+            "key" => "value"
+        ));
+
+        $ini = StringUtils::crlfize(<<<'INI'
+[Section]
+key = "value"
+[Section.Subsection]
+key = "value"
+
+INI
+        );
+
+        $this->assertEquals($ini, IniSerializer::serializeSectionedData($data));
+        $this->assertEquals($data, IniSerializer::deserialize($ini));
+
+    }
+
+
 
     /**
      * @test
