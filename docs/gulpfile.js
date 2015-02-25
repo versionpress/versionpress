b23672191f679c9286e545b465482ce84db5c846
Borek Bernard <borekb@gmail.com>
2015-02-25 11:17:50 +0100

    Updated 'gulp watch' to automatically start the ASP.NET website

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -5,6 +5,7 @@ var browserSync = require('browser-sync');
 var gutil = require('gulp-util');
 var watch = require('gulp-watch');
 var runSequence = require('run-sequence');
+var run = require('gulp-run');
 
 var source = "./content/**";
 var destination = "../VersionPress-docssite/VersionPress.DocsSite/App_Data/content";
@@ -22,23 +23,40 @@ gulp.task('default', function (cb) {
 
 
 gulp.task('clean', function(cb) {
-  del(destination, {force: true}, cb);
+    del(destination, {force: true}, cb);
 });
 
 gulp.task('copy-docs', ['clean'], function() {
-  return gulp.src(source, { dot: true }).pipe(gulp.dest(destination));
+    return gulp.src(source, { dot: true }).pipe(gulp.dest(destination));
 });
 
-gulp.task('watch', ['browser-sync'], function() {
-  watch(source, function() {
-      runSequence('copy-docs', browserSync.reload);
-  });
+gulp.task('watch', ['start-site', 'browser-sync'], function() {
+    watch(source, function() {
+        runSequence('copy-docs', browserSync.reload);
+    });
 });
 
-gulp.task('browser-sync', function() {
-  browserSync({
-      proxy: 'http://localhost:1515',
-      port: 1516,
-      notify: false
-  });
+gulp.task('start-site', function(cb) {
+
+    // There were issues with running the command directly from here (https://github.com/cbarrick/gulp-run/issues/23)
+    // so this helper batch file has been created.
+    var command = "start-site.cmd";
+
+    var cmd = new run.Command(command);
+    cmd.exec(function(err) {
+        if (err) {
+            gutil.log(err);
+        }
+        cb();
+    });
+
+
+});
+
+gulp.task('browser-sync', ['start-site'], function() {
+    browserSync({
+        proxy: 'http://localhost:1515',
+        port: 1516,
+        notify: false
+    });
 });
