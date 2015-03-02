a85cd4a6b62498461d121edc53a350f474b1094c
Jan Voráček <jan@voracek.net>
2015-03-02 18:27:09 +0100

    [#189] RevertTests transformed to new End2End tests

---
+++ plugins/versionpress/tests/End2End/Revert/RevertTest.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace VersionPress\Tests\End2End\Revert;
+
+use VersionPress\Tests\End2End\Utils\End2EndTestCase;
+use VersionPress\Tests\Utils\CommitAsserter;
+
+class RevertTest extends End2EndTestCase {
+
+    /** @var IRevertTestWorker */
+    private static $worker;
+
+    /**
+     * @test
+     * @testdox Undo reverts changes in given commit
+     */
+    public function undoRevertChangesInGivenCommit() {
+        self::$worker->prepare_undoLastCommit();
+
+        $commitAsserter = new CommitAsserter($this->gitRepository);
+
+        self::$worker->undoLastCommit();
+
+        $commitAsserter->assertNumCommits(1);
+        $commitAsserter->assertCommitAction('versionpress/undo');
+        $commitAsserter->assertCountOfAffectedFiles(1);
+        $commitAsserter->assertCommitPath('D', '%vpdb%/posts/*');
+        $commitAsserter->assertCleanWorkingDirectory();
+    }
+
+    /**
+     * @test
+     * @testdox Undo reverts only one commit
+     */
+    public function undoRevertsOnlyOneCommit() {
+        self::$worker->prepare_undoSecondCommit();
+        $commitAsserter = new CommitAsserter($this->gitRepository);
+
+        self::$worker->undoSecondCommit();
+
+        $commitAsserter->assertNumCommits(1);
+        $commitAsserter->assertCommitAction('versionpress/undo');
+        $commitAsserter->assertCountOfAffectedFiles(1);
+        $commitAsserter->assertCommitPath('M', '%vpdb%/options.ini');
+        $commitAsserter->assertCleanWorkingDirectory();
+    }
+
+    /**
+     * @test
+     * @testdox Undo commit can be also reverted.
+     */
+    public function undoCommitCanBeAlsoReverted() {
+        self::$worker->prepare_undoRevertedCommit();
+
+        $commitAsserter = new CommitAsserter($this->gitRepository);
+
+        self::$worker->undoLastCommit();
+        self::$worker->undoLastCommit();
+
+        $commitAsserter->assertNumCommits(2);
+        $commitAsserter->assertCommitAction('versionpress/undo');
+        $commitAsserter->assertCountOfAffectedFiles(1);
+        $commitAsserter->assertCommitPath('A', '%vpdb%/posts/*');
+        $commitAsserter->assertCleanWorkingDirectory();
+    }
+
+    /**
+     * @test
+     * @testdox Comment deletion cannot be reverted if the commented post no longer exists
+     */
+    public function entityWithMissingReferenceCannotBeRestoredWithRevert() {
+        self::$worker->prepare_tryRestoreEntityWithMissingReference();
+
+        $commitAsserter = new CommitAsserter($this->gitRepository);
+
+        self::$worker->tryRestoreEntityWithMissingReference();
+        $commitAsserter->assertNumCommits(0);
+        $commitAsserter->assertCleanWorkingDirectory();
+
+    }
+
+    /**
+     * @test
+     * @testdox Rollback reverts all changes made after chosen commit
+     */
+    public function rollbackRevertsAllChangesMadeAfterChosenCommit() {
+        self::$worker->prepare_rollbackMoreChanges();
+
+        $commitAsserter = new CommitAsserter($this->gitRepository);
+
+        self::$worker->rollbackMoreChanges();
+        $commitAsserter->assertNumCommits(1);
+        $commitAsserter->assertCommitAction('versionpress/rollback');
+        $commitAsserter->assertCountOfAffectedFiles(3);
+        $commitAsserter->assertCommitPath('D', '%vpdb%/posts/*');
+        $commitAsserter->assertCommitPath('D', '%vpdb%/comments/*');
+        $commitAsserter->assertCommitPath('M', '%vpdb%/options.ini');
+        $commitAsserter->assertCleanWorkingDirectory();
+    }
+
+    /**
+     * @test
+     * @testdox Clicking on Cancel only hides the popup
+     */
+    public function clickingOnCancelOnlyHidesThePopup() {
+        self::$worker->prepare_clickOnCancel();
+
+        $commitAsserter = new CommitAsserter($this->gitRepository);
+
+        self::$worker->clickOnCancel();
+
+        $commitAsserter->assertNumCommits(0);
+        $commitAsserter->assertCleanWorkingDirectory();
+    }
+
+    /**
+     * @test
+     * @testdox OK button is disabled if the working directory is not clean
+     */
+    public function okButtonIsDisabledIfTheWorkingDirectoryIsNotClean() {
+        self::$worker->prepare_undoWithNotCleanWorkingDirectory();
+
+        $commitAsserter = new CommitAsserter($this->gitRepository);
+        touch(self::$testConfig->testSite->path . '/revert-test-file');
+
+        self::$worker->undoLastCommit();
+
+        $commitAsserter->assertNumCommits(0);
+        unlink(self::$testConfig->testSite->path . '/revert-test-file');
+        $commitAsserter->assertCleanWorkingDirectory();
+    }
+}
---
+++ plugins/versionpress/tests/End2End/Revert/RevertTestSeleniumWorker.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace VersionPress\Tests\End2End\Revert;
+
+use VersionPress\Tests\End2End\Utils\SeleniumWorker;
+
+class RevertTestSeleniumWorker extends SeleniumWorker implements IRevertTestWorker {
+
+    public function prepare_undoLastCommit() {
+        $this->loginIfNecessary();
+        $this->createTestPost();
+        $this->url('wp-admin/admin.php?page=versionpress/admin/index.php');
+    }
+
+    public function undoLastCommit() {
+        $this->_undoLastCommit();
+    }
+
+    public function prepare_undoSecondCommit() {
+        $this->loginIfNecessary();
+        self::$wpAutomation->editOption('blogname', 'Blogname for undo test');
+        $this->createTestPost();
+        $this->url('wp-admin/admin.php?page=versionpress/admin/index.php');
+    }
+
+    public function undoSecondCommit() {
+        $this->undoNthCommit(2);
+    }
+
+    public function prepare_undoRevertedCommit() {
+        $this->loginIfNecessary();
+        $this->createTestPost();
+        $this->url('wp-admin/admin.php?page=versionpress/admin/index.php');
+    }
+
+    public function prepare_tryRestoreEntityWithMissingReference() {
+        $this->loginIfNecessary();
+        $postId = $this->createTestPost();
+        $commentId = $this->createCommentForPost($postId);
+        self::$wpAutomation->deleteComment($commentId);
+        self::$wpAutomation->deletePost($postId);
+        $this->url('wp-admin/admin.php?page=versionpress/admin/index.php');
+    }
+
+    public function tryRestoreEntityWithMissingReference() {
+        $this->undoNthCommit(2);
+
+    }
+
+    public function prepare_rollbackMoreChanges() {
+        $this->loginIfNecessary();
+        $postId = $this->createTestPost();
+        $this->createCommentForPost($postId);
+        self::$wpAutomation->editOption('blogname', 'Blogname for rollback test');
+        $this->url('wp-admin/admin.php?page=versionpress/admin/index.php');
+    }
+
+    public function rollbackMoreChanges() {
+        $this->rollbackToNthCommit(4);
+    }
+
+    public function prepare_clickOnCancel() {
+        $this->loginIfNecessary();
+        $this->createTestPost();
+        $this->url('wp-admin/admin.php?page=versionpress/admin/index.php');
+    }
+
+    public function clickOnCancel() {
+        $this->jsClick("#versionpress-commits-table tr:nth-child(1) a[href*=vp_undo]");
+        $this->waitForAjax();
+        $this->jsClick("#popover-cancel-button");
+        $this->waitForAjax(); // there shouldn't be any AJAX request, but for sure...
+    }
+
+    public function prepare_undoWithNotCleanWorkingDirectory() {
+        $this->loginIfNecessary();
+        $this->createTestPost();
+        $this->url('wp-admin/admin.php?page=versionpress/admin/index.php');
+    }
+
+    //---------------------
+    // Helper methods
+    //---------------------
+
+    private function createTestPost() {
+        $post = array(
+            "post_type" => "post",
+            "post_status" => "publish",
+            "post_title" => "Test post for revert",
+            "post_date" => "2011-11-11 11:11:11",
+            "post_content" => "Test post",
+            "post_author" => 1
+        );
+
+        return self::$wpAutomation->createPost($post);
+    }
+
+    private function createCommentForPost($postId) {
+        $comment = array(
+            "comment_author" => "Mr VersionPress",
+            "comment_author_email" => "versionpress@example.com",
+            "comment_author_url" => "https://wordpress.org/",
+            "comment_date" => "2012-12-12 12:12:12",
+            "comment_content" => "Have you heard about VersionPress? It's new awesome version control plugin for WordPress.",
+            "comment_approved" => 1,
+            "comment_post_ID" => $postId,
+        );
+
+        return self::$wpAutomation->createComment($comment);
+    }
+
+    private function _undoLastCommit() {
+        $this->undoNthCommit(1);
+    }
+
+    private function undoNthCommit($whichCommit) {
+        $this->jsClick("#versionpress-commits-table tr:nth-child($whichCommit) a[href*=vp_undo]");
+        $this->waitForAjax();
+        $this->jsClick("#popover-ok-button");
+        $this->waitAfterRedirect(10000);
+    }
+
+    private function rollbackToNthCommit($whichCommit) {
+        $this->jsClick("#versionpress-commits-table tr:nth-child($whichCommit) a[href*=vp_rollback]");
+        $this->waitForAjax();
+        $this->jsClick("#popover-ok-button");
+        $this->waitAfterRedirect(10000);
+    }
+
+}
