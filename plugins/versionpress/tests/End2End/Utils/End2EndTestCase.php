8a6def5f9877a6b8b05cc4b7a799f25d0972fbf7
Jan Voráček <jan@voracek.net>
2015-03-03 14:31:19 +0100

    [#189] Implemented mechanism that skips tests if the worker does not exist

--- plugins/versionpress/tests/End2End/Utils/End2EndTestCase.php
+++ plugins/versionpress/tests/End2End/Utils/End2EndTestCase.php
@@ -16,11 +16,20 @@ class End2EndTestCase extends PHPUnit_Framework_TestCase {
     /** @var WpAutomation */
     protected static $wpAutomation;
 
+    private static $skipAllBecauseOfMissingWorker = false;
+
     public function __construct($name = NULL, array $data = array(), $dataName = '') {
         parent::__construct($name, $data, $dataName);
         $this->staticInitialization();
         $this->gitRepository = new GitRepository(self::$testConfig->testSite->path);
         self::$wpAutomation = new WpAutomation(self::$testConfig->testSite);
+    }
+
+    protected function setUp() {
+        parent::setUp();
+        if (self::$skipAllBecauseOfMissingWorker) {
+            $this->markTestSkipped('Missing worker');
+        }
     }
 
     private function staticInitialization() {
@@ -30,6 +39,12 @@ class End2EndTestCase extends PHPUnit_Framework_TestCase {
         $workerType = implode('', array_map('ucfirst', explode('-', self::$testConfig->end2endTestType)));
 
         $workerClass = $class . $workerType . 'Worker';
+
+        if (!class_exists($workerClass)) {
+            self::$skipAllBecauseOfMissingWorker = true;
+            return;
+        }
+
         $worker = new $workerClass(self::$testConfig);
 
         $propertyReflection = new \ReflectionProperty($class, 'worker');
