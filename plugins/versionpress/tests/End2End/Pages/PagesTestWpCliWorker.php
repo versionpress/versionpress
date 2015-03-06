cc75ffd5e9c361b5a68bc366527080a9c63ce32a
Jan Voráček <jan@voracek.net>
2015-03-06 16:55:29 +0100

    [#189] Fixed name of PagesTestWpCliWorker

--- plugins/versionpress/tests/End2End/Pages/PagesTestWpCliWorker.php
+++ plugins/versionpress/tests/End2End/Pages/PagesTestWpCliWorker.php
@@ -4,7 +4,7 @@ namespace VersionPress\Tests\End2End\Pages;
 
 use VersionPress\Tests\End2End\Utils\PostTypeTestWpCliWorker;
 
-class PagesTestTestWpCliWorker extends PostTypeTestWpCliWorker {
+class PagesTestWpCliWorker extends PostTypeTestWpCliWorker {
 
     public function getPostType() {
         return "page";
