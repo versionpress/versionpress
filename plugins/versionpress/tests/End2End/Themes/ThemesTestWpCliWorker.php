5a393b920b99a0d74302068b45a2cea2c4864ea6
Jan Voráček <jan@voracek.net>
2015-03-06 16:54:05 +0100

    [#189] Implemented WP-CLI worker for themes tests

---
+++ plugins/versionpress/tests/End2End/Themes/ThemesTestWpCliWorker.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace VersionPress\Tests\End2End\Themes;
+
+use VersionPress\Tests\End2End\Utils\WpCliWorker;
+
+class ThemesTestWpCliWorker extends WpCliWorker implements IThemesTestWorker {
+
+    private $themeInfo;
+
+    public function setThemeInfo($themeInfo) {
+        $this->themeInfo = $themeInfo;
+    }
+
+    public function prepare_uploadTheme() {
+    }
+
+    public function uploadTheme() {
+        $this->wpAutomation->runWpCliCommand('theme', 'install', array($this->themeInfo['zipfile']));
+    }
+
+    public function prepare_switchTheme() {
+    }
+
+    public function switchTheme() {
+        $this->wpAutomation->runWpCliCommand('theme', 'activate', array($this->themeInfo['stylesheet']));
+        file_get_contents($this->testConfig->testSite->url . '/wp-admin/');
+    }
+
+    public function prepare_deleteTheme() {
+    }
+
+    public function deleteTheme() {
+        $this->wpAutomation->runWpCliCommand('theme', 'delete', array($this->themeInfo['stylesheet']));
+    }
+}
