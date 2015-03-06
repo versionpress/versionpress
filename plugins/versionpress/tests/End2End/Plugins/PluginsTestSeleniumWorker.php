20ce4f549f62fcffb8dc8c2d4b507b89026a0cae
Václav Ostrožlík <vaclav@ostrozlik.cz>
2015-03-06 17:02:06 +0100

    [#189] Require login in Plugin tests

--- plugins/versionpress/tests/End2End/Plugins/PluginsTestSeleniumWorker.php
+++ plugins/versionpress/tests/End2End/Plugins/PluginsTestSeleniumWorker.php
@@ -13,6 +13,7 @@ class PluginsTestSeleniumWorker extends SeleniumWorker implements IPluginsTestWo
     }
 
     public function prepare_installPlugin() {
+        $this->loginIfNecessary();
         $this->url('wp-admin/plugin-install.php?tab=upload');
     }
 
