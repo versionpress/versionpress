7fe51eab164f70b82bb737380f1a82d03aa1c98d
Jan Voráček <jan@voracek.net>
2015-03-17 16:25:50 +0100

    [#334] Covered selective synchronization of users

--- plugins/versionpress/tests/SynchronizerTests/UserSynchronizerTest.php
+++ plugins/versionpress/tests/SynchronizerTests/UserSynchronizerTest.php
@@ -26,9 +26,7 @@ class UserSynchronizerTest extends SynchronizerTestCase {
      * @testdox Synchronizer adds new user to the database
      */
     public function synchronizerAddsNewUserToDatabase() {
-        $user = EntityUtils::prepareUser();
-        self::$vpId = $user['vp_id'];
-        $this->storage->save($user);
+        $this->createUser();
         $this->synchronizer->synchronize(Synchronizer::SYNCHRONIZE_EVERYTHING);
         DBAsserter::assertFilesEqualDatabase();
     }
@@ -38,7 +36,7 @@ class UserSynchronizerTest extends SynchronizerTestCase {
      * @testdox Synchronizer updates changed user in the database
      */
     public function synchronizerUpdatesChangedUserInDatabase() {
-        $this->storage->save(EntityUtils::prepareUser(self::$vpId, array('user_email' => 'changed.email@example.com')));
+        $this->editUser();
         $this->synchronizer->synchronize(Synchronizer::SYNCHRONIZE_EVERYTHING);
         DBAsserter::assertFilesEqualDatabase();
     }
@@ -48,8 +46,55 @@ class UserSynchronizerTest extends SynchronizerTestCase {
      * @testdox Synchronizer removes deleted user from the database
      */
     public function synchronizerRemovesDeletedUserFromDatabase() {
-        $this->storage->delete(EntityUtils::prepareUser(self::$vpId));
+        $this->deleteUser();
         $this->synchronizer->synchronize(Synchronizer::SYNCHRONIZE_EVERYTHING);
         DBAsserter::assertFilesEqualDatabase();
+    }
+
+    /**
+     * @test
+     * @testdox Synchronizer adds new user to the database (selective synchronization)
+     */
+    public function synchronizerAddsNewUserToDatabase_selective() {
+        $entitiesToSynchronize = $this->createUser();
+        $this->synchronizer->synchronize(Synchronizer::SYNCHRONIZE_EVERYTHING, $entitiesToSynchronize);
+        DBAsserter::assertFilesEqualDatabase();
+    }
+
+    /**
+     * @test
+     * @testdox Synchronizer updates changed user in the database (selective synchronization)
+     */
+    public function synchronizerUpdatesChangedUserInDatabase_selective() {
+        $entitiesToSynchronize = $this->editUser();
+        $this->synchronizer->synchronize(Synchronizer::SYNCHRONIZE_EVERYTHING, $entitiesToSynchronize);
+        DBAsserter::assertFilesEqualDatabase();
+    }
+
+    /**
+     * @test
+     * @testdox Synchronizer removes deleted user from the database (selective synchronization)
+     */
+    public function synchronizerRemovesDeletedUserFromDatabase_selective() {
+        $entitiesToSynchronize = $this->deleteUser();
+        $this->synchronizer->synchronize(Synchronizer::SYNCHRONIZE_EVERYTHING, $entitiesToSynchronize);
+        DBAsserter::assertFilesEqualDatabase();
+    }
+
+    private function createUser() {
+        $user = EntityUtils::prepareUser();
+        self::$vpId = $user['vp_id'];
+        $this->storage->save($user);
+        return array(array('vp_id' => self::$vpId, 'parent' => self::$vpId));
+    }
+
+    private function editUser() {
+        $this->storage->save(EntityUtils::prepareUser(self::$vpId, array('user_email' => 'changed.email@example.com')));
+        return array(array('vp_id' => self::$vpId, 'parent' => self::$vpId));
+    }
+
+    private function deleteUser() {
+        $this->storage->delete(EntityUtils::prepareUser(self::$vpId));
+        return array(array('vp_id' => self::$vpId, 'parent' => self::$vpId));
     }
 }
