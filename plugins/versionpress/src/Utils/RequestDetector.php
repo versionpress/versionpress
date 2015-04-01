versionpress/versionpress

commit 03dcc95e263266ff38cef772bc24f2ac0b9946f1

[#322] Bulk deletion of themes is now supported (it's possible only using wp-cli)

plugins/versionpress/src/Utils/RequestDetector.php
@@ -83,11 +83,11 @@ class RequestDetector {
         return $plugins;
     }
 
-    public function getThemeStylesheet() {
+    public function getThemeStylesheets() {
         if (!$this->isWpCli) {
             return $_GET['stylesheet'];
         }
 
-        return $this->wpCliArguments[2]; // theme delete <stylesheet>
+        return array_slice($this->wpCliArguments, 2); // theme delete <stylesheet>
     }
 }

plugins/versionpress/src/ChangeInfos/ThemeChangeInfo.php
@@ -73,7 +73,7 @@ class ThemeChangeInfo extends TrackedChangeInfo {
 
     public function getChangedFiles() {
         $themeChange = array("type" => "path", "path" => $path = WP_CONTENT_DIR . "/themes/" . $this->themeId . "/*");
-        $optionChange = array("type" => "storage-file", "entity" => "option", "id" => "");
+        $optionChange = array("type" => "storage-file", "entity" => "option", "id" => "", "parent-id" => "");
         return array($themeChange, $optionChange);
     }
 

plugins/versionpress/versionpress.php
@@ -191,8 +191,10 @@ function vp_register_hooks() {
     }
 
     if ($requestDetector->isThemeDeleteRequest()) {
-        $themeId = $requestDetector->getThemeStylesheet();
-        $committer->forceChangeInfo(new ThemeChangeInfo($themeId, 'delete'));
+        $themeIds = $requestDetector->getThemeStylesheets();
+        foreach ($themeIds as $themeId) {
+            $committer->forceChangeInfo(new ThemeChangeInfo($themeId, 'delete'));
+        }
     }
 
     if ($requestDetector->isPluginDeleteRequest()) {

commit 28dcbb341f07ec368c5170e95570256660de57c2

[#322] Fixed bug: Deleting theme now does a commit. Bug introduced in 03dcc95e263266ff38cef772bc24f2ac0b9946f1.

plugins/versionpress/src/Utils/RequestDetector.php
@@ -85,7 +85,7 @@ class RequestDetector {
 
     public function getThemeStylesheets() {
         if (!$this->isWpCli) {
-            return $_GET['stylesheet'];
+            return array($_GET['stylesheet']);
         }
 
         return array_slice($this->wpCliArguments, 2); // theme delete <stylesheet>