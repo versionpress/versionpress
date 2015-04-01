f97746d32563e8c79874ec04914031a9887dd622
Jan Voráček <jan@voracek.net>
2015-04-01 13:59:50 +0200

    [#335] PluginsTestWpCliWorker now uses method getRelativePath from parent class.

--- plugins/versionpress/tests/End2End/Plugins/PluginsTestWpCliWorker.php
+++ plugins/versionpress/tests/End2End/Plugins/PluginsTestWpCliWorker.php
@@ -10,7 +10,7 @@ class PluginsTestWpCliWorker extends WpCliWorker implements IPluginsTestWorker {
 
     public function setPluginInfo($pluginInfo) {
         $this->pluginInfo = $pluginInfo;
-        $this->pluginInfo['zipfile'] = $this->getRelativePath($this->testConfig->testSite->path, $this->pluginInfo['zipfile']);
+        $this->pluginInfo['zipfile'] = $this->getRelativePath($this->pluginInfo['zipfile']);
     }
 
     public function prepare_installPlugin() {
@@ -39,37 +39,5 @@ class PluginsTestWpCliWorker extends WpCliWorker implements IPluginsTestWorker {
 
     public function deletePlugin() {
         $this->wpAutomation->runWpCliCommand('plugin', 'delete', array($this->pluginInfo['css-id']));
-    }
-
-    private function getRelativePath($from, $to) {
-        // some compatibility fixes for Windows paths
-        $from = is_dir($from) ? rtrim($from, '\/') . '/' : $from;
-        $to   = is_dir($to)   ? rtrim($to, '\/') . '/'   : $to;
-        $from = str_replace('\\', '/', $from);
-        $to   = str_replace('\\', '/', $to);
-
-        $from     = explode('/', $from);
-        $to       = explode('/', $to);
-        $relPath  = $to;
-
-        foreach($from as $depth => $dir) {
-            // find first non-matching dir
-            if($dir === $to[$depth]) {
-                // ignore this directory
-                array_shift($relPath);
-            } else {
-                // get number of remaining dirs to $from
-                $remaining = count($from) - $depth;
-                if($remaining > 1) {
-                    // add traversals up to first matching dir
-                    $padLength = (count($relPath) + $remaining - 1) * -1;
-                    $relPath = array_pad($relPath, $padLength, '..');
-                    break;
-                } else {
-                    $relPath[0] = './' . $relPath[0];
-                }
-            }
-        }
-        return implode('/', $relPath);
     }
 }
