versionpress/versionpress

commit 34672043e70c81b1b7e45bf282fc2c0aed94fc40

[#334] Created tests for CommentSynchronizer

plugins/versionpress/src/DI/DIContainer.php
@@ -5,6 +5,7 @@ namespace VersionPress\DI;
 use Committer;
 use VersionPress\Configuration\VersionPressConfig;
 use VersionPress\Database\DbSchemaInfo;
+use VersionPress\Database\ExtendedWpdb;
 use VersionPress\Database\MirroringDatabase;
 use VersionPress\Git\GitRepository;
 use VersionPress\Git\Reverter;
@@ -49,8 +50,12 @@ class DIContainer {
             return new VersionPressConfig();
         });
 
+        $dic->register(VersionPressServices::PLAIN_WPDB, function () {
+            return new ExtendedWpdb(DB_USER, DB_PASSWORD, DB_NAME, DB_HOST);
+        });
+
         $dic->register(VersionPressServices::STORAGE_FACTORY, function () use ($dic) {
-            return new StorageFactory(VERSIONPRESS_MIRRORING_DIR, $dic->resolve(VersionPressServices::DB_SCHEMA));
+            return new StorageFactory(VERSIONPRESS_MIRRORING_DIR, $dic->resolve(VersionPressServices::DB_SCHEMA), $dic->resolve(VersionPressServices::PLAIN_WPDB));
         });
 
         $dic->register(VersionPressServices::MIRROR, function () use ($dic) {

plugins/versionpress/src/DI/VersionPressServices.php
@@ -14,6 +14,7 @@ final class VersionPressServices {
     const REVERTER = 'reverter';
     const REPOSITORY = 'repository';
     const VP_CONFIGURATION = 'vp-configuration';
+    const PLAIN_WPDB = 'wpdb';
 
     private function __construct() {
     }

plugins/versionpress/src/Storages/CommentStorage.php
@@ -3,9 +3,20 @@
 namespace VersionPress\Storages;
 
 use VersionPress\ChangeInfos\CommentChangeInfo;
+use VersionPress\Database\ExtendedWpdb;
 use VersionPress\Utils\EntityUtils;
 
 class CommentStorage extends DirectoryStorage {
+    /**
+     * @var ExtendedWpdb
+     */
+    private $database;
+
+    function __construct($directory, $entityInfo, $database) {
+        parent::__construct($directory, $entityInfo);
+        $this->database = $database;
+    }
+
 
     protected function createChangeInfo($oldEntity, $newEntity, $action = null) {
 
@@ -40,8 +51,10 @@ class CommentStorage extends DirectoryStorage {
 
         $author = $newEntity["comment_author"];
 
-        global $wpdb;
-        $result = $wpdb->get_row("SELECT post_title FROM {$wpdb->prefix}posts JOIN {$wpdb->prefix}vp_id ON {$wpdb->prefix}posts.ID = {$wpdb->prefix}vp_id.id WHERE vp_id = UNHEX('$newEntity[vp_comment_post_ID]')");
+        $postTable = $this->database->prefix . 'posts';
+        $vpIdTable = $this->database->prefix . 'vp_id';
+        $result = $this->database->get_row("SELECT post_title FROM {$postTable} JOIN {$vpIdTable} ON {$postTable}.ID = {$vpIdTable}.id WHERE vp_id = UNHEX('$newEntity[vp_comment_post_ID]')");
+
         $postTitle = $result->post_title;
 
         return new CommentChangeInfo($action, $newEntity["vp_id"], $author, $postTitle);

plugins/versionpress/src/Storages/StorageFactory.php
@@ -14,15 +14,19 @@ class StorageFactory {
 
     private $storages = array();
     const ENTITY_INFO = '%entityInfo%';
+    /** @var \wpdb */
+    private $database;
 
     /**
      * @param string $vpdbDir Path to the `wp-content/vpdb` directory
      * @param DbSchemaInfo $dbSchemaInfo Passed to storages
+     * @param $database
      */
-    function __construct($vpdbDir, $dbSchemaInfo) {
+    function __construct($vpdbDir, $dbSchemaInfo, $database) {
         $this->vpdbDir = $vpdbDir;
         $this->initStorageClasses();
         $this->dbSchemaInfo = $dbSchemaInfo;
+        $this->database = $database;
     }
 
     /**
@@ -52,7 +56,7 @@ class StorageFactory {
 
     private function initStorageClasses() {
         $this->addStorageClassInfo('post', 'VersionPress\Storages\PostStorage', '%vpdb%/posts', self::ENTITY_INFO);
-        $this->addStorageClassInfo('comment', 'VersionPress\Storages\CommentStorage', '%vpdb%/comments', self::ENTITY_INFO);
+        $this->addStorageClassInfo('comment', 'VersionPress\Storages\CommentStorage', '%vpdb%/comments', self::ENTITY_INFO, '%database%');
         $this->addStorageClassInfo('option', 'VersionPress\Storages\OptionsStorage', '%vpdb%/options.ini', self::ENTITY_INFO);
         $this->addStorageClassInfo('term', 'VersionPress\Storages\TermsStorage', '%vpdb%/terms.ini', self::ENTITY_INFO);
         $this->addStorageClassInfo('term_taxonomy', 'VersionPress\Storages\TermTaxonomyStorage', '%vpdb%/terms.ini', self::ENTITY_INFO);
@@ -99,6 +103,10 @@ class StorageFactory {
                 $matches = Strings::match($arg, '%storage\((.*)\)%');
                 $entity = $matches[1];
                 $arg = $this->getStorage($entity);
+            }
+
+            if (is_string($arg) && $arg === '%database%') {
+                $arg = $this->database;
             }
 
             $expandedArgs[] = $arg;

plugins/versionpress/tests/StorageTests/StorageFactoryTest.php
@@ -13,7 +13,7 @@ class StorageFactoryTest extends \PHPUnit_Framework_TestCase {
      * @test
      * @testdox Factory creates right storages
      */
-    public function testBanan() {
+    public function factoryCreatesRightStorages() {
         $storages = array(
             'post' => 'VersionPress\Storages\PostStorage',
             'comment' => 'VersionPress\Storages\CommentStorage',
@@ -25,7 +25,7 @@ class StorageFactoryTest extends \PHPUnit_Framework_TestCase {
             'postmeta' => 'VersionPress\Storages\PostMetaStorage',
         );
 
-        $factory = new StorageFactory(__DIR__ . '/vpdb', new DbSchemaInfo(__DIR__ . '/../../src/Database/wordpress-schema.neon', 'wp_'));
+        $factory = new StorageFactory(__DIR__ . '/vpdb', new DbSchemaInfo(__DIR__ . '/../../src/Database/wordpress-schema.neon', 'wp_'), null);
         foreach ($storages as $entityName => $expectedClass) {
             $this->assertInstanceOf($expectedClass, $factory->getStorage($entityName));
         }

plugins/versionpress/tests/SynchronizerTests/CommentSynchronizerTest.php
@@ -1,0 +1,151 @@
+<?php
+
+namespace VersionPress\Tests\SynchronizerTests;
+
+use Nette\Utils\Random;
+use VersionPress\Storages\CommentStorage;
+use VersionPress\Storages\PostStorage;
+use VersionPress\Storages\UserStorage;
+use VersionPress\Synchronizers\CommentsSynchronizer;
+use VersionPress\Synchronizers\PostsSynchronizer;
+use VersionPress\Synchronizers\Synchronizer;
+use VersionPress\Synchronizers\UsersSynchronizer;
+use VersionPress\Tests\SynchronizerTests\Utils\EntityUtils;
+use VersionPress\Tests\Utils\DBAsserter;
+use VersionPress\Utils\IdUtil;
+
+class CommentSynchronizerTest extends SynchronizerTestCase {
+    /** @var CommentStorage */
+    private $storage;
+    /** @var PostStorage */
+    private $postStorage;
+    /** @var UserStorage */
+    private $userStorage;
+    /** @var CommentsSynchronizer */
+    private $synchronizer;
+    /** @var PostsSynchronizer */
+    private $postSynchronizer;
+    /** @var UsersSynchronizer */
+    private $userSynchronizer;
+    private static $authorVpId;
+    private static $postVpId;
+    private static $vpId;
+
+    protected function setUp() {
+        parent::setUp();
+        $this->storage = self::$storageFactory->getStorage('comment');
+        $this->postStorage = self::$storageFactory->getStorage('post');
+        $this->userStorage = self::$storageFactory->getStorage('user');
+        $this->synchronizer = new CommentsSynchronizer($this->storage, self::$wpdb, self::$schemaInfo);
+        $this->postSynchronizer = new PostsSynchronizer($this->postStorage, self::$wpdb, self::$schemaInfo);
+        $this->userSynchronizer = new UsersSynchronizer($this->userStorage, self::$wpdb, self::$schemaInfo);
+    }
+
+    /**
+     * @test
+     * @testdox Synchronizer adds new comment to the database
+     */
+    public function synchronizerAddsNewCommentToDatabase() {
+        $this->createComment();
+        $this->userSynchronizer->synchronize(Synchronizer::SYNCHRONIZE_EVERYTHING);
+        $this->postSynchronizer->synchronize(Synchronizer::SYNCHRONIZE_EVERYTHING);
+        $this->synchronizer->synchronize(Synchronizer::SYNCHRONIZE_EVERYTHING);
+
+        DBAsserter::assertFilesEqualDatabase();
+    }
+
+    /**
+     * @test
+     * @testdox Synchronizer updates changed comment in the database
+     */
+    public function synchronizerUpdatesChangedCommentInDatabase() {
+        $this->editComment();
+        $this->synchronizer->synchronize(Synchronizer::SYNCHRONIZE_EVERYTHING);
+        DBAsserter::assertFilesEqualDatabase();
+    }
+
+    /**
+     * @test
+     * @testdox Synchronizer removes deleted comment from the database
+     */
+    public function synchronizerRemovesDeletedCommentFromDatabase() {
+        $this->deleteComment();
+        $this->synchronizer->synchronize(Synchronizer::SYNCHRONIZE_EVERYTHING);
+        $this->postSynchronizer->synchronize(Synchronizer::SYNCHRONIZE_EVERYTHING);
+        $this->userSynchronizer->synchronize(Synchronizer::SYNCHRONIZE_EVERYTHING);
+        DBAsserter::assertFilesEqualDatabase();
+    }
+
+    /**
+     * @test
+     * @testdox Synchronizer adds new comment to the database (selective synchronization)
+     */
+    public function synchronizerAddsNewCommentToDatabase_selective() {
+        $entitiesToSynchronize = $this->createComment();
+        $this->userSynchronizer->synchronize(Synchronizer::SYNCHRONIZE_EVERYTHING, $entitiesToSynchronize);
+        $this->postSynchronizer->synchronize(Synchronizer::SYNCHRONIZE_EVERYTHING, $entitiesToSynchronize);
+        $this->synchronizer->synchronize(Synchronizer::SYNCHRONIZE_EVERYTHING, $entitiesToSynchronize);
+
+        DBAsserter::assertFilesEqualDatabase();
+    }
+
+    /**
+     * @test
+     * @testdox Synchronizer updates changed comment in the database (selective synchronization)
+     */
+    public function synchronizerUpdatesChangedCommentInDatabase_selective() {
+        $entitiesToSynchronize = $this->editComment();
+        $this->synchronizer->synchronize(Synchronizer::SYNCHRONIZE_EVERYTHING, $entitiesToSynchronize);
+        DBAsserter::assertFilesEqualDatabase();
+    }
+
+    /**
+     * @test
+     * @testdox Synchronizer removes deleted comment from the database (selective synchronization)
+     */
+    public function synchronizerRemovesDeletedCommentFromDatabase_selective() {
+        $entitiesToSynchronize = $this->deleteComment();
+        $this->synchronizer->synchronize(Synchronizer::SYNCHRONIZE_EVERYTHING, $entitiesToSynchronize);
+        $this->postSynchronizer->synchronize(Synchronizer::SYNCHRONIZE_EVERYTHING, $entitiesToSynchronize);
+        $this->userSynchronizer->synchronize(Synchronizer::SYNCHRONIZE_EVERYTHING, $entitiesToSynchronize);
+        DBAsserter::assertFilesEqualDatabase();
+    }
+
+    private function createComment() {
+        $author = EntityUtils::prepareUser();
+        self::$authorVpId = $author['vp_id'];
+        $this->userStorage->save($author);
+
+        $post = EntityUtils::preparePost(null, self::$authorVpId);
+        self::$postVpId = $post['vp_id'];
+        $this->postStorage->save($post);
+
+        $comment = EntityUtils::prepareComment(null, self::$postVpId, self::$authorVpId);
+        self::$vpId = $comment['vp_id'];
+        $this->storage->save($comment);
+
+        return array(
+            array('vp_id' => self::$authorVpId, 'parent' => self::$authorVpId),
+            array('vp_id' => self::$postVpId, 'parent' => self::$postVpId),
+            array('vp_id' => self::$vpId, 'parent' => self::$vpId),
+        );
+    }
+
+    private function editComment() {
+        $this->storage->save(EntityUtils::prepareComment(self::$vpId, null, null, array('comment_approved' => '0')));
+        return array(
+            array('vp_id' => self::$vpId, 'parent' => self::$vpId),
+        );
+    }
+
+    private function deleteComment() {
+        $this->storage->delete(EntityUtils::prepareComment(self::$vpId));
+        $this->postStorage->delete(EntityUtils::preparePost(self::$postVpId));
+
+        return array(
+            array('vp_id' => self::$authorVpId, 'parent' => self::$authorVpId),
+            array('vp_id' => self::$postVpId, 'parent' => self::$postVpId),
+            array('vp_id' => self::$vpId, 'parent' => self::$vpId),
+        );
+    }
+}

plugins/versionpress/tests/SynchronizerTests/SynchronizerTestCase.php
@@ -33,16 +33,15 @@ class SynchronizerTestCase extends \PHPUnit_Framework_TestCase {
         $schemaFile = dirname($schemaReflection->getFileName()) . '/wordpress-schema.neon';
         self::$schemaInfo = new DbSchemaInfo($schemaFile, self::$testConfig->testSite->dbTablePrefix);
 
-        $vpdbPath = self::$testConfig->testSite->path . '/wp-content/vpdb';
-        self::$storageFactory = new StorageFactory($vpdbPath, self::$schemaInfo);
-
-
         $dbHost = self::$testConfig->testSite->dbHost;
         $dbUser = self::$testConfig->testSite->dbUser;
         $dbPassword = self::$testConfig->testSite->dbPassword;
         $dbName = self::$testConfig->testSite->dbName;
         self::$wpdb = new ExtendedWpdb($dbUser, $dbPassword, $dbName, $dbHost);
         self::$wpdb->set_prefix(self::$testConfig->testSite->dbTablePrefix);
+
+        $vpdbPath = self::$testConfig->testSite->path . '/wp-content/vpdb';
+        self::$storageFactory = new StorageFactory($vpdbPath, self::$schemaInfo, self::$wpdb);
     }
 
     private static function setUpSite() {

plugins/versionpress/tests/SynchronizerTests/Utils/EntityUtils.php
@@ -86,4 +86,30 @@ class EntityUtils {
 
         return $usermeta;
     }
+
+    public static function prepareComment($vpId = null, $postVpId = null, $authorVpId = null, $commentValues = array()) {
+        if ($vpId === null) {
+            $vpId = IdUtil::newId();
+        }
+
+        $comment = array_merge(array(
+            'comment_author' => 'Joe Tester',
+            'comment_author_email' => 'joetester@example.com',
+            'comment_author_url' => '',
+            'comment_date' => '2012-12-12 12:12:12',
+            'comment_content' => 'Some content',
+            'comment_approved' => 1,
+            'vp_id' => $vpId,
+        ), $commentValues);
+
+        if ($postVpId !== null) {
+            $comment['vp_comment_post_ID'] = $postVpId;
+        }
+
+        if ($authorVpId !== null) {
+            $comment['vp_user_id'] = $authorVpId;
+        }
+
+        return $comment;
+    }
 }

commit 3ccb3e572ffee22ab068a084e664c245a4667e19

[#334] Fixed CommentStorageTest. Bug introduced in 34672043e70c81b1b7e45bf282fc2c0aed94fc40.

plugins/versionpress/tests/End2End/Utils/AnonymousObject.php
@@ -1,0 +1,31 @@
+<?php
+
+namespace VersionPress\Tests\End2End\Utils;
+
+class AnonymousObject {
+    private $implementation = array();
+
+    public function __construct(array $options) {
+        $this->implementation = $options;
+    }
+
+    public function __call($name, $arguments) {
+        if (array_key_exists($name, $this->implementation)) {
+            $callable = $this->implementation[$name];
+
+            if (is_callable($callable)) {
+                return call_user_func_array($callable, $arguments);
+            }
+        }
+
+        throw new \BadMethodCallException("Method {$name} does not exists");
+    }
+
+    public function __get($name) {
+        return $this->implementation[$name];
+    }
+
+    public function __set($name, $value) {
+        $this->implementation[$name] = $value;
+    }
+}

plugins/versionpress/tests/StorageTests/CommentStorageTest.php
@@ -4,6 +4,7 @@ namespace VersionPress\Tests\StorageTests;
 
 use VersionPress\Database\EntityInfo;
 use VersionPress\Storages\CommentStorage;
+use VersionPress\Tests\End2End\Utils\AnonymousObject;
 use VersionPress\Utils\FileSystem;
 
 class CommentStorageTest extends \PHPUnit_Framework_TestCase {
@@ -58,23 +59,12 @@ class CommentStorageTest extends \PHPUnit_Framework_TestCase {
             )
         ));
         mkdir(__DIR__ . '/comments');
-        $this->storage = new CommentStorage(__DIR__ . '/comments', $entityInfo);
+        $wpdbFake = new AnonymousObject(array('prefix' => '', 'get_row' => function () { return new AnonymousObject(array('post_title' => '')); }));
+        $this->storage = new CommentStorage(__DIR__ . '/comments', $entityInfo, $wpdbFake);
     }
 
     protected function tearDown() {
         parent::tearDown();
         FileSystem::remove(__DIR__ . '/comments');
-    }
-}
-
-global $wpdb;
-$wpdb = new wpdbFake;
-
-class wpdbFake {
-    public $prefix;
-    function get_row() {
-        $fakePost = new \stdClass();
-        $fakePost->post_title = "";
-        return $fakePost;
     }
 }