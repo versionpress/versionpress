0d1fde46aff4449deebde14484d0d82b2c1dedf4
Václav Ostrožlík <vaclav@ostrozlik.cz>
2015-05-20 23:47:02 +0200

    [#369] Added tsd for managing TypeScript definition files.

--- frontend/typings/tsd.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="react/react-addons.d.ts" />
-/// <reference path="../node_modules/ts-jsx-loader/react-addons-jsx.d.ts" />
-/// <reference path="react-router/react-router.d.ts" />
-/// <reference path="mocha/mocha.d.ts" />
-/// <reference path="chai/chai.d.ts" />
---
+++ frontend/typings/typings.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="./es6.d.ts" />
+/// <reference path="./tsd.d.ts" />
+/// <reference path="../node_modules/ts-jsx-loader/react-addons-jsx.d.ts" />
