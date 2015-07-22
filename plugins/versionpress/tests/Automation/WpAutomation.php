df8341ad71817ae5784e319974277e3825cfb485
Jan Voráček <jan@voracek.net>
2015-07-22 13:19:53 +0200

    [#406] Fixed wrong constant value

--- plugins/versionpress/tests/Automation/WpAutomation.php
+++ plugins/versionpress/tests/Automation/WpAutomation.php
@@ -733,6 +733,6 @@ class WpAutomation {
     }
 
     private function disableAutoUpdate() {
-        file_put_contents($this->siteConfig->path . '/wp-config.php', "\ndefine( 'AUTOMATIC_UPDATER_DISABLED', false );\n", FILE_APPEND);
+        file_put_contents($this->siteConfig->path . '/wp-config.php', "\ndefine( 'AUTOMATIC_UPDATER_DISABLED', true );\n", FILE_APPEND);
     }
 }
