2a9bb852782a81ae402dbcb345aa4272ae7c619d
Václav Ostrožlík <vaclav@ostrozlik.cz>
2015-07-27 20:58:59 +0200

    [#400] Exported base API url.

--- plugins/versionpress/admin/inc/javascriptGui.php
+++ plugins/versionpress/admin/inc/javascriptGui.php
@@ -1,3 +1,9 @@
+<script>
+    var apiBaseUrl = '<?php
+        echo get_site_url() . '/' . vp_rest_get_url_prefix() . '/versionpress' ;
+    ?>';
+</script>
+
 <?php
 
 wp_enqueue_style('versionpress_gui_style', plugins_url('../public/gui/app.css', __FILE__));
