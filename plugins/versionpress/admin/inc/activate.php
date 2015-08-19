be457b2aad8ed8dc6d60525cd8785cb5298baef2
Václav Ostrožlík <vaclav@ostrozlik.cz>
2015-08-19 14:21:27 +0200

    [#400] Fixed old gui styling.

--- plugins/versionpress/admin/inc/activate.php
+++ plugins/versionpress/admin/inc/activate.php
@@ -8,6 +8,45 @@ use VersionPress\VersionPress;
 
 ?>
 
+<style>
+    .vp-index .welcome-panel {
+        padding-bottom: 23px;
+        margin-top: 20px;
+    }
+
+    .vp-index .welcome-panel p {
+        color: inherit;
+    }
+
+    .vp-index .welcome-panel .about-description {
+        margin: 23px 0 10px;
+    }
+
+    .vp-index .welcome-panel ul {
+        list-style: none;
+        padding-left: 40px;
+    }
+
+    .vp-index .welcome-panel ul li {
+        position: relative;
+    }
+
+    .vp-index .welcome-panel ul .icon {
+        position: absolute;
+        left: -25px;
+        top: 6px;
+    }
+
+    .initialization-progress p {
+        margin: 1px 0;
+    }
+
+    .initialization-done {
+        font-size: 1.2em;
+        font-weight: bold;
+    }
+</style>
+
 <div class="welcome-panel">
 
     <div class="welcome-panel-content">
