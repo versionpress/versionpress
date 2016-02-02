d092f21c61ee1b96bc98d70df97869abb3d89acd
Václav Ostrožlík <vaclav@ostrozlik.cz>
2016-02-02 19:56:11 +0100

    [#690] Refactored revertDialog to use Stateless components

--- frontend/src/Commits/revertDialog.tsx
+++ frontend/src/Commits/revertDialog.tsx
@@ -5,6 +5,37 @@ import * as request from 'superagent';
 import * as portal from '../common/portal';
 import * as WpApi from '../services/WpApi';
 
+const UndoEnabledDialog: React.StatelessComponent<{}> = () => {
+  return (
+    <div>
+      <p>
+        For EAP releases, please have a backup. {' '}
+        <a
+          href='http://docs.versionpress.net/en/feature-focus/undo-and-rollback'
+          target='_blank'
+        >Learn more about reverts.
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export const UndoDisabledDialog: React.StatelessComponent<{}> = () => (
+  <div>
+    <p className='undo-warning'>
+      <span className='icon icon-warning' />
+      You have {' '}
+      <a
+        href='http://docs.versionpress.net/en/feature-focus/undo-and-rollback#uncommitted-files'
+        target='_blank'
+      >uncommitted changes</a> {' '}
+      in your WordPress directory. {' '}
+      <br />
+      Please commit them before doing a revert.
+    </p>
+  </div>
+);
+
 export function revertDialog(title: React.ReactNode, okHandler: Function) {
   portal.confirmDialog(title, '', () => {}, () => {}, {loading: true});
 
@@ -12,34 +43,10 @@ export function revertDialog(title: React.ReactNode, okHandler: Function) {
     .get('can-revert')
     .end((err: any, res: request.Response) => {
       if (res.body) {
-        const body = (
-          <div>
-            <p>
-              For EAP releases, please have a backup. {' '}
-              <a
-                href='http://docs.versionpress.net/en/feature-focus/undo-and-rollback'
-                target='_blank'
-              >Learn more about reverts.</a>
-            </p>
-          </div>
-        );
+        const body = <UndoEnabledDialog />;
         portal.confirmDialog(title, body, okHandler, () => {}, {});
       } else {
-        const body = (
-          <div>
-            <p className='undo-warning'>
-              <span className='icon icon-warning' />
-              You have {' '}
-              <a
-                href='http://docs.versionpress.net/en/feature-focus/undo-and-rollback#uncommitted-files'
-                target='_blank'
-              >uncommitted changes</a>
-              in your WordPress directory. {' '}
-              <br />
-              Please commit them before doing a revert.
-            </p>
-          </div>
-        );
+        const body = <UndoDisabledDialog />;
         portal.confirmDialog(title, body, () => {}, () => {}, {okButtonClasses: 'disabled'});
       }
     });
