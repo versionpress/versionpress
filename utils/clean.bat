4671654a0b929d36d6bdad733e8a3b216b2cdff8
Borek Bernard <borekb@gmail.com>
2016-02-10 15:33:58 +0100

    [#540] Removed obsolete scripts in the `utils` dir (the entire dir removed)

--- utils/clean.bat
+++
@@ -1,5 +0,0 @@
-REM Deletes VersionPress working files in case you want to start over. Run from the WP site root.
-
-rmdir ".git" /s /q
-rmdir "wp-content\plugins\versionpress\db" /s /q
-del "wp-content\plugins\versionpress\.active" /q
