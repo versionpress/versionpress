9b279ac105e8670dd117ada02705aac3742ac845
Ivan Doležal <ivan.dolezal@gmail.com>
2016-02-27 17:22:43 +0100

    [#510] - parsing of Delete query, refactor, comments added,  tests.

--- plugins/versionpress/src/Database/SqlQueryParser.php
+++ plugins/versionpress/src/Database/SqlQueryParser.php
@@ -5,82 +5,119 @@ namespace VersionPress\Database;
 use SqlParser\Components\SetOperation;
 use SqlParser\Parser;
 use SqlParser\Statement;
+use SqlParser\Statements\DeleteStatement;
+use SqlParser\Statements\InsertStatement;
 use SqlParser\Statements\UpdateStatement;
 use VersionPress\Database\DbSchemaInfo;
 
 class SqlQueryParser {
 
     /**
+     * Parses Sql query called. If not parseable, returns null
      * @param $query string
      * @param $schema DbSchemaInfo
      * @param \wpdb $wpdb
-     * @return \Exception
+     * @return ParsedQueryData
      */
     public static function parseQuery($query, $schema, $wpdb) {
-
-        $parser = new Parser($query);
+        $parser = self::getParser($query);
         $primaryStatement = $parser->statements[0];
         if ($primaryStatement instanceof UpdateStatement) {
             return self::parseUpdateQuery($parser, $query, $schema, $wpdb);
+        } elseif ($primaryStatement instanceof InsertStatement) {
+            return self::parseInsertQuery($parser, $query);
+        } elseif ($primaryStatement instanceof DeleteStatement) {
+            return self::parseDeleteQuery($parser, $query, $schema, $wpdb);
         }
+        return null;
 
     }
 
+    /**
+     * Parses UPDATE query
+     *
+     * @param $parser Parser
+     * @param $schema DbSchemaInfo
+     * @param $query string
+     * @param \wpdb $wpdb
+     */
     private static function parseUpdateQuery($parser, $query, $schema, $wpdb) {
-        $primaryStatement = $parser->statements[0];
-        $tableExpression = $primaryStatement->tables[0]->expr;
-        $table = $primaryStatement->tables[0]->table;
-        $idColumn = $schema->getEntityInfoByPrefixedTableName($table)->idColumnName;
+        $statement = $parser->statements[0];
+        $table = $statement->tables[0]->table;
+        $idColumn = self::getIdColumn($schema, $table);
         $result = new ParsedQueryData('UPDATE');
         $result->originalQuery = $query;
         $result->table = $table;
 
-        $selectSql = "SELECT $idColumn FROM $tableExpression";
-        $where = self::getSelectWhereClause($parser, $query, $primaryStatement);
+        $selectSql = self::getSelect($parser, $idColumn);
+        $where = self::getSelectWhereClause($parser, $query, $statement);
         if (isset($where)) {
             $selectSql .= " WHERE " . join(' ', $where);
         }
         $result->query = $selectSql;
         $result->ids = $wpdb->get_col($selectSql);
-        $result->data = self::getData($primaryStatement);
-        $result->dirty = self::maybeDataDirty($parser);
+        $result->data = self::getData($statement);
+        $result->dirty = self::isQueryDirty($parser);
         $result->where = self::getWhere($where);
+        return $result;
+    }
+
+    /**
+     * Parses INSERT query
+     *
+     * @param $parser
+     * @param $query
+     * @return ParsedQueryData
+     */
+    private static function parseInsertQuery($parser, $query) {
+        $primaryStatement = $parser->statements[0];
+        $queryType = 'INSERT';
+        $table = $primaryStatement->into->dest->table;
+        if (count($primaryStatement->options->options) > 0) {
+            foreach (array_keys($primaryStatement->options->options) as $key) {
+                $queryType .= "_" . $primaryStatement->options->options[$key];
+            }
+        }
+        $result = new ParsedQueryData($queryType);
+        $result->data = self::getData($primaryStatement);
+        $result->table = $table;
+        $result->originalQuery = $query;
+        $result->dirty = self::isQueryDirty($parser);
         return $result;
     }
 
 
     /**
-     * @param $statement UpdateStatement
-     * @param $idColumn string Name of the id column
-     * @return string
+     * Parses DELETE query
+     *
+     * @param $parser
+     * @param $query
+     * @param $schema
+     * @param $wpdb
+     * @return ParsedQueryData
      */
-    public static function prepareUpdateQuery($statement, $idColumn) {
-        if (!($statement instanceof UpdateStatement)) {
-            return new \Exception("Wrong method used for processing statement");
+    private static function parseDeleteQuery($parser, $query, $schema, $wpdb) {
+        $statement = $parser->statements[0];
+        $table = $statement->from[0]->table;
+        $idColumn = self::getIdColumn($schema, $table);
+        $result = new ParsedQueryData('DELETE');
+        $result->originalQuery = $query;
+        $result->table = $table;
+        $selectSql = self::getSelect($parser, $idColumn);
+        $where = self::getSelectWhereClause($parser, $query, $statement);
+        if (isset($where)) {
+            $selectSql .= " WHERE " . join(' ', $where);
         }
-
-        $tableExpression = $statement->tables[0]->expr;
-        $set = self::setToString($statement->set);
-        $where = ' WHERE ' . $idColumn . '= %s';
-        $sql = "UPDATE " . $tableExpression . $set . $where;
-        return $sql;
-
+        $result->query = $selectSql;
+        $result->ids = $wpdb->get_col($selectSql);
+        $result->dirty = self::isQueryDirty($parser);
+        $result->where = self::getWhere($where);
+        return $result;
     }
 
     /**
-     * @param $setOperation SetOperation
-     * @return string
-     */
-    private static function setToString($setOperations) {
-        $sqlChunk = ' SET';
-        foreach ($setOperations as $setOperation) {
-            $sqlChunk .= " " . $setOperation->column . "=" . $setOperation->value . ",";
-        }
-
-        return rtrim($sqlChunk, ',');
-    }
-
-    /**
+     * Returns representation of WHERE SQL clauses found in whole query
+     *
      * @param $parser
      * @param $sql
      * @param $primaryStatement
@@ -105,24 +142,83 @@ class SqlQueryParser {
         }
     }
 
-
+    /**
+     * Gets data which needs to be set by UPDATE statement
+     *
+     * Example
+     *
+     * [data] => Array
+     *     (
+     *         [0] => Array
+     *               (
+     *                   [column] => post_modified
+     *                   [value] => NOW()
+     *               )
+     *     )
+     * @param $statement
+     * @return array
+     */
     private static function getData($statement) {
-        if (!($statement instanceof UpdateStatement)) {
-            return new \Exception("Wrong method used for processing statement");
+
+        if ($statement instanceof UpdateStatement) {
+            return array_map(function ($set) {
+                return array('column' => $set->column, 'value' => $set->value);
+            }, $statement->set);
+        } elseif ($statement instanceof InsertStatement) {
+            $columns = $statement->into->columns;
+            $result = [];
+            for ($i = 0; $i < count($statement->values); $i++) {
+                $sets = $statement->values[$i];
+                $data = [];
+                for ($j = 0; $j < count($sets->values); $j++) {
+                    array_push($data, array('column' => $columns[$j], 'value' => $sets->values[$j]));
+                }
+                array_push($result, $data);
+            }
+            return $result;
         }
-        return array_map(function ($set) {
-            return array('column' => $set->column, 'value' => $set->value);
-        }, $statement->set);
+
     }
 
-    private static function maybeDataDirty($parser) {
+    /**
+     * If query contains some markers (unknown functions) or more than one statement.
+     * @param $parser
+     * @return bool
+     */
+    private static function isQueryDirty($parser) {
         if (count($parser->statements) > 1) {
             return true;
         } else {
-            return self::containsDirtyPatterns($parser->statements[0]->set);
+            $statement = $parser->statements[0];
+            if ($statement instanceof UpdateStatement) {
+                return self::containsDirtyPatterns($parser->statements[0]->set);
+            } elseif ($statement instanceof InsertStatement) {
+                if (isset($statement->tables)) {
+                    return true;
+                }
+                $values = [];
+                foreach ($statement->values as $dataSet) {
+                    $values = array_merge($values, $dataSet->values);
+                }
+                return self::containsDirtyPatterns($values);
+            }
         }
     }
 
+    /**
+     * Returns Sql query WHERE fragments as array
+     *
+     * Example
+     *
+     * [where] => Array
+     *     (
+     *          [0] => option_name LIKE '%_'
+     *          [1] => option_value LIKE '%s'
+     *     )
+     * Filters operators  where fragments
+     * @param $whereConditions
+     * @return mixed
+     */
     private static function getWhere($whereConditions) {
         $where = array_filter($whereConditions, function ($w) {
             if (is_object($w)) {
@@ -149,7 +245,9 @@ class SqlQueryParser {
      */
     private static function containsDirtyPatterns($sets) {
         $dirtyPatterns = array(
-            "/DATE_ADD[ ]?\\(.*/" //https://regex101.com/r/tS2iC6/1
+            "/DATE_ADD[ ]?\\(.*/", //https://regex101.com/r/tS2iC6/1
+            "/NOW\\(\\)/",
+            "/NOW/"
         );
         foreach ($sets as $set) {
             foreach ($dirtyPatterns as $pattern) {
@@ -159,6 +257,92 @@ class SqlQueryParser {
             }
         }
         return false;
+    }
+
+    /**
+     * Creates Select SQL query from query in Parser
+     *
+     *
+     *
+     * @param $parser
+     * @param $idColumn
+     * @return string
+     */
+    private static function getSelect($parser, $idColumn) {
+        $query = "SELECT $idColumn FROM ";
+        $statement = $parser->statements[0];
+        if (isset($statement->from)) {
+            $from = $statement->from[0];
+        } else {
+            $from = $statement->tables[0];
+        }
+        $query .= $from->expr;
+        if ($from->alias != null) {
+            $query .= ' AS ' . $from->alias;
+        }
+        if (isset($statement->join)) {
+            $join = $statement->join[0];
+            $query .= ' JOIN ' . $join->expr->expr;
+            if ($join->expr->alias != null) {
+                $query .= ' AS ' . $join->expr->alias;
+            }
+            if ($join->on != null) {
+                $query .= ' ON ' . $join->on[0]->expr;
+            }
+        }
+        return $query;
+    }
+
+    /**
+     * If query contains some suspicious patten, we need to transform it and than create Parser for further use.
+     *
+     * @param $query
+     * @return Parser
+     */
+    private static function getParser($query) {
+        $containsUsingPattern = "/(.*)(USING ?\\(([^\\)]+)\\))(.*)/"; //https://regex101.com/r/vF6dI5/1
+        $isTransformed = false;
+        $parser = new Parser($query);
+        $transformedQuery = '';
+        $primaryStatement = $parser->statements[0];
+        if ($primaryStatement instanceof DeleteStatement) {
+            $transformedPart = 'ON ';
+            if (preg_match_all($containsUsingPattern, $query, $matches)) {
+                $usingColumn = str_replace('`', '', $matches[3][0]);
+                $isTransformed = true;
+                $from = $primaryStatement->from[0];
+                if ($from->alias != null) {
+                    $transformedPart .= $from->alias . '.' . $usingColumn;
+                } else {
+                    $transformedPart .= $from->table . '.' . $usingColumn;
+                }
+                $transformedPart .= '=';
+                $join = $primaryStatement->join[0];
+
+                if ($join->expr->alias != null) {
+                    $transformedPart .= $join->expr->alias . '.' . $usingColumn;
+                } else {
+                    $transformedPart .= $join->expr->table . '.' . $usingColumn;
+                }
+                $transformedQuery = preg_replace($containsUsingPattern, '$1' . $transformedPart . '$4', $query);
+
+            }
+        }
+        if ($isTransformed) {
+            return new Parser($transformedQuery);
+        }
+        return $parser;
+    }
+
+    /**
+     * Returns ID column for a table
+     *
+     * @param $schema
+     * @param $table
+     * @return mixed
+     */
+    private static function getIdColumn($schema, $table) {
+        return $schema->getEntityInfoByPrefixedTableName($table)->idColumnName;
     }
 
 
--- plugins/versionpress/tests/SqlTests/ParserTest.php
+++ plugins/versionpress/tests/SqlTests/ParserTest.php
@@ -19,7 +19,6 @@ class ParserTest extends PHPUnit_Framework_TestCase {
      */
     private static $DbSchemaInfo;
 
-
     /** @var \wpdb $wpdbStub */
     private $wpdbStub;
 
@@ -31,35 +30,30 @@ class ParserTest extends PHPUnit_Framework_TestCase {
         $this->wpdbStub = $this->getMockBuilder('\wpdb')->disableOriginalConstructor()->getMock();
     }
 
-
     /**
      * @test
      * @dataProvider updateQueryParseTestDataProvider
      */
-    public function expectedWhereClausesAreOk($query, $select, $data, $where, $dirty) {
+    public function whereClausesFromUpdate($query, $select, $data, $where, $dirty) {
+
         $this->wpdbStub->expects(new PHPUnit_Framework_MockObject_Matcher_AnyInvokedCount)->method("get_col");
-
-
-        /** @var ParsedQueryData $parsedQueryData */
         $parsedQueryData = SqlQueryParser::parseQuery($query, self::$DbSchemaInfo, $this->wpdbStub);
+
         $this->assertEquals($where, $parsedQueryData->where);
 
     }
 
-
     /**
      * @test
      * @dataProvider updateQueryParseTestDataProvider
      */
-    public function expectedDataToSetAreOk($query, $select, $data, $where, $dirty) {
+    public function dataToSetFromUpdate($query, $select, $data, $where, $dirty) {
+
         $this->wpdbStub->expects(new PHPUnit_Framework_MockObject_Matcher_AnyInvokedCount)->method("get_col");
-
-
-        /** @var ParsedQueryData $parsedQueryData */
         $parsedQueryData = SqlQueryParser::parseQuery($query, self::$DbSchemaInfo, $this->wpdbStub);
+
         $this->assertEquals($data, $parsedQueryData->data);
 
-
     }
 
 
@@ -67,15 +61,12 @@ class ParserTest extends PHPUnit_Framework_TestCase {
      * @test
      * @dataProvider updateQueryParseTestDataProvider
      */
-    public function expectedDirtyAreDirty($query, $select, $data, $where, $dirty) {
+    public function detectDirtyUpdate($query, $select, $data, $where, $dirty) {
 
         $this->wpdbStub->expects(new PHPUnit_Framework_MockObject_Matcher_AnyInvokedCount)->method("get_col");
-
-
-        /** @var ParsedQueryData $parsedQueryData */
         $parsedQueryData = SqlQueryParser::parseQuery($query, self::$DbSchemaInfo, $this->wpdbStub);
+        print_r($parsedQueryData);
         $this->assertEquals($dirty, $parsedQueryData->dirty);
-
     }
 
 
@@ -83,54 +74,173 @@ class ParserTest extends PHPUnit_Framework_TestCase {
      * @test
      * @dataProvider updateQueryParseTestDataProvider
      */
-    public function expectedSelectQueriesAreOk($query, $select, $data, $where, $dirty) {
+    public function selectQueryFromUpdate($query, $select, $data, $where, $dirty, $ids) {
 
         $this->wpdbStub->expects(new PHPUnit_Framework_MockObject_Matcher_AnyInvokedCount)->method("get_col")
-            ->with($select)->will(new PHPUnit_Framework_MockObject_Stub_Return(true));
+            ->with($select)->will(new PHPUnit_Framework_MockObject_Stub_Return($ids));
+        $parsedQueryData = SqlQueryParser::parseQuery($query, self::$DbSchemaInfo, $this->wpdbStub);
+
+        $this->assertEquals($select, $parsedQueryData->query);
+    }
+
+    /**
+     * @test
+     * @dataProvider insertQueryParseTestDataProvider
+     */
+    public function dataFromInsert($query, $data) {
+
+        $parsedQueryData = SqlQueryParser::parseQuery($query, self::$DbSchemaInfo, $this->wpdbStub);
+
+        $this->assertEquals($parsedQueryData->data, $data);
+    }
+
+    /**
+     * @test
+     * @dataProvider insertQueryParseTestDataProvider
+     */
+    public function markDirtyInsertDirty($query, $data, $dirty) {
+
         /** @var ParsedQueryData $parsedQueryData */
         $parsedQueryData = SqlQueryParser::parseQuery($query, self::$DbSchemaInfo, $this->wpdbStub);
-        //print_r($parsedQueryData);
-        $this->assertEquals($select, $parsedQueryData->query);
+        $this->assertEquals($parsedQueryData->dirty, $dirty);
+    }
 
+    /**
+     * @test
+     * @dataProvider insertQueryParseTestDataProvider
+     */
+    public function detectNonStandardInsert($query, $data, $dirty, $queryType) {
+
+        $parsedQueryData = SqlQueryParser::parseQuery($query, self::$DbSchemaInfo, $this->wpdbStub);
+
+        $this->assertEquals($parsedQueryData->queryType, $queryType);
+    }
+
+    /**
+     * @test
+     * @dataProvider deleteQueryParseTestDataProvider
+     */
+    public function selectQueryFromDelete($query, $select, $testIds) {
+        $this->wpdbStub->expects(new PHPUnit_Framework_MockObject_Matcher_AnyInvokedCount)->method("get_col")
+            ->with($select)->will(new PHPUnit_Framework_MockObject_Stub_Return($testIds));
+        $parsedQueryData = SqlQueryParser::parseQuery($query, self::$DbSchemaInfo, $this->wpdbStub);
+
+        $this->assertEquals($select, $parsedQueryData->query);
+    }
+
+
+    public function deleteQueryParseTestDataProvider() {
+        $testIds = [1, 3, 15];
+        return array(
+            array("DELETE o1 FROM `wp_options` AS o1 JOIN `wp_options` AS o2 ON o1.option_name=o2.option_name WHERE o2.option_id > o1.option_id",
+                "SELECT option_name FROM `wp_options` AS o1 JOIN `wp_options` AS o2 ON o1.option_name=o2.option_name WHERE o2.option_id > o1.option_id",
+                $testIds
+            ),
+            array("DELETE o1 FROM `wp_options` AS o1 JOIN `wp_options` AS o2 USING (`option_name`) WHERE o2.option_id > o1.option_id",
+                "SELECT option_name FROM `wp_options` AS o1 JOIN `wp_options` AS o2 ON o1.option_name=o2.option_name WHERE o2.option_id > o1.option_id",
+                $testIds
+            ),
+            array("DELETE FROM `wp_usermeta` WHERE meta_key IN ('key1', 'key2')",
+                "SELECT umeta_id FROM `wp_usermeta` WHERE meta_key IN ('key1', 'key2')",
+                $testIds
+            ),
+            array("DELETE FROM `wp_usermeta` WHERE meta_key = 'key1'",
+                "SELECT umeta_id FROM `wp_usermeta` WHERE meta_key = 'key1'",
+                $testIds
+            )
+        );
+    }
+
+
+    public function insertQueryParseTestDataProvider() {
+        return array(
+            array(
+                "INSERT INTO `wp_term_relationships` (object_id, term_taxonomy_id, term_order) VALUES (10, 4, 5) , (20, 10, 15) ON DUPLICATE KEY UPDATE term_order = VALUES(term_order)",
+                array(array(array("column" => "object_id", "value" => "10"), array("column" => "term_taxonomy_id", "value" => "4"), array("column" => "term_order", "value" => "5")),
+                    array(array("column" => "object_id", "value" => "20"), array("column" => "term_taxonomy_id", "value" => "10"), array("column" => "term_order", "value" => "15"))),
+                1,
+                'INSERT'
+            ),
+            array(
+                "INSERT IGNORE INTO `wp_terms` (term_id, name, slug, term_group) VALUES (10, 'term name', 'term-name', 5) , (20, 'term another', 'term-another', 15)",
+                array(array(array("column" => "term_id", "value" => "10"), array("column" => "name", "value" => "term name"), array("column" => "slug", "value" => "term-name"), array("column" => "term_group", "value" => "5")),
+                    array(array("column" => "term_id", "value" => "20"), array("column" => "name", "value" => "term another"), array("column" => "slug", "value" => "term-another"), array("column" => "term_group", "value" => "15"))),
+                0,
+                'INSERT_IGNORE'
+            ),
+            array(
+                "INSERT INTO `wp_terms` (term_id, name, slug, term_group) VALUES (10, 'term name', 'term-name', 5)",
+                array(array(array("column" => "term_id", "value" => "10"), array("column" => "name", "value" => "term name"), array("column" => "slug", "value" => "term-name"), array("column" => "term_group", "value" => "5"))),
+                0,
+                'INSERT'
+            ),
+            array(
+                "INSERT INTO `wp_terms` (term_id, name, slug, term_group) VALUES (10, 'term name', 'term-name', 5) , (20, 'term another', 'term-another', 15)",
+                array(array(array("column" => "term_id", "value" => "10"), array("column" => "name", "value" => "term name"), array("column" => "slug", "value" => "term-name"), array("column" => "term_group", "value" => "5")),
+                    array(array("column" => "term_id", "value" => "20"), array("column" => "name", "value" => "term another"), array("column" => "slug", "value" => "term-another"), array("column" => "term_group", "value" => "15"))),
+                0,
+                'INSERT'
+            ),
+            array(
+                "INSERT INTO `wp_terms` (term_id, date) VALUES (10, NOW())",
+                array(array(array("column" => "term_id", "value" => "10"), array("column" => "date", "value" => "NOW"))),
+                1,
+                'INSERT'
+            )
+
+        );
     }
 
     public function updateQueryParseTestDataProvider() {
         $testIds = [1, 3, 15];
         return array(
             array(
+                "UPDATE  `wp_posts` SET post_modified = NOW() WHERE post_author = 'B'",
+                "SELECT ID FROM `wp_posts` WHERE post_author = 'B'",
+                array(array("column" => "post_modified", "value" => "NOW()")),
+                array("post_author = 'B'"),
+                1,
+                $testIds
+            ),
+            array(
                 "UPDATE `wp_options` SET option_value=REPLACE(option_value, 'wp-links/links-images/', 'wp-images/links/') WHERE option_name LIKE '%_' AND option_value LIKE '%s'",
                 "SELECT option_name FROM `wp_options` WHERE option_name LIKE '%_' AND option_value LIKE '%s'",
                 array(array("column" => "option_value", "value" => "")),
                 array("option_name LIKE '%_'", "option_value LIKE '%s'"),
-                1
+                1,
+                $testIds
             ),
             array(
                 "UPDATE `wp_options` SET option_value=REPLACE(option_value, 'wp-links/links-images/', 'wp-images/links/'), option_abc = 'def' WHERE option_name LIKE '%A' AND option_value LIKE '%s'",
                 "SELECT option_name FROM `wp_options` WHERE option_name LIKE '%A' AND option_value LIKE '%s'",
                 array(array("column" => "option_value", "value" => "")),
                 array("option_name LIKE '%A'", "option_value LIKE '%s'"),
-                1
+                1,
+                $testIds
             ),
             array(
                 "UPDATE `wp_posts` SET post_parent = '10', post_type='page' WHERE post_type = 'attachment' AND ID IN (" . join(',', $testIds) . ")",
                 "SELECT ID FROM `wp_posts` WHERE post_type = 'attachment' AND ID IN (" . join(',', $testIds) . ")",
                 array(array("column" => "post_parent", "value" => "'10'"), array("column" => "post_type", "value" => "'page'")),
                 array("post_type = 'attachment'", "ID IN (" . join(',', $testIds) . ")"),
-                0
+                0,
+                $testIds
             ),
             array(
                 "UPDATE `wp_posts` SET post_date_gmt = DATE_ADD(post_date, INTERVAL '01:20' HOUR_MINUTE)",
                 "SELECT ID FROM `wp_posts` WHERE 1=1",
                 array(array("column" => "post_date_gmt", "value" => "DATE_ADD(post_date")),
                 array("1=1"),
-                1
+                1,
+                $testIds
             ),
             array(
                 "UPDATE  `wp_posts` SET post_author = 'A' WHERE post_author = 'B'",
                 "SELECT ID FROM `wp_posts` WHERE post_author = 'B'",
                 array(array("column" => "post_author", "value" => "'A'")),
                 array("post_author = 'B'"),
-                0
+                0,
+                $testIds
             )
 
 
