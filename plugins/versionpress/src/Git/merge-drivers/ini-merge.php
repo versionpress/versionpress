75e5d63cc91849959b97eb395b6fa55708a515d5
Ivan Doležal <ivan.dolezal@gmail.com>
2016-02-28 18:11:34 +0100

    [#719] - added comments to php driver implementation.

--- plugins/versionpress/src/Git/merge-drivers/ini-merge.php
+++ plugins/versionpress/src/Git/merge-drivers/ini-merge.php
@@ -1,9 +1,11 @@
 <?php
 
+// File names passed via cli arguments
 $O = $argv[1];
 $A = $argv[2];
 $B = $argv[3];
 
+// Dates fields to merge
 $dates = array("post_modified", "post_modified_gmt");
 
 $mergeCommand = 'git merge-file -L mine -L base -L theirs ' . $A . ' ' . $O . ' ' . $B;
@@ -14,10 +16,14 @@ $bFile = file_get_contents($B);
 
 
 foreach ($dates as $date) {
+
+    // Find values
     $dateMatchPattern = "/" . $date . " = \"([^'\"]*)\"/";
     $dateReplacePattern = "/(" . $date . " = \")([0-9 :-]*)(\")/";
     $matches = array();
     preg_match($dateMatchPattern, $aFile, $matches);
+
+    // If file does not contain field, we will skip date replacement
     if (count($matches) == 0) {
         break;
     }
@@ -28,6 +34,7 @@ foreach ($dates as $date) {
 
     $aDate = new DateTime($aDateString);
     $bDate = new DateTime($bDateString);
+    //Replace date value in both files to be more recent
     if ($aDate->getTimestamp() > $bDate->getTimestamp()) {
         $bFile = preg_replace($dateReplacePattern, '${1}' . $aDateString . '${3}', $bFile);
     } else {
@@ -36,13 +43,15 @@ foreach ($dates as $date) {
     }
 
 }
-
+// Add temporary placeholder between adjacent lines to prevent merge conflicts
 file_put_contents($B, preg_replace('/(\r\n|\r|\n)/', "$1#######\n", $bFile));
 file_put_contents($A, preg_replace('/(\r\n|\r|\n)/', "$1#######\n", $aFile));
 file_put_contents($O, preg_replace('/(\r\n|\r|\n)/', "$1#######\n", $oFile));
 
+// Call git merge command and receive the exitcode
 exec($mergeCommand, $dummy, $mergeExitCode);
 
+// Remove temporary placeholders
 file_put_contents($B, str_replace("#######\n", '', file_get_contents($B)));
 file_put_contents($A, str_replace("#######\n", '', file_get_contents($A)));
 file_put_contents($O, str_replace("#######\n", '', file_get_contents($O)));
