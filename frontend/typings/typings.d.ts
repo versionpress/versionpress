cf2f7f1d63d3242247742eb602623c5f57be4059
Václav Ostrožlík <vaclav@ostrozlik.cz>
2016-03-20 16:10:46 +0100

    [#201] Updated react, added react-addons-update package

--- frontend/typings/typings.d.ts
+++ frontend/typings/typings.d.ts
@@ -1,3 +1,2 @@
 /// <reference path="tsd/tsd.d.ts" />
 /// <reference path="versionpress-custom/karma-chai-sinon.d.ts" />
-/// <reference path="versionpress-custom/react-custom.d.ts" />
--- frontend/typings/versionpress-custom/react-custom.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace __React {
-  interface HTMLAttributes {
-    [key: string]: any
-  }
-}
