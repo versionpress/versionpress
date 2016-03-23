72f8b0e3ff130d19aae54959176a1aa2c73d2bf4
Jan Voráček <jan@voracek.net>
2016-03-23 10:00:47 +0100

    [#785] Added support for default values in annotation generator + refactoring

--- plugins/versionpress/src/Database/wpdb-api-to-phpdoc.php
+++ plugins/versionpress/src/Database/wpdb-api-to-phpdoc.php
@@ -1,30 +1,49 @@
 <?php
 // Generates wpdb API description as PhpDoc. Used by `Database`.
+// Run this file as `php wpdb-api-to-phpdoc.php`.
 
-$r = new ReflectionClass('wpdb');
-$props = $r->getProperties(ReflectionProperty::IS_PUBLIC);
+$pathToWpdb = __DIR__ . '/../../../../ext-libs/wordpress/wp-includes/wp-db.php';
 
-foreach ($props as $prop) {
-    preg_match_all('/@var (.*?)\n/s', $prop->getDocComment(), $annotations);
-    $type = $annotations[1][0];
+require_once $pathToWpdb;
 
-    echo "@property ", $type, " ", $prop->getName(), "\n";
+$wpdbReflection = new ReflectionClass('wpdb');
+
+// Generate annotations for all public properties as @property <type> <name>
+$properties = $wpdbReflection->getProperties(ReflectionProperty::IS_PUBLIC);
+foreach ($properties as $property) {
+    preg_match_all('/@var (.*?)\n/s', $property->getDocComment(), $annotations);
+    $type = @$annotations[1][0];
+
+    echo "@property " . ($type ? $type . ' ' : '') . $property->getName() . "\n";
 }
 
-$methods = $r->getMethods(ReflectionMethod::IS_PUBLIC);
+echo "\n";
 
+// Generate annotations for all public methods as @method <return type> <name>(<parameters with default value if there is any>)
+$methods = $wpdbReflection->getMethods(ReflectionMethod::IS_PUBLIC);
 foreach ($methods as $method) {
     $parameters = $method->getParameters();
     preg_match_all('/@return (.*?)[ \n]/s', $method->getDocComment(), $annotations);
-    $returnType = $annotations[1][0];
 
-    echo "@method ", $returnType, " ", $method->getName(), "(";
+    @$returnType = $annotations[1][0];
 
-    $params = array_map(function (ReflectionParameter $parameter) {
-        return '$' . $parameter->getName();
+    echo "@method " . ($returnType ? $returnType . ' ' : '') . $method->getName() . "(";
+
+    $parametersWithDefaultValues = array_map(function (ReflectionParameter $parameter) {
+        if ($parameter->isDefaultValueAvailable()) {
+            if ($parameter->isDefaultValueConstant()) {
+                $default = $parameter->getDefaultValueConstantName();
+            } else {
+                $default = str_replace(['array (', "\n"], ['array(', ''], var_export($parameter->getDefaultValue(), true));
+            }
+        } else {
+            $default = null;
+        }
+
+        return '$' . $parameter->getName() . ($default !== null ? ' = ' . $default : '');
     }, $method->getParameters());
 
-    echo join(', ', $params);
+    echo join(', ', $parametersWithDefaultValues);
 
     echo ")\n";
 }
