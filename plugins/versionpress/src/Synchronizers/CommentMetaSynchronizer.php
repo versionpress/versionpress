69b908d455ce679838c77a51a2aea395eb0da226
Ivan Doležal <ivan.dolezal@gmail.com>
2016-03-30 14:12:56 +0200

    [#815] - unnecessary comment removal.

--- plugins/versionpress/src/Synchronizers/CommentMetaSynchronizer.php
+++ plugins/versionpress/src/Synchronizers/CommentMetaSynchronizer.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ivan
- * Date: 3/29/16
- * Time: 10:32 AM
- */
 
 namespace VersionPress\Synchronizers;
 
@@ -16,7 +10,7 @@ use VersionPress\Storages\Storage;
 use VersionPress\Utils\AbsoluteUrlReplacer;
 use VersionPress\Utils\WordPressCacheUtils;
 
-class CommentMetaSynchronizer extends SynchronizerBase{
+class CommentMetaSynchronizer extends SynchronizerBase {
 
     function __construct(Storage $storage, Database $database, DbSchemaInfo $dbSchema, AbsoluteUrlReplacer $urlReplacer, ShortcodesReplacer $shortcodesReplacer) {
         parent::__construct($storage, $database, $dbSchema, $urlReplacer, $shortcodesReplacer, 'commentmeta');
