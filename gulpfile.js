versionpress/versionpress

commit a5864217196765fae21aaee465a36213753dbd5c

[#550] Removed nightly build task - is served by plain `build` now

gulpfile.js
@@ -27,17 +27,6 @@ var runSequence = require('run-sequence');
 var packageVersion = "";
 
 /**
- * Type of build. Possible values:
- *
- *  - '' (default - means production)
- *  - 'nightly' - set by the `nightly` task
- *  - 'test-deploy' - set by the `test-deploy` task
- *
- * @type {string}
- */
-var buildType = ''; // empty (default, means production),
-
-/**
  * Directory where the VP files are copied to and some actions
  * like `composer install` etc. are run on them.
  *
@@ -85,10 +74,6 @@ var adminGuiDir = vpDir + '/admin/public/gui';
  */
 var srcDef = [];
 
-
-gulp.task('set-nightly-build', false, function () {
-    buildType = 'nightly';
-});
 
 /**
  * Sets `buildDir` and `buildType` so that the copy methods copies to the WP test site.
@@ -313,12 +298,6 @@ gulp.task('build', 'Task that exports production build', ['clean-build'], functi
     console.log(" ");
 });
 
-/**
- * Exports "nightly" build which contains the same files as production (`build`) build
- * but the version number in both plugin metadata and file name contains short Git hash,
- * e.g., "versionpress-1.0+58a96f2.zip" or "Version: 1.0+58a96f2".
- */
-gulp.task('nightly-build', 'Nightly build from current Git revision. Short hash is added to ZIP name.', ['set-nightly-build', 'clean-build']);
 
 /**
  * Task called from WpAutomation to copy the plugin files to the test directory

commit 699ccee297a9bdffe2e51a66386bc6f74e3b1d62

[#837] Fixing `gulp build` (undefined `buildType` variable)
The bug was introduced in a5864217196765fae21aaee465a36213753dbd5c

gulpfile.js
@@ -74,9 +74,15 @@ var adminGuiDir = vpDir + '/admin/public/gui';
  */
 var srcDef = [];
 
+/**
+ * Set to true for test-deploy build 
+ *
+ * @type {boolean}
+ */
+var isTestDeployBuild = false;
 
 /**
- * Sets `buildDir` and `buildType` so that the copy methods copies to the WP test site.
+ * Sets `buildDir` and `isTestDeployBuild` so that the copy methods copies to the WP test site.
  */
 gulp.task('prepare-test-deploy', false, function () {
     var testConfigStr = fs.readFileSync(vpDir + '/tests/test-config.yml', 'utf-8');
@@ -87,12 +93,12 @@ gulp.task('prepare-test-deploy', false, function () {
     }
     buildDir = sitePath + "/wp-content/plugins/versionpress";
 
-    buildType = 'test-deploy'; // later influences the `prepare-src-definition` task
+    isTestDeployBuild = true;
 });
 
 /**
  * Prepares `srcDef` that is later used in the `copy` task. The src definition
- * is slightly different for various buildTypes.
+ * is slightly different for production build and test-deploy build.
  */
 gulp.task('prepare-src-definition', false, function () {
 
@@ -109,7 +115,7 @@ gulp.task('prepare-src-definition', false, function () {
     srcDef.push('!' + vpDir + '/**/*.md'); // all Markdown files are considered documentation
 
     // and now some build-specific patterns:
-    if (buildType == 'test-deploy') {
+    if (isTestDeployBuild) {
         srcDef.push('!' + vpDir + '/composer.json');
         srcDef.push('!' + vpDir + '/composer.lock');
         srcDef.push('!' + vpDir + '/vendor/**/.git{,/**}'); // keep vendor but ignore all nested Git repositories in it