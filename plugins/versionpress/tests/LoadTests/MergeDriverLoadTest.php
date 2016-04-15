83466c42bbac1058e50f459307160d4233273acd
Borek Bernard <borekb@gmail.com>
2016-04-15 20:30:37 +0200

    The default number of iterations in MergeDriverLoadTest changed from 1000 to 1 so that this test doesn't slow down the execution if someone runs all tests in the `tests` folder.

--- plugins/versionpress/tests/LoadTests/MergeDriverLoadTest.php
+++ plugins/versionpress/tests/LoadTests/MergeDriverLoadTest.php
@@ -10,6 +10,12 @@ class MergeDriverLoadTest extends \PHPUnit_Framework_TestCase {
 
     private static $repositoryDir;
 
+    /**
+     * !!! INCREASE THIS to something like 100 or 1000 to get more real-world results.
+     * The default is low so that if someone accidentally runs all tests, it doesn't block it.
+     */
+    const ITERATIONS = 1;
+
     public static function setUpBeforeClass() {
         self::$repositoryDir = __DIR__ . '/repository';
     }
@@ -73,19 +79,17 @@ class MergeDriverLoadTest extends \PHPUnit_Framework_TestCase {
     }
 
     private function prepareTestRepositoryHistory() {
-        $limit = 1000;
-
-        for ($i = 0; $i < $limit; $i++) {
+        for ($i = 0; $i < self::ITERATIONS; $i++) {
             MergeDriverTestUtils::writeIniFile('file' . $i . '.ini', '2011-11-11 11:11:11');
         }
         MergeDriverTestUtils::commit('Initial commit to Ancestor');
         MergeDriverTestUtils::runGitCommand('git checkout -b test-branch');
-        for ($i = 0; $i < $limit; $i++) {
+        for ($i = 0; $i < self::ITERATIONS; $i++) {
             MergeDriverTestUtils::writeIniFile('file' . $i . '.ini', '2012-12-12 12:12:12', 'Custom content');
         }
         MergeDriverTestUtils::commit('Commit to branch');
         MergeDriverTestUtils::runGitCommand('git checkout master');
-        for ($i = 0; $i < $limit; $i++) {
+        for ($i = 0; $i < self::ITERATIONS; $i++) {
             MergeDriverTestUtils::writeIniFile('file' . $i . '.ini', '2013-03-03 13:13:13');
         }
         MergeDriverTestUtils::commit('Commit to master');
