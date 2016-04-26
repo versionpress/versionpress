5a8c16b9c33f1da38eea3e6c133ac7b9972bf528
Ivan Doležal <ivan.dolezal@gmail.com>
2016-04-26 12:53:47 +0200

    [minor] Test of yaml parsing fixed after default page title was renamed in test-config.sample.yml

--- plugins/versionpress/tests/Unit/TestConfigTest.php
+++ plugins/versionpress/tests/Unit/TestConfigTest.php
@@ -25,7 +25,7 @@ class TestConfigTest extends PHPUnit_Framework_TestCase
 
         // 'sites' section
         $this->assertArrayHasKey('vp01', $config->sites);
-        $this->assertEquals("VP Test @ localhost", $config->sites["vp01"]->title);
+        $this->assertEquals("localhost", $config->sites["vp01"]->host);
 
     }
 }
