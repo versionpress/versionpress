94c427c3ebfa743d81228af454040787ba82e3fb
Jan Voráček <jan@voracek.net>
2016-07-21 15:29:03 +0200

    [#1036] ActionsInfo now parses action files (for now it uses only the last one)

--- plugins/versionpress/src/Git/ActionsInfo.php
+++ plugins/versionpress/src/Git/ActionsInfo.php
@@ -2,26 +2,43 @@
 
 namespace VersionPress\Git;
 
+use Symfony\Component\Yaml\Yaml;
+
 class ActionsInfo
 {
+    const DEFAULT_PRIORITY = 10;
+
     /** @var array */
     private $actionMap;
 
     public function __construct($actionFiles = [])
     {
         foreach ($actionFiles as $file) {
+            $content = file_get_contents($file);
+            $this->actionMap = Yaml::parse($content);
+        }
 
+        foreach ($this->actionMap as $entityName => &$tagsAndActions) {
+            foreach ($tagsAndActions['actions'] as &$action) {
+                if (is_string($action)) {
+                    $action = ['message' => $action, 'priority' => self::DEFAULT_PRIORITY];
+                }
+
+                if (!isset($action['priority'])) {
+                    $action['priority'] = self::DEFAULT_PRIORITY;
+                }
+            }
         }
     }
 
     public function getTags($entityName)
     {
-        return $this->actionMap[$entityName]['tags'];
+        return @$this->actionMap[$entityName]['tags'] ?: [];
     }
 
     public function createCommitMessage($entityName, $action, $vpid, $tags)
     {
-        $message = $this->actionMap[$entityName]['actions'][$action]['message'];
+        $message = @$this->actionMap[$entityName]['actions'][$action]['message'] ?: '';
 
         foreach ($tags as $tag => $value) {
             $message = str_replace("%{$tag}%", $value, $message);
@@ -34,6 +51,11 @@ class ActionsInfo
 
     public function getActionPriority($entityName, $action)
     {
-        return $this->actionMap[$entityName]['actions'][$action]['priority'];
+        return @$this->actionMap[$entityName]['actions'][$action]['priority'] ?: self::DEFAULT_PRIORITY;
+    }
+
+    public function getTagContainingParentId($entityName)
+    {
+        return @$this->actionMap[$entityName]['parent-id-tag'] ?: null;
     }
 }
