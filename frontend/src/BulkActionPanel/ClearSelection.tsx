1d0cefd74327a3062d94e22b5f386288cf80ca38
Michal Snovický <snovickym@gmail.com>
2016-08-09 12:11:13 +0200

    [#1087] Split logic of BulkActionPanel into smaller components.

---
+++ frontend/src/BulkActionPanel/ClearSelection.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import * as classNames from 'classnames';
+
+interface ClearSelectionProps {
+  changes: number;
+  onClick(e: React.MouseEvent): void;
+}
+
+const ClearSelection: React.StatelessComponent<ClearSelectionProps> = ({ changes, onClick }) => {
+  const noteClassName = classNames({
+    'BulkActionPanel-note': true,
+    'hide': changes === 0
+  });
+
+  return (
+    <div className={noteClassName}>
+      {`(${changes} ${changes === 1 ? 'change' : 'changes'} selected; `}
+      <a
+        className='BulkActionPanel-clear'
+        href='#'
+        onClick={onClick}
+      >
+        clear selection
+      </a>
+      {')'}
+    </div>
+  );
+}
+
+export default ClearSelection;
