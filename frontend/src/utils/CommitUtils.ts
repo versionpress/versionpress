ca15465236a2226f70ffc6752176bc64421b1ea0
Václav Ostrožlík <vaclav@ostrozlik.cz>
2016-08-31 13:59:51 +0200

    [#1087] Linked reference to CommitUtils

--- frontend/src/utils/CommitUtils.ts
+++ frontend/src/utils/CommitUtils.ts
@@ -1,3 +1,5 @@
+/// <reference path='../common/Commits.d.ts' />
+
 export function indexOf(array: Commit[], commit: Commit) {
   for (let i = 0; i < array.length; i++) {
     if (commit.hash === array[i].hash) {
