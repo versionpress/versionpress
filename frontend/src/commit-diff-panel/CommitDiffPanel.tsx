70d48880fa09b519225b07c089f03b8abd99214a
Michal Snovický <snovickym@gmail.com>
2016-09-05 15:30:07 +0200

    [#1087] Removed unnecessary lines.

--- frontend/src/commit-diff-panel/CommitDiffPanel.tsx
+++ frontend/src/commit-diff-panel/CommitDiffPanel.tsx
@@ -11,18 +11,14 @@ interface CommitDiffPanelProps {
   diff: string;
 }
 
-const CommitDiffPanel: React.StatelessComponent<CommitDiffPanelProps> = ({ diff }) => {
-  if (diff === null) {
-    return <div />;
-  }
-
-  const diffs = DiffParser.parse(diff);
-
-  return (
-    <div>
-      {diffs.map((diff: Diff, i) => <Diff diff={diff} key={i} />)}
-    </div>
-  );
-};
+const CommitDiffPanel: React.StatelessComponent<CommitDiffPanelProps> = ({ diff }) => (
+  <div>
+    {diff !== null &&
+      DiffParser.parse(diff).map((diff: Diff, i) => (
+        <Diff diff={diff} key={i} />
+      ))
+    }
+  </div>
+);
 
 export default CommitDiffPanel;
