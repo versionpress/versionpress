d2458e532d1613ca3d7bb23c7b66df602dbd451f
Michal Snovický <snovickym@gmail.com>
2016-09-13 14:45:26 +0200

    [#1091] Added basic MobX functionality.

--- frontend/src/components/home/HomePage.tsx
+++ frontend/src/components/home/HomePage.tsx
@@ -3,9 +3,9 @@
 
 import * as React from 'react';
 import * as ReactRouter from 'react-router';
-import * as request from 'superagent';
 import * as moment from 'moment';
 import * as classNames from 'classnames';
+import { observer } from 'mobx-react';
 
 import BulkActionPanel from '../bulk-action-panel/BulkActionPanel';
 import CommitPanel from '../commit-panel/CommitPanel';
@@ -17,15 +17,12 @@ import ServicePanel from '../service-panel/ServicePanel';
 import UpdateNotice from './update-notice/UpdateNotice';
 import VpTitle from './vp-title/VpTitle';
 import WelcomePanel from '../welcome-panel/WelcomePanel';
-import config from '../../config/config';
-import * as WpApi from '../../services/WpApi';
-import { indexOf } from '../../utils/CommitUtils';
 import { revertDialog } from '../portal/portal';
-import { getErrorMessage, getDiff, getGitStatus, parsePageNumber } from './utils';
+import { getDiff, getGitStatus } from './utils';
+
+import appStore from '../../stores/appStore';
 
 import './HomePage.less';
-
-const routes = config.routes;
 
 interface HomePageProps {
   params: {
@@ -33,26 +30,12 @@ interface HomePageProps {
   };
 }
 
-interface HomePageState {
-  pages?: number[];
-  query?: string;
-  commits?: Commit[];
-  selectedCommits?: Commit[];
-  lastSelectedCommit?: Commit;
-  message?: InfoMessage;
-  isLoading?: boolean;
-  displayServicePanel?: boolean;
-  displayWelcomePanel?: boolean;
-  displayUpdateNotice?: boolean;
-  isDirtyWorkingDirectory?: boolean;
-  progress?: number;
-}
-
 interface HomePageContext {
   router: ReactRouter.Context;
 }
 
-export default class HomePage extends React.Component<HomePageProps, HomePageState> {
+@observer
+export default class HomePage extends React.Component<HomePageProps, {}> {
 
   static contextTypes: React.ValidationMap<any> = {
     router: React.PropTypes.func.isRequired,
@@ -60,232 +43,41 @@ export default class HomePage extends React.Component<HomePageProps, HomePageSta
 
   context: HomePageContext;
 
-  state = {
-    pages: [],
-    query: '',
-    commits: [],
-    selectedCommits: [],
-    lastSelectedCommit: null,
-    message: null,
-    isLoading: true,
-    displayServicePanel: false,
-    displayWelcomePanel: false,
-    displayUpdateNotice: false,
-    isDirtyWorkingDirectory: false,
-    progress: 100,
-  };
-
-  private refreshInterval;
-
-  private updateProgress = (e: {percent: number}) => {
-    this.setState({
-      progress: e.percent,
-    });
-  };
-
-  private setLoading = () => {
-    this.setState({
-      isLoading: true,
-      progress: 0,
-    });
-  };
-
-  private wpUndoRollback = (name: string, query: any) => {
-    this.setLoading();
-
-    WpApi
-      .get(name)
-      .query(query)
-      .on('progress', this.updateProgress)
-      .end((err: any, res: request.Response) => {
-        if (err) {
-          this.setState({
-            message: getErrorMessage(res, err),
-            isLoading: false,
-          });
-        } else {
-          this.context.router.transitionTo(routes.home);
-          document.location.reload();
-        }
-      });
-  };
-
-  private wpCommitDiscardEnd = (successMessage: string) => {
-    return (err: any, res: request.Response) => {
-      if (err) {
-        this.setState({
-          message: getErrorMessage(res, err),
-        });
-      } else {
-        this.setState({
-          isDirtyWorkingDirectory: false,
-          message: {
-            code: 'updated',
-            message: successMessage,
-          },
-        });
-        this.fetchCommits();
-      }
-      return !err;
-    };
-  };
-
   componentDidMount() {
-    this.fetchWelcomePanel();
-    this.fetchCommits();
-    this.refreshInterval = setInterval(this.checkUpdate, 10 * 1000);
+    appStore.updatePage(this.props.params.page);
+    appStore.setRouter(this.context.router);
+    appStore.fetchWelcomePanel();
+    appStore.fetchCommits();
   }
 
   componentWillReceiveProps(nextProps: HomePageProps) {
-    this.fetchCommits(nextProps.params);
+    appStore.updatePage(nextProps.params.page);
+    appStore.fetchCommits();
   }
 
-  componentWillUnmount() {
-    clearInterval(this.refreshInterval);
-  }
-
-  fetchCommits = (params = this.props.params) => {
-    this.setLoading();
-
-    const page = parsePageNumber(params.page);
-    if (page < 1) {
-      this.context.router.transitionTo(routes.home);
-    }
-
-    WpApi
-      .get('commits')
-      .query({
-        page: page,
-        query: encodeURIComponent(this.state.query),
-      })
-      .on('progress', this.updateProgress)
-      .end((err: any, res: request.Response) => {
-        const data = res.body.data as VpApi.GetCommitsResponse;
-
-        if (err) {
-          this.setState({
-            pages: [],
-            commits: [],
-            message: getErrorMessage(res, err),
-            isLoading: false,
-            displayUpdateNotice: false,
-          });
-        } else {
-          this.setState({
-            pages: data.pages.map(c => c + 1),
-            commits: data.commits,
-            message: null,
-            isLoading: false,
-            displayUpdateNotice: false,
-          });
-          this.checkUpdate();
-        }
-      });
-  };
-
-  fetchWelcomePanel = () => {
-    WpApi
-      .get('display-welcome-panel')
-      .end((err: any, res: request.Response) => {
-        const data = res.body.data as VpApi.DisplayWelcomePanelResponse;
-
-        if (err) {
-          return;
-        }
-
-        this.setState({
-          displayWelcomePanel: data === true,
-        });
-      });
-  };
-
-  checkUpdate = () => {
-    const { query, commits, isLoading } = this.state;
-
-    if (!commits.length || isLoading) {
-      return;
-    }
-
-    WpApi
-      .get('should-update')
-      .query({
-        query: encodeURIComponent(query),
-        latestCommit: commits[0].hash,
-      })
-      .end((err: any, res: request.Response) => {
-        const data = res.body.data as VpApi.ShouldUpdateResponse;
-
-        if (err) {
-          this.setState({
-            displayUpdateNotice: false,
-            isDirtyWorkingDirectory: false,
-          });
-          clearInterval(this.refreshInterval);
-        } else {
-          this.setState({
-            displayUpdateNotice: !this.props.params.page && data.update === true,
-            isDirtyWorkingDirectory: data.cleanWorkingDirectory !== true,
-          });
-        }
-      });
+  fetchCommits = () => {
+    appStore.fetchCommits()
   };
 
   undoCommits = (commits: string[]) => {
-    this.wpUndoRollback('undo', { commits: commits });
+    appStore.undoCommits(commits);
   };
 
   rollbackToCommit = (hash: string) => {
-    this.wpUndoRollback('rollback', { commit: hash });
+    appStore.rollbackToCommit(hash);
   };
 
   onServicePanelClick = () => {
-    this.setState({
-      displayServicePanel: !this.state.displayServicePanel,
-    });
+    appStore.changeDisplayServicePanel();
   };
 
   onCommitsSelect = (commitsToSelect: Commit[], isChecked: boolean, isShiftKey: boolean) => {
-    const { commits } = this.state;
-    let { selectedCommits, lastSelectedCommit } = this.state;
-    const isBulk = commitsToSelect.length > 1;
-
-    commitsToSelect
-      .filter(commit => commit.canUndo)
-      .forEach(commit => {
-        let lastIndex = -1;
-        const index = indexOf(commits, commit);
-
-        if (!isBulk && isShiftKey) {
-          lastIndex = indexOf(commits, lastSelectedCommit);
-        }
-
-        lastIndex = lastIndex === -1 ? index : lastIndex;
-
-        const step = index < lastIndex ? -1 : 1;
-        const cond = index + step;
-        for (let i = lastIndex; i !== cond; i += step) {
-          const currentCommit = commits[i];
-          const currentIndex = indexOf(selectedCommits, currentCommit);
-
-          if (isChecked && currentIndex === -1) {
-            selectedCommits.push(currentCommit);
-          } else if (!isChecked && currentIndex !== -1) {
-            selectedCommits.splice(currentIndex, 1);
-          }
-
-          lastSelectedCommit = currentCommit;
-        }
-      });
-
-    this.setState({
-      selectedCommits: selectedCommits,
-      lastSelectedCommit: isBulk ? null : lastSelectedCommit,
-    });
+    appStore.onCommitsSelect(commitsToSelect, isChecked, isShiftKey);
   };
 
   onBulkAction = (action: string) => {
     if (action === 'undo') {
-      const { selectedCommits } = this.state;
+      const { selectedCommits } = appStore;
       const count = selectedCommits.length;
 
       const title = (
@@ -298,43 +90,23 @@ export default class HomePage extends React.Component<HomePageProps, HomePageSta
   };
 
   onClearSelection = () => {
-    this.setState({
-      selectedCommits: [],
-      lastSelectedCommit: null,
-    });
+    appStore.clearSelection();
   };
 
   onCommit = (message: string) => {
-    this.updateProgress({ percent: 0 });
-
-    WpApi
-      .post('commit')
-      .send({ 'commit-message': message })
-      .on('progress', this.updateProgress)
-      .end(this.wpCommitDiscardEnd('Changes have been committed.'));
+    appStore.onCommit(message);
   };
 
   onDiscard = () => {
-    this.updateProgress({ percent: 0 });
-
-    WpApi
-      .post('discard-changes')
-      .on('progress', this.updateProgress)
-      .end(this.wpCommitDiscardEnd('Changes have been discarded.'));
+    appStore.onDiscard();
   };
 
   onFilterQueryChange = (query: string) => {
-    this.setState({
-      query: query,
-    });
+    appStore.onFilterQueryChange(query);
   };
 
   onFilter = () => {
-    if (parsePageNumber(this.props.params.page) > 0) {
-      this.context.router.transitionTo(routes.home);
-    } else {
-      this.fetchCommits();
-    }
+    appStore.onFilter();
   };
 
   onUndo = (hash: string, message: string) => {
@@ -356,21 +128,13 @@ export default class HomePage extends React.Component<HomePageProps, HomePageSta
   onWelcomePanelHide = (e: React.MouseEvent) => {
     e.preventDefault();
 
-    this.setState({
-      displayWelcomePanel: false,
-    });
-
-    WpApi
-      .post('hide-welcome-panel')
-      .end((err: any, res: request.Response) => {
-        this.fetchCommits();
-      });
+    appStore.onWelcomePanelHide();
   };
 
   onUpdateNoticeClick = (e: React.MouseEvent) => {
     e.preventDefault();
 
-    this.fetchCommits();
+    appStore.fetchCommits();
   };
 
   render() {
@@ -386,7 +150,7 @@ export default class HomePage extends React.Component<HomePageProps, HomePageSta
       displayUpdateNotice,
       isDirtyWorkingDirectory,
       progress,
-    } = this.state;
+    } = appStore;
 
     const homePageClassName = classNames({
       'loading': isLoading,
