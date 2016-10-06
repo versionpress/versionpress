a883fe3d094010f99bba0cc398acbef304dcd0a6
Václav Ostrožlík <vaclav@ostrozlik.cz>
2016-10-06 14:38:29 +0200

    [#1043] Added parentHashes field to Api endpoint getCommits

--- frontend/src/stores/commitsTableStore.ts
+++ frontend/src/stores/commitsTableStore.ts
@@ -57,37 +57,10 @@ class CommitsTableStore {
     let graphStructure = [];
     for (let i = 0; i < this.commits.length; i++) {
       const commit = this.commits[i];
-      let parents = [];
-
-      if (commit.hash === '8f3c2d0a7161f1aa51b60eee06ce9c32644a9417') {
-        parents = ['cf9d842a5fe9a6f5878d4505ed301f28bbd2db77'];
-
-      } else if (commit.hash === 'e5e557d04a0b1ee18d5269d187a814c9cb60eeb8') {
-        parents = ['bea439d9c06894f573acb8e98f13a02500143c26'];
-      } else if (commit.hash === '208ba26f16dc668a56f925c39bd5083bcdddc8c9') {
-        parents = ['db8df6fd3df8867fd63c1e3fcbace6eb416e0be6'];
-      } else if (commit.hash === '3c408b411e8e3764a6159ec4da2c1f08503d640e') {
-        parents = ['5e6ca8ab6303bf02b3cd26a74a499fea4584e945'];
-      } else if (commit.hash === 'db872709db982dfdb13e7bf02c0bdb3de6968b8e') {
-        parents = ['1524372c235a167d73cbcbcfeb008620ff471c46'];
-
-      } else if (commit.hash === '202c87ebc6468ee2c272c488687bcb9315f88729') {
-        parents = ['b8adb93da34462f3016d81636d86cd97d6dbf791', '5e6ca8ab6303bf02b3cd26a74a499fea4584e945'];
-
-      } else if (commit.hash === '314a97fa25fedbc977e97c562daed0834b8b72ca') {
-        parents = ['7214375088f22d111c8efe54d3a9d949b8b24f15', 'e5e557d04a0b1ee18d5269d187a814c9cb60eeb8'];
-      } else if (commit.hash === 'bfbd729934794e435c3f276e97a9c6c6b3faf0fd') {
-        parents = ['db872709db982dfdb13e7bf02c0bdb3de6968b8e', '208ba26f16dc668a56f925c39bd5083bcdddc8c9'];
-
-      } else if (i === this.commits.length - 1) {
-        parents = [];
-      } else {
-        parents = [this.commits[i + 1].hash];
-      }
 
       graphStructure.push({
         sha: commit.hash,
-        parents: parents,
+        parents: commit.parentHashes,
         environment: commit.environment,
       });
     }
