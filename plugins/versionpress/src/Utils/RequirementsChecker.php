405bbafd327d5066c11fdfd78f754111b00be444
Jan Voracek <jan@voracek.net>
2017-07-04 13:27:21 +0200

    Removing temp dir entirely

--- plugins/versionpress/src/Utils/RequirementsChecker.php
+++ plugins/versionpress/src/Utils/RequirementsChecker.php
@@ -274,7 +274,7 @@ class RequirementsChecker
 
     private function tryAccessControlFiles()
     {
-        $securedUrl = site_url() . '/wp-content/plugins/versionpress/temp/security-check.txt';
+        $securedUrl = site_url() . '/wp-content/plugins/versionpress/versionpress-nginx.conf';
         /** @noinspection PhpUsageOfSilenceOperatorInspection */
         return @file_get_contents($securedUrl) === false; // intentionally @
     }
