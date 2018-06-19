a7f556a189edb33469856b923fd971216cdb9a24
Michal Snovický <snovickym@gmail.com>
2018-06-19 14:18:08 +0200

    Fix incorrect icon display and reduce production CSS size

--- frontend/webpack/webpack.prod.js
+++ frontend/webpack/webpack.prod.js
@@ -19,6 +19,7 @@ module.exports = () => {
                     parallel: true,
                     sourceMap: true,
                 }),
+                new OptimizeCSSAssetsPlugin({}),
             ],
         },
     };
