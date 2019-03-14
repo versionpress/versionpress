e04e396c7e63cc856342624f14ef6af040936e19
Jan Voracek <jan@voracek.net>
2019-03-14 15:23:30 +0100

    Fixed creating log directory

--- plugins/versionpress/tests/phpunit-bootstrap.php
+++ plugins/versionpress/tests/phpunit-bootstrap.php
@@ -9,7 +9,7 @@ require_once(__DIR__ . '/../vendor/autoload.php');
 // Internal logs for things like exceptions or `Debugger::log` calls.
 // Location of PHPUnit logs is dictated externally, see docker-compose.yml or CLI invocations.
 $logDir = getenv('VP_TESTS_LOG_DIR') ? getenv('VP_TESTS_LOG_DIR') : sys_get_temp_dir() . '/vp-logs/.tracy';
-@mkdir($logDir);
+@mkdir($logDir, 0777, true);
 Debugger::enable(Debugger::DEVELOPMENT, $logDir);
 
 TestConfig::$defaultConfigFile = __DIR__ . '/test-config.yml';
