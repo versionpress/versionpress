a9324d3f61d07113bc9ee9dea8c07f3406044a43
Borek Bernard <borekb@gmail.com>
2019-03-16 22:54:18 +0100

    Remove temporary experiments, make run-tests.ts run real tests

--- scripts/run-tests.ts
+++ scripts/run-tests.ts
@@ -11,8 +11,7 @@ shell.exec(`${dc} down -v`);
 utils.printTaskHeading('Starting tests...');
 shell.exec(`${dc} up -d wordpress-for-tests`);
 
-// Try commenting out the following line; the test should then fail as they won't wait for MySQL
 shell.exec(`${dc} run --rm wait`);
 
-shell.exec(`${dc} -f docker-compose-tests.yml run --rm tests-with-wordpress ../vendor/bin/phpunit -c phpunit-temp.xml`);
+shell.exec(`${dc} run --rm tests-with-wordpress ../vendor/bin/phpunit -c phpunit.xml`);
 shell.exec(`${dc} down`);
