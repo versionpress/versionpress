c46b93b0ae09322bf644d7000a10067012257d5e
Borek Bernard <borekb@gmail.com>
2019-03-17 21:14:14 +0100

    Cosmetic cleanup of run-tests.ts

--- scripts/run-tests.ts
+++ scripts/run-tests.ts
@@ -4,12 +4,11 @@ import { repoRoot } from "./script-utils";
 
 const dc = 'docker-compose -f docker-compose-tests.yml';
 
-utils.printTaskHeading('Cleaning up Docker stack incl. volumes...');
+utils.printTaskHeading('Cleaning up Docker containers and volumes...');
 shell.exec(`${dc} down -v`, { cwd: repoRoot });
 
 utils.printTaskHeading('Starting tests...');
 shell.exec(`${dc} up -d wordpress-for-tests`, { cwd: repoRoot });
-
 shell.exec(`${dc} run --rm wait`, { cwd: repoRoot });
 
 shell.exec(`${dc} run --rm tests-with-wordpress ../vendor/bin/phpunit -c phpunit.xml`, { cwd: repoRoot });
