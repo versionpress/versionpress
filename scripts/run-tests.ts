8d21756f333dc217baf7b73e09d2e8f9402c350d
Borek Bernard <borekb@gmail.com>
2019-03-17 21:10:32 +0100

    Update run-tests after merging in PR #1404

--- scripts/run-tests.ts
+++ scripts/run-tests.ts
@@ -1,17 +1,16 @@
 import * as shell from 'shelljs';
 import * as utils from "./script-utils";
+import { repoRoot } from "./script-utils";
 
 const dc = 'docker-compose -f docker-compose-tests.yml';
 
-utils.exitIfNotRunFromRootDir();
-
 utils.printTaskHeading('Cleaning up Docker stack incl. volumes...');
-shell.exec(`${dc} down -v`);
+shell.exec(`${dc} down -v`, { cwd: repoRoot });
 
 utils.printTaskHeading('Starting tests...');
-shell.exec(`${dc} up -d wordpress-for-tests`);
+shell.exec(`${dc} up -d wordpress-for-tests`, { cwd: repoRoot });
 
-shell.exec(`${dc} run --rm wait`);
+shell.exec(`${dc} run --rm wait`, { cwd: repoRoot });
 
-shell.exec(`${dc} run --rm tests-with-wordpress ../vendor/bin/phpunit -c phpunit.xml`);
-shell.exec(`${dc} down`);
+shell.exec(`${dc} run --rm tests-with-wordpress ../vendor/bin/phpunit -c phpunit.xml`, { cwd: repoRoot });
+shell.exec(`${dc} down`, { cwd: repoRoot });
